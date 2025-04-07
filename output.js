5132eea87d260a6fc4eb403ecee3f4ac708af999
action <action@github.com>
2025-04-07 18:48:33 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,196 +1,124 @@
-//Mon Apr 07 2025 18:33:12 GMT+0000 (Coordinated Universal Time)
+//Mon Apr 07 2025 18:48:33 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
 (function () {
   "use strict";
-  const _0x265870 = function () {};
-  const _0x4da061 = window.Function.toString;
-  if (/https:\/\/www\.66rpg\.com\/h5\/\d+/.test(window.location.href) || /https:\/\/m\.66rpg\.com\/h5\/\d+/.test(window.location.href)) {
-    {
-      const _0x1807bd = window.Function.prototype.constructor;
-      const _0x280168 = {
-        apply: function (_0x3a7a48, _0x53fc71, _0x384f71) {
-          {
-            const _0x408706 = _0x384f71.join(",");
-            if (_0x408706.includes("debugger")) {
-              {
-                return _0x265870;
-              }
+  function _0x198fb0() {
+    (function () {})();
+    const _0x5eaf4d = function () {},
+      _0xca22c0 = window.Function.toString;
+    if (/https:\/\/www\.66rpg\.com\/h5\/\d+/.test(window.location.href) || /https:\/\/m\.66rpg\.com\/h5\/\d+/.test(window.location.href)) {
+      const _0x289271 = new Proxy(window.Function.prototype.constructor, {
+          "apply": function (_0xaae010, _0x55c468, _0x39e28a) {
+            {
+              const _0x33ac0c = _0x39e28a.join(",");
+              if (_0x33ac0c.includes("debugger")) return _0x5eaf4d;
+              return Reflect.apply(_0xaae010, _0x55c468, _0x39e28a);
             }
-            return Reflect.apply(_0x3a7a48, _0x53fc71, _0x384f71);
-          }
-        },
-        get: function (_0x6263cb, _0x19ae5a, _0x1220e4) {
-          {
-            if (_0x19ae5a === "toString") {
-              {
+          },
+          "get": function (_0x2b503f, _0x38e67d, _0x2b6c68) {
+            {
+              if (_0x38e67d === "toString") {
                 return function () {
-                  {
-                    if (this === _0x24cb31) {
-                      {
-                        return "function Function() { [native code] }";
-                      }
-                    }
-                    return _0x4da061.call(this);
+                  if (this === _0x144691) {
+                    return "function Function() { [native code] }";
                   }
+                  return _0xca22c0.call(this);
                 };
               }
+              return Reflect.get(_0x2b503f, _0x38e67d, _0x2b6c68);
             }
-            return Reflect.get(_0x6263cb, _0x19ae5a, _0x1220e4);
           }
-        }
-      };
-      const _0x2aa050 = new Proxy(_0x1807bd, _0x280168);
-      const _0x24cb31 = _0x2aa050;
-      const _0x217922 = {
-        value: _0x2aa050,
-        writable: true,
-        configurable: true,
-        enumerable: false
-      };
-      Object.defineProperty(Function.prototype, "constructor", _0x217922);
+        }),
+        _0x144691 = _0x289271;
+      Object.defineProperty(Function.prototype, "constructor", {
+        "value": _0x144691,
+        "writable": true,
+        "configurable": true,
+        "enumerable": false
+      });
     }
   }
-  const _0x2c90bc = function (_0x4512f4, _0x11223e) {
-    {
-      const _0x47f382 = _0x4512f4.Function.prototype.toString;
-      const _0x4b3a08 = {
-        apply: function (_0x38ac12, _0x3e3547, _0x12abc6) {
-          {
-            if (_0x3e3547 === _0x11223e.console2.table) {
-              {
-                return "function table() { [native code] }";
-              }
-            }
-            return Reflect.apply(_0x38ac12, _0x3e3547, _0x12abc6);
-          }
-        },
-        get: function (_0x4fd165, _0x4c4829, _0x171b3a) {
-          {
-            if (_0x4c4829 === "toString") {
-              {
-                return function () {
-                  {
-                    return "function toString() { [native code] }";
-                  }
-                };
-              }
-            }
-            return Reflect.get(_0x4fd165, _0x4c4829, _0x171b3a);
-          }
+  function _0x1b25ac(_0x93af62) {
+    const _0x582d86 = _0x93af62.Function.prototype.toString;
+    _0x93af62.Function.prototype.toString = new Proxy(_0x582d86, {
+      "apply": function (_0x36f7db, _0x29399c, _0x2ba1cd) {
+        {
+          if (_0x29399c === _0x93af62.console2.table) return "function table() { [native code] }";
+          return Reflect.apply(_0x36f7db, _0x29399c, _0x2ba1cd);
         }
-      };
-      _0x4512f4.Function.prototype.toString = new Proxy(_0x47f382, _0x4b3a08);
-    }
-  };
-  const _0x6e90d0 = function (_0x419eaa) {
+      },
+      "get": function (_0x271fbb, _0x8d2ecc, _0x2c9557) {
+        if (_0x8d2ecc === "toString") return function () {
+          return "function toString() { [native code] }";
+        };
+        return Reflect.get(_0x271fbb, _0x8d2ecc, _0x2c9557);
+      }
+    });
+  }
+  function _0x542df3(_0x54dbf3) {
     {
-      const _0x4ea891 = _0x419eaa.Array.prototype.join;
-      _0x419eaa.Array.prototype.join = function () {
+      const _0x5caa07 = _0x54dbf3.Array.prototype.join;
+      _0x54dbf3.Array.prototype.join = function () {
         {
           if (this.length === 2 && this[0].includes("rand=") && this[1] === "source=devTools") {
             {
-              const _0x4881d8 = window.document.getElementById("gameMainBox")?.["contentWindow"] || window;
-              _0x4881d8.console2.table = function () {};
-              _0x2c90bc(_0x419eaa, _0x4881d8);
-              _0x4881d8.console2.log = function () {};
-              _0x4881d8.Array.prototype.join = _0x4ea891;
+              const _0x51d0b = window.document.getElementById("gameMainBox")?.["contentWindow"] || window;
+              _0x51d0b.console2.table = function () {};
+              _0x1b25ac(_0x54dbf3);
+              _0x51d0b.console2.log = function () {};
+              _0x51d0b.Array.prototype.join = _0x5caa07;
             }
           }
-          return _0x4ea891.apply(this, arguments);
+          return _0x5caa07.apply(this, arguments);
         }
       };
     }
-  };
-  const _0x1eb7c0 = window.setInterval;
-  if (/https:\/\/m\.66rpg\.com\/h5\/\d+/.test(window.location.href)) {
-    if (!window.isMS) {
-      window.setInterval = new Proxy(_0x1eb7c0, {
-        apply(_0x14d263, _0x45252a, _0x5f08b4) {
-          {
-            const [_0x1cfa81, _0x4a7db4, ..._0x15fb8f] = _0x5f08b4;
-            if (_0x4a7db4 === 20) {
-              {
-                let _0x1931a6 = _0x1cfa81.toString();
-                if (_0x1931a6.includes("window") && _0x1931a6.includes("function(){}") && _0x1931a6.includes("Date")) {
+  }
+  function _0x81d7b8() {
+    {
+      const _0x1e09a1 = window.setInterval;
+      if (/https:\/\/m\.66rpg\.com\/h5\/\d+/.test(window.location.href)) {
+        {
+          if (!window.isMS) {
+            {
+              window.setInterval = new Proxy(_0x1e09a1, {
+                "apply": function (_0x312770, _0x572be3, _0x6db5e) {
                   {
-                    window.setInterval = _0x1eb7c0;
-                    window._user_report = function () {};
-                    window.console.log = function () {};
-                    window.console.table = function () {};
-                    return null;
-                  }
-                }
-              }
-            }
-            return Reflect.apply(_0x14d263, _0x45252a, _0x5f08b4);
-          }
-        },
-        get(_0x381837, _0x45e9a9, _0x5ccac4) {
-          {
-            if (_0x45e9a9 === "toString") {
-              {
-                return function () {
-                  {
-                    if (this === _0x256b46) {
-                      {
-                        return "function setInterval() { [native code] }";
+                    const [_0x3368c1, _0x1e6f06, ..._0x228051] = _0x6db5e;
+                    if (_0x1e6f06 === 20) {
+                      const _0x4049b5 = _0x3368c1.toString();
+                      if (_0x4049b5.includes("window") && _0x4049b5.includes("function(){}") && _0x4049b5.includes("Date")) {
+                        window.setInterval = _0x1e09a1;
+                        window._user_report = function () {};
+                        window.console.log = function () {};
+                        window.console.table = function () {};
+                        return null;
                       }
                     }
-                    return _0x4da061.call(this);
+                    return Reflect.apply(_0x312770, _0x572be3, _0x6db5e);
                   }
-                };
-              }
+                },
+                "get": function (_0x9741e3, _0x29368f, _0x2b1af6) {
+                  {
+                    if (_0x29368f === "toString") return function () {
+                      if (this === _0x5b14a4) {
+                        return "function setInterval() { [native code] }";
+                      }
+                      return originalFunctionToString.call(this);
+                    };
+                    return Reflect.get(_0x9741e3, _0x29368f, _0x2b1af6);
+                  }
+                }
+              });
+              const _0x5b14a4 = window.setInterval;
+              _0x542df3(window);
             }
-            return Reflect.get(_0x381837, _0x45e9a9, _0x5ccac4);
           }
         }
-      });
-      const _0x256b46 = window.setInterval;
-      _0x6e90d0(window);
+      }
     }
   }
-  if (/https:\/\/www\.66rpg\.com\/game\/\d+/.test(window.location.href)) {
-    let _0x386b1e = 0;
-    const _0x255d11 = _0x1eb7c0(function () {
-      if (window.document.getElementById("gameMainBox")) {
-        const _0x4b2880 = window.document.getElementById("gameMainBox");
-        const _0xa90398 = _0x4b2880.contentWindow.setInterval;
-        _0x4b2880.contentWindow.setInterval = new Proxy(_0xa90398, {
-          apply(_0x170334, _0x3af393, _0x573544) {
-            const [_0x155d05, _0x47c6f5, ..._0x112ea7] = _0x573544;
-            if (_0x47c6f5 === 20) {
-              let _0x437c00 = _0x155d05.toString();
-              if (_0x437c00.includes("window") && _0x437c00.includes("function(){}") && _0x437c00.includes("Date")) {
-                _0x4b2880.contentWindow.setInterval = _0xa90398;
-                _0x4b2880.contentWindow._user_report = function () {};
-                _0x4b2880.contentWindow.console.log = function () {};
-                _0x4b2880.contentWindow.console.table = function () {};
-                _0x6e90d0(_0x4b2880.contentWindow);
-                return null;
-              }
-            }
-            return Reflect.apply(_0x170334, _0x3af393, _0x573544);
-          },
-          get(_0x1f27a6, _0x2cb957, _0x4af51d) {
-            if (_0x2cb957 === "toString") {
-              return function () {
-                if (this === _0x4db294) {
-                  return "function setInterval() { [native code] }";
-                }
-                return _0x4da061.call(this);
-              };
-            }
-            return Reflect.get(_0x1f27a6, _0x2cb957, _0x4af51d);
-          }
-        });
-        const _0x4db294 = _0x4b2880.contentWindow.setInterval;
-        _0x4b2880.contentWindow.isMS = true;
-        clearInterval(_0x255d11);
-      } else {
-        _0x386b1e++;
-        _0x386b1e > 500 && clearInterval(_0x255d11);
-      }
-    }, 10);
-  }
+  _0x198fb0();
+  _0x81d7b8();
 })();
