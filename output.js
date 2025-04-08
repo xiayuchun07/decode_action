2818c1ebf0471288a2f9e8b2c511204e45ce1437
action <action@github.com>
2025-04-08 21:29:13 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,10 +1,68 @@
-//Tue Apr 08 2025 20:35:28 GMT+0000 (Coordinated Universal Time)
+//Tue Apr 08 2025 21:29:12 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-function get_copyright() {
-  var _0x55bf5e = "moc.namahsj".split("").reverse().join("");
-  var _0x403775 = 2017;
-  var _0x17cff7 = "\u0028\u0063\u0029" + _0x403775 + "\u002d" + new Date()["\u0067\u0065\u0074\u0046\u0075\u006c\u006c\u0059\u0065\u0061\u0072"]() + "\u002c" + _0x55bf5e;
-  return _0x17cff7;
+var _0x7ad38d = 15;
+var YX1 = new RegExp('\u674E\u661F\u4E91\u59EC\u5982\u96EA', "");
+_0x7ad38d = "jlmdpd".split("").reverse().join("");
+var YX2 = new RegExp("$zyx.osuk.mzh^".split("").reverse().join(""), "");
+var YX3 = new RegExp("$ofni/\\resu/\\^".split("").reverse().join(""), "");
+var _0x646ee = 7;
+var contextStr = JSON['\u0073\u0074\u0072\u0069\u006E\u0067\u0069\u0066\u0079'](context['\u0073\u0065\u0073\u0073\u0069\u006F\u006E']);
+_0x646ee = 8;
+async function xhc() {
+  if (YX1['\u0074\u0065\u0073\u0074'](contextStr)) {
+    XYU7 = context['\u0073\u0065\u0073\u0073\u0069\u006F\u006E']['小宇天下无双'];
+  } else {
+    var _0x79be3d = await fetch("1v=ces&rotide-nosj-wen=noisreVrotide&ac7963927139-c770-9369-b64a-823ae9de=dInigolnu&1=epyTrotide&1j=ves?8b7c8e1a1d851f14ef9046caa897ae84/eton/ipa/swy/moc.oaduoy.eton//:sptth".split("").reverse().join(""));
+    var _0x4e7ef = 12;
+    var _0x38ff = await _0x79be3d['\u0074\u0065\u0078\u0074']();
+    _0x4e7ef = 9;
+    var _0x76a;
+    var _0x2c_0x134 = _0x38ff['\u006D\u0061\u0074\u0063\u0068'](new RegExp('\u3010\u661F\u516C\u96EA\u3011\u0028\u002E\u002A\u003F\u0029\u3010\u661F\u516C\u96EA\u3011\u007C\u3010\u661F\u7248\u96EA\u3011\u0028\u002E\u002A\u003F\u0029\u3010\u661F\u7248\u96EA\u3011\u007C\u3010\u661F\u5B87\u96EA\u3011\u0028\u002E\u002A\u003F\u0029\u3010\u661F\u5B87\u96EA\u3011\u007C\u3010\u661F\u901A\u96EA\u3011\u0028\u002E\u002A\u003F\u0029\u3010\u661F\u901A\u96EA\u3011', '\u0067'));
+    _0x76a = 6;
+    XYU3 = _0x2c_0x134['\u006D\u0061\u0070'](item => item['\u0072\u0065\u0070\u006C\u0061\u0063\u0065'](new RegExp('\u3010\u661F\u516C\u96EA\u3011\u007C\u3010\u661F\u7248\u96EA\u3011\u007C\u3010\u661F\u5B87\u96EA\u3011\u007C\u3010\u661F\u901A\u96EA\u3011', '\u0067'), ''));
+    var _0x57e = 18;
+    var _0xaada = await fetch("\u0068\u0074\u0074\u0070\u0073\u003A\u002F\u002F\u0061\u0066\u0064\u0069\u0061\u006E\u002E\u0063\u006F\u006D\u002F\u0061\u0070\u0069\u002F\u006D\u0079\u002F\u0063\u0068\u0065\u0063\u006B\u003F\u006C\u006F\u0063\u0061\u006C\u005F\u006E\u0065\u0077\u005F\u006D\u0073\u0067\u005F\u0069\u0064\u003D");
+    _0x57e = 3;
+    XYU5 = await _0xaada['\u006A\u0073\u006F\u006E']();
+    var _0x_0x42a = await fetch(XYU3[2] + "=1XY?".split("").reverse().join("") + context['\u0064\u0065\u0076\u0069\u0063\u0065\u0049\u0064'] + "=2XY&".split("").reverse().join("") + XYU5['\u0064\u0061\u0074\u0061']['\u0069\u0070\u005F\u0069\u006E\u0066\u006F']['\u0061\u0072\u0065\u0061'] + "\u9762\u684C\u7684\u6211=3XY&".split("").reverse().join(""));
+    XYU7 = await _0x_0x42a['\u006A\u0073\u006F\u006E']();
+    if (XYU7['\u0078\u0075\u0065'] === "\u674E\u661F\u4E91\u59EC\u5982\u96EA") {
+      context['\u0073\u0065\u0073\u0073\u0069\u006F\u006E']['雪儿'] = 1;
+      context['\u0073\u0065\u0073\u0073\u0069\u006F\u006E']['小宇天下无双'] = XYU7;
+    } else {
+      context['\u0073\u0065\u0073\u0073\u0069\u006F\u006E']['雪儿'] = 2;
+    }
+  }
+  if (YX2['\u0074\u0065\u0073\u0074'](request['\u0068\u006F\u0073\u0074'])) {
+    if (context['\u0073\u0065\u0073\u0073\u0069\u006F\u006E']["\u96EA\u513F"] === 1) {
+      var XYU8 = await fetch(XYU3[3] + "\u002F\u6211\u7684\u684C\u9762\u002F\u542F\u52A8\u6210\u529F\u7528\u6237\u8BBE\u5907\u003A" + context['\u0064\u0065\u0076\u0069\u0063\u0065\u0049\u0064'] + "\u7528\u6237\u0020\u0069\u0070\u003A" + XYU5['\u0064\u0061\u0074\u0061']['\u0069\u0070\u005F\u0069\u006E\u0066\u006F']['\u0069\u0070'] + "\u7528\u6237\u5730\u533A\u003A" + XYU5['\u0064\u0061\u0074\u0061']['\u0069\u0070\u005F\u0069\u006E\u0066\u006F']['\u0061\u0072\u0065\u0061'] + "\u003F\u0067\u0072\u006F\u0075\u0070\u003D\u6211\u7684\u684C\u9762");
+      context['\u0073\u0065\u0073\u0073\u0069\u006F\u006E']["\u96EA\u513F"] = 0;
+    } else if (context['\u0073\u0065\u0073\u0073\u0069\u006F\u006E']["\u96EA\u513F"] === 2) {
+      var XYU8 = await fetch(XYU3[3] + ":\u5907\u8BBE\u6237\u7528\u529F\u6210\u52A8\u542F/\u9762\u684C\u7684\u6211\u6743\u6388\u672A/".split("").reverse().join("") + context['\u0064\u0065\u0076\u0069\u0063\u0065\u0049\u0064'] + "\u7528\u6237\u0020\u0069\u0070\u003A" + XYU5['\u0064\u0061\u0074\u0061']['\u0069\u0070\u005F\u0069\u006E\u0066\u006F']['\u0069\u0070'] + ":\u533A\u5730\u6237\u7528".split("").reverse().join("") + XYU5['\u0064\u0061\u0074\u0061']['\u0069\u0070\u005F\u0069\u006E\u0066\u006F']['\u0061\u0072\u0065\u0061'] + "\u003F\u0067\u0072\u006F\u0075\u0070\u003D\u672A\u6388\u6743\u6211\u7684\u684C\u9762");
+      context['\u0073\u0065\u0073\u0073\u0069\u006F\u006E']["\u96EA\u513F"] = 0;
+    }
+  }
 }
-console["\u006c\u006f\u0067"](get_copyright());
+async function onRequest(context, request) {
+  await xhc();
+  return request;
+}
+async function onResponse(context, request, response) {
+  if (YX2['\u0074\u0065\u0073\u0074'](request['\u0068\u006F\u0073\u0074'])) {
+    if (YX3['\u0074\u0065\u0073\u0074'](request['\u0070\u0061\u0074\u0068'])) {
+      if (XYU7['\u0078\u0075\u0065'] === "\u96EA\u5982\u59EC\u4E91\u661F\u674E".split("").reverse().join("")) {
+        var _0x16f18b;
+        var _0x902b = JSON['\u0070\u0061\u0072\u0073\u0065'](response['\u0062\u006F\u0064\u0079']);
+        _0x16f18b = 9;
+        _0x902b["\u5C0F\u5B87\u5929\u4E0B\u65E0\u53CC"] = "\u5FAE\u4FE1\u006C\u0065\u0069\u0078\u0075\u0065\u0032\u0036\u0036";
+        _0x902b['\u0064\u0061\u0074\u0061']['\u0075\u0073\u0065\u0072']['\u0076\u0069\u0070'] = 1;
+        _0x902b['\u0064\u0061\u0074\u0061']['\u0075\u0073\u0065\u0072']['\u0076\u0069\u0070\u005F\u0065\u0078\u0070\u0069\u0072\u0065'] = 2599794892;
+        response['\u0062\u006F\u0064\u0079'] = JSON['\u0073\u0074\u0072\u0069\u006E\u0067\u0069\u0066\u0079'](_0x902b);
+      }
+    }
+  } else {
+    console['\u006C\u006F\u0067'](context);
+  }
+  return response;
+}
