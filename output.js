e36329c81b19224e9d3d168b9cbec2ee66e29b86
action <action@github.com>
2025-04-09 20:01:24 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,11 +1,11 @@
-//Wed Apr 09 2025 10:43:05 GMT+0000 (Coordinated Universal Time)
+//Wed Apr 09 2025 20:01:24 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-(function () {
-  var _0x195e31 = 2;
-  var _0x254875 = "53";
-  var _0x2849aa = "5" + -"2";
-  var _0x38e605 = ["10", "10", "10", "10", "10"].map(parseInt);
-  var _0xdc7ead = "foo 11";
-  alert(_0x195e31 + _0x254875 + _0x2849aa + _0x38e605 + _0xdc7ead);
-})();
+var YX2 = new RegExp("^hzm.kuso.xyz$", "");
+var YX3 = new RegExp("^\\/user\\/info$", "");
+var _0x73f8e;
+var contextStr = JSON.stringify(context.session);
+_0x73f8e = function () {
+  return "_0x38d752310";
+}();
+async function xhc() {}
