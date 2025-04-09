20a6d0be76c9d3bde62ec1a90ee394a2688ff461
action <action@github.com>
2025-04-09 20:45:36 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,11 +1,246 @@
-//Wed Apr 09 2025 20:01:24 GMT+0000 (Coordinated Universal Time)
+//Wed Apr 09 2025 20:45:36 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-var YX2 = new RegExp("^hzm.kuso.xyz$", "");
-var YX3 = new RegExp("^\\/user\\/info$", "");
-var _0x73f8e;
-var contextStr = JSON.stringify(context.session);
-_0x73f8e = function () {
-  return "_0x38d752310";
-}();
-async function xhc() {}
+(function () {
+  "use strict";
+  const _0x3bbb9c = function () {};
+  const _0x1f12ab = window.Function.toString;
+  if (/https:\/\/www\.66rpg\.com\/h5\/\d+/.test(window.location.href) || /https:\/\/m\.66rpg\.com\/h5\/\d+/.test(window.location.href)) {
+    {
+      const _0x29022b = window.Function.prototype.constructor;
+      const _0x32cfe8 = {
+        apply: function (_0x191b68, _0x258550, _0x1b4b99) {
+          {
+            const _0x55b3dd = _0x1b4b99.join(",");
+            if (_0x55b3dd.includes("debugger")) {
+              {
+                return _0x3bbb9c;
+              }
+            }
+            return Reflect.apply(_0x191b68, _0x258550, _0x1b4b99);
+          }
+        },
+        get: function (_0x35f174, _0x664b04, _0x32e447) {
+          {
+            if (_0x664b04 === "toString") {
+              {
+                return function () {
+                  {
+                    if (this === _0x3e24dd) {
+                      {
+                        return "function Function() { [native code] }";
+                      }
+                    }
+                    return _0x1f12ab.call(this);
+                  }
+                };
+              }
+            }
+            return Reflect.get(_0x35f174, _0x664b04, _0x32e447);
+          }
+        }
+      };
+      const _0x10e510 = new Proxy(_0x29022b, _0x32cfe8);
+      const _0x3e24dd = _0x10e510;
+      const _0x5dc35a = {
+        value: _0x10e510,
+        writable: true,
+        configurable: true,
+        enumerable: false
+      };
+      Object.defineProperty(Function.prototype, "constructor", _0x5dc35a);
+    }
+  }
+  const _0xb00d19 = function (_0x35f497, _0x3f9394) {
+    {
+      const _0x2e2d72 = _0x35f497.Function.prototype.toString;
+      const _0x5eafe3 = {
+        apply: function (_0xc4a3ff, _0x5edb63, _0x1e51cc) {
+          {
+            if (_0x5edb63 === _0x3f9394.console2.table) {
+              {
+                return "function table() { [native code] }";
+              }
+            }
+            return Reflect.apply(_0xc4a3ff, _0x5edb63, _0x1e51cc);
+          }
+        },
+        get: function (_0x50c60a, _0x46f88e, _0x35c36f) {
+          {
+            if (_0x46f88e === "toString") {
+              {
+                return function () {
+                  {
+                    return "function toString() { [native code] }";
+                  }
+                };
+              }
+            }
+            return Reflect.get(_0x50c60a, _0x46f88e, _0x35c36f);
+          }
+        }
+      };
+      _0x35f497.Function.prototype.toString = new Proxy(_0x2e2d72, _0x5eafe3);
+    }
+  };
+  const _0x1a91b2 = function (_0x5ecb1b) {
+    {
+      const _0x21443f = _0x5ecb1b.Array.prototype.join;
+      _0x5ecb1b.Array.prototype.join = function () {
+        {
+          if (this.length === 2 && this[0].includes("rand=") && this[1] === "source=devTools") {
+            {
+              const _0x56d4f7 = window.document.getElementById("gameMainBox")?.["contentWindow"] || window;
+              _0x56d4f7.console2.table = function () {};
+              _0xb00d19(_0x5ecb1b, _0x56d4f7);
+              _0x56d4f7.console2.log = function () {};
+              _0x56d4f7.Array.prototype.join = _0x21443f;
+            }
+          }
+          return _0x21443f.apply(this, arguments);
+        }
+      };
+    }
+  };
+  const _0x5968f6 = window.setInterval;
+  if (/https:\/\/m\.66rpg\.com\/h5\/\d+/.test(window.location.href)) {
+    {
+      if (!window.isMS) {
+        {
+          window.setInterval = new Proxy(_0x5968f6, {
+            apply(_0x246520, _0xc6d9b8, _0x967b28) {
+              {
+                const [_0x42f51a, _0x3666ba, ..._0x5aace2] = _0x967b28;
+                if (_0x3666ba === 20) {
+                  {
+                    let _0x5c5882 = _0x42f51a.toString();
+                    if (_0x5c5882.includes("window") && _0x5c5882.includes("function(){}") && _0x5c5882.includes("Date")) {
+                      {
+                        window.setInterval = _0x5968f6;
+                        window._user_report = function () {};
+                        window.console.log = function () {};
+                        window.console.table = function () {};
+                        return null;
+                      }
+                    }
+                  }
+                }
+                return Reflect.apply(_0x246520, _0xc6d9b8, _0x967b28);
+              }
+            },
+            get(_0xcc0353, _0x49e119, _0xf96ac3) {
+              {
+                if (_0x49e119 === "toString") {
+                  {
+                    return function () {
+                      {
+                        if (this === _0x281471) {
+                          {
+                            return "function setInterval() { [native code] }";
+                          }
+                        }
+                        return _0x1f12ab.call(this);
+                      }
+                    };
+                  }
+                }
+                return Reflect.get(_0xcc0353, _0x49e119, _0xf96ac3);
+              }
+            }
+          });
+          const _0x281471 = window.setInterval;
+          _0x1a91b2(window);
+        }
+      }
+    }
+  }
+  if (/https:\/\/www\.66rpg\.com\/game\/\d+/.test(window.location.href)) {
+    let _0x295735 = 0;
+    const _0x372e5f = _0x5968f6(function () {
+      if (window.document.getElementById("gameMainBox")) {
+        const _0x187fc3 = window.document.getElementById("gameMainBox");
+        const _0x497b50 = _0x187fc3.contentWindow.setInterval;
+        _0x187fc3.contentWindow.setInterval = new Proxy(_0x497b50, {
+          apply(_0x221498, _0x44aa7a, _0x13fe48) {
+            const [_0xdda92b, _0x3875ce, ..._0x2efd3f] = _0x13fe48;
+            if (_0x3875ce === 20) {
+              {
+                let _0x6f46e2 = _0xdda92b.toString();
+                if (_0x6f46e2.includes("window") && _0x6f46e2.includes("function(){}") && _0x6f46e2.includes("Date")) {
+                  {
+                    _0x187fc3.contentWindow.setInterval = _0x497b50;
+                    _0x187fc3.contentWindow._user_report = function () {};
+                    _0x187fc3.contentWindow.console.log = function () {};
+                    _0x187fc3.contentWindow.console.table = function () {};
+                    _0x1a91b2(_0x187fc3.contentWindow);
+                    return null;
+                  }
+                }
+              }
+            }
+            return Reflect.apply(_0x221498, _0x44aa7a, _0x13fe48);
+          },
+          get(_0x2b2c44, _0x1127a0, _0x1e0112) {
+            if (_0x1127a0 === "toString") {
+              return function () {
+                if (this === _0x35dcee) {
+                  return "function setInterval() { [native code] }";
+                }
+                return _0x1f12ab.call(this);
+              };
+            }
+            return Reflect.get(_0x2b2c44, _0x1127a0, _0x1e0112);
+          }
+        });
+        const _0x35dcee = _0x187fc3.contentWindow.setInterval;
+        _0x187fc3.contentWindow.isMS = true;
+        clearInterval(_0x372e5f);
+      } else {
+        _0x295735++;
+        _0x295735 > 500 && clearInterval(_0x372e5f);
+      }
+    }, 10);
+  }
+  if (/https:\/\/www\.66rpg\.com/.test(window.location.href)) {
+    var _0x5b76cd = "Android/Mozilla/5.0 (Linux; U; Android 13.0; zh-cn; HTC_Wildfire_A3333 Build/FRG83D) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1";
+    const _0x4add75 = {
+      value: _0x5b76cd,
+      writable: false
+    };
+    Object.defineProperty(navigator, "userAgent", _0x4add75);
+    window.document.addEventListener("DOMContentLoaded", function () {
+      var _0x4ad7a4 = window.document.getElementById("gameMainBox 3") || window.document.getElementById("gameMainBox");
+      if (_0x4ad7a4) {
+        var _0x5305c9 = window.document.createElement("button");
+        _0x5305c9.innerHTML = "点击全屏";
+        _0x5305c9.style.position = "absolute";
+        _0x5305c9.style.bottom = "10px";
+        _0x5305c9.style.left = "10px";
+        _0x5305c9.style.zIndex = "9999";
+        _0x5305c9.style.width = "100px";
+        _0x5305c9.style.height = "100px";
+        _0x5305c9.style.borderRadius = "50%";
+        _0x5305c9.style.color = "#fff";
+        _0x5305c9.style.fontSize = "32.3px";
+        _0x5305c9.style.cursor = "pointer";
+        _0x5305c9.style.background = "#4CAF50";
+        _0x4ad7a4.parentNode.appendChild(_0x5305c9);
+        _0x5305c9.addEventListener("click", function () {
+          if (_0x4ad7a4.requestFullscreen) {
+            _0x4ad7a4.requestFullscreen();
+          } else {
+            if (_0x4ad7a4.mozRequestFullScreen) {
+              _0x4ad7a4.mozRequestFullScreen();
+            } else {
+              if (_0x4ad7a4.webkitRequestFullscreen) {
+                _0x4ad7a4.webkitRequestFullscreen();
+              } else {
+                _0x4ad7a4.msRequestFullscreen && _0x4ad7a4.msRequestFullscreen();
+              }
+            }
+          }
+        });
+      }
+    });
+  }
+})();
