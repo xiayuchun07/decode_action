076029a13b7fb01a54307324adaa11d13cf4c088
action <action@github.com>
2025-04-14 13:02:02 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,27 +1,27 @@
-//Mon Apr 14 2025 12:57:43 GMT+0000 (Coordinated Universal Time)
+//Mon Apr 14 2025 13:02:02 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-function calculator(_0x5be15c, _0x5ece49, _0x4c8889) {
-  if (_0x5be15c === "add") {
-    return _0x5ece49 + _0x4c8889;
+function calculator(_0x47f461, _0x4bc5e0, _0x383f14) {
+  if (_0x47f461 === "add") {
+    {
+      return _0x4bc5e0 + _0x383f14;
+    }
   } else {
-    if (_0x5be15c === "subtract") {
-      return _0x5ece49 - _0x4c8889;
+    if (_0x47f461 === "subtract") {
+      return _0x4bc5e0 - _0x383f14;
     } else {
-      if (_0x5be15c === "multiply") {
-        return _0x5ece49 * _0x4c8889;
+      if (_0x47f461 === "multiply") {
+        return _0x4bc5e0 * _0x383f14;
       } else {
-        if (_0x5be15c === "divide") {
-          if (_0x4c8889 === 0) {
+        if (_0x47f461 === "divide") {
+          if (_0x383f14 === 0) {
             {
               throw new Error("不能除以零");
             }
           }
-          return _0x5ece49 / _0x4c8889;
+          return _0x4bc5e0 / _0x383f14;
         } else {
-          {
-            throw new Error("不支持的操作: " + _0x5be15c);
-          }
+          throw new Error("不支持的操作: " + _0x47f461);
         }
       }
     }
