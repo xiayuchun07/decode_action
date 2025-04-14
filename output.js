fb174bfb8b0e2b253f065eea0bed85fc42516290
action <action@github.com>
2025-04-14 19:01:57 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,10 +1,29 @@
-//Mon Apr 14 2025 18:16:36 GMT+0000 (Coordinated Universal Time)
+//Mon Apr 14 2025 19:01:57 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-var u = [`\u0028\u0066\u0075\u006e\u0063\u0074\u0069\u006f\u006e`, `\u0028\u0029\u007b`, `\u0076\u0061\u0072`, `\u0064\u006f\u006d\u0061\u0069\u006e`, `\u0022\u006a\u0073\u0068\u0061\u006d\u0061\u006e\u002e\u0063\u006f\u006d\u0022\u003b`, `\u0066\u0072\u006f\u006d\u005f\u0079\u0065\u0061\u0072`, `\u0032\u0030\u0031\u0037\u003b`, `\u0063\u006f\u0070\u0079\u0072\u0069\u0067\u0068\u0074`, `\u0066\u0075\u006e\u0063\u0074\u0069\u006f\u006e\u0028\u0029\u007b`, `\u0072\u0065\u0074\u0075\u0072\u006e`, `\u0022\u0028\u0063\u0029\u0022`, `\u0022\u002d\u0022`, `\u0028\u006e\u0065\u0077`, `\u0044\u0061\u0074\u0065\u0029\u002e\u0067\u0065\u0074\u0046\u0075\u006c\u006c\u0059\u0065\u0061\u0072\u0028\u0029`, `\u0022\u002c\u0022`, `\u0064\u006f\u006d\u0061\u0069\u006e\u003b`, `\u0063\u006f\u006e\u0073\u006f\u006c\u0065\u005f\u006c\u006f\u0067`, `\u0063\u006f\u006e\u0073\u006f\u006c\u0065\u002e\u006c\u006f\u0067\u003b`, `\u0063\u006f\u006e\u0073\u006f\u006c\u0065\u005f\u006c\u006f\u0067\u0028\u0063\u006f\u0070\u0079\u0072\u0069\u0067\u0068\u0074\u0028\u0029\u0029`, `\u007d\u0029\u0028\u0029\u003b`];
-var u2 = [0, 1, 2, 3, 5, 7, 9, 11, 13, 14, 15, 19, 21, 22, 24, 26, 29, 31, 32, 33];
-var u3 = `u6[0] u6[1]u6[2] u6[3] = u6[5]u6[2] u6[7] = u6[9]u6[2] u6[11] = u6[13]u6[14] u6[15] + u6[7] + u6[19] + u6[21] u6[22] + u6[24] + u6[26]};u6[2] u6[29] = u6[31]u6[32]u6[33]`;
-for (u5 = 0; u5 < u.length; u5++) {
-  u3 = u3.replace(new RegExp("u6\\[" + u2[u5] + "\\]", "g"), u[u5].replace("`", "").replace("`", ""));
+function _0x5199cb() {
+  console.log("宇");
 }
-eval(u3);
+_0x5199cb();
+console.log("Hello World");
+function mMJpIi(_0x3324af, _0x3f2ac4) {
+  return;
+  mMJpIi = function (_0x206e81, _0x9d7fcf) {
+    _0x206e81 = _0x206e81 - 0;
+    var _0xfdd6ef = _0x5e50e6[_0x206e81];
+    return _0xfdd6ef;
+  };
+  return mMJpIi(_0x3324af, _0x3f2ac4);
+}
+mMJpIi();
+alert("测试示例");
+function lSrMOz(_0x3e2e5a, _0x189bc0) {
+  return;
+  lSrMOz = function (_0x1404e6, _0x1cf2c6) {
+    _0x1404e6 = _0x1404e6 - 0;
+    var _0x4d3132 = _0x5e50e6[_0x1404e6];
+    return _0x4d3132;
+  };
+  return lSrMOz(_0x3e2e5a, _0x189bc0);
+}
+lSrMOz();
