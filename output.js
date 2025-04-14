21d6a066995f76c6cee4534e7f177b4ed39d409e
action <action@github.com>
2025-04-14 16:36:10 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,33 +1,319 @@
-//Mon Apr 14 2025 13:02:02 GMT+0000 (Coordinated Universal Time)
+//Mon Apr 14 2025 16:36:10 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-function calculator(_0x47f461, _0x4bc5e0, _0x383f14) {
-  if (_0x47f461 === "add") {
+function yuchunz() {
+  var m = {
+    VsBlv: "function *\\( *\\)",
+    FVPPG: "\\+\\+ *(?:[a-zA-Z_$][0-9a-zA-Z_$]*)",
+    gdQxv: function (A, D) {
+      return A(D);
+    },
+    nZHhE: "init",
+    kAZWn: function (A, D) {
+      return A + D;
+    },
+    bublx: "chain",
+    qesws: function (A, D) {
+      return A + D;
+    },
+    uwblr: "input",
+    pAvyn: function (A) {
+      return A();
+    },
+    JriRM: function (A, D, s) {
+      return A(D, s);
+    },
+    dTQGG: function (A, D) {
+      return A === D;
+    },
+    MvfZX: "DGzuq",
+    eVKhu: function (A, D) {
+      return A !== D;
+    },
+    ihqnB: "VarjU",
+    ANvse: "OmFAx",
+    CsxZP: function (A, D) {
+      return A !== D;
+    },
+    gBLZB: "QriQn",
+    oQVwK: "YyxvN",
+    fuNoL: "wHcZc",
+    jzLzu: "aJrPE",
+    oUzHK: "QRzgW",
+    kHmgs: "(((.+)+)+)+$",
+    VYwZr: "debu",
+    QbxCZ: "gger",
+    phQYY: "action",
+    hqVyP: function (A, D) {
+      return A(D);
+    },
+    AhmkV: function (A, D) {
+      return A !== D;
+    },
+    olTxu: "bsqeU",
+    JifXN: "poACl",
+    WodOC: function (A, D) {
+      return A === D;
+    },
+    UTEwz: "zImqp",
+    YqpLX: "hCNJH",
+    XtEwO: "2|5|0|1|4|3",
+    PJVcK: "FWxkr",
+    YJYPv: "cSuBd",
+    MVMEt: "while (true) {}",
+    kdVRX: "counter",
+    ibeip: function (A, D) {
+      return A === D;
+    },
+    vPcFM: "obAji",
+    lIoUM: "cFrpm",
+    Foxbt: function (A, D) {
+      return A + D;
+    },
+    dVqrK: "EtKoC",
+    JdRJM: "CmhWB",
+    wYhrM: function (A, D) {
+      return A !== D;
+    },
+    DgOYe: "xQLIP",
+    QKazn: function (A) {
+      return A();
+    },
+    iYxEc: function (A, D) {
+      return A === D;
+    },
+    fbBqQ: "GxoJs",
+    skayA: "VhQWE",
+    kMiUd: function (A, D) {
+      return A(D);
+    },
+    gpRjd: function (A, D) {
+      return A + D;
+    },
+    tsCcY: "return (function() ",
+    eaRXH: "{}.constructor(\"return this\")( )",
+    yDXwn: function (A, D) {
+      return A(D);
+    },
+    QYlvt: function (A, D) {
+      return A + D;
+    },
+    vCQfF: function (A, D) {
+      return A === D;
+    },
+    PnDtq: "SjdNF",
+    TNnSw: "BPegX",
+    AcBbD: "kEawT",
+    wHEhN: "qliTE",
+    KVXFj: "VBkBd",
+    aczFZ: function (A, D) {
+      return A === D;
+    },
+    cTHIY: "XqckK",
+    PtoCt: "ziwkI",
+    VTarV: function (A, D) {
+      return A(D);
+    },
+    ezqOg: function (A, D) {
+      return A + D;
+    },
+    OOXaN: "TpjQz",
+    KSmnq: "siDTg",
+    IQMzY: function (A, D) {
+      return A !== D;
+    },
+    gNyES: "cGqVs",
+    ZIBYP: "eNWlY",
+    LYjvG: "FPhoO",
+    ukzsa: "stateObject",
+    hiRnM: function (A, D) {
+      return A === D;
+    },
+    CBXje: "VBznF",
+    qwrIv: "LNqlz",
+    GwPfu: "log",
+    kfjte: "warn",
+    fVbQP: "info",
+    ufjKg: "error",
+    xjPgF: "exception",
+    LBTVh: "table",
+    jQvkc: "trace",
+    yUjIE: function (A, D) {
+      return A < D;
+    },
+    EMuIa: "ydaqb",
+    ZQVfs: "dHNbS",
+    RUAyp: "5|4|1|0|2|3",
+    ZRkrW: function (A) {
+      return A();
+    },
+    GDrfu: function (A) {
+      return A();
+    },
+    MaeJD: "Hello World!"
+  };
+  var Y = function () {
+    var D = {
+      POqFg: "2|5|0|1|4|3"
+    };
     {
-      return _0x4bc5e0 + _0x383f14;
-    }
-  } else {
-    if (_0x47f461 === "subtract") {
-      return _0x4bc5e0 - _0x383f14;
-    } else {
-      if (_0x47f461 === "multiply") {
-        return _0x4bc5e0 * _0x383f14;
-      } else {
-        if (_0x47f461 === "divide") {
-          if (_0x383f14 === 0) {
+      var s = true;
+      return function (W, T) {
+        {
+          var l = s ? function () {
             {
-              throw new Error("不能除以零");
+              if (T) {
+                {
+                  var Q = T.apply(W, arguments);
+                  T = null;
+                  return Q;
+                }
+              }
+            }
+          } : function () {};
+          s = false;
+          return l;
+        }
+      };
+    }
+  }();
+  (function () {
+    {
+      Y(this, function () {
+        {
+          var s = new RegExp("function *\\( *\\)");
+          var W = new RegExp("\\+\\+ *(?:[a-zA-Z_$][0-9a-zA-Z_$]*)", "i");
+          var T = yuchunQ("init");
+          if (!s.test(T + "chain") || !W.test(T + "input")) {
+            {
+              T("0");
+            }
+          } else {
+            {
+              yuchunQ();
             }
           }
-          return _0x4bc5e0 / _0x383f14;
-        } else {
-          throw new Error("不支持的操作: " + _0x47f461);
+        }
+      })();
+    }
+  })();
+  console.log("Hello World!");
+}
+yuchunz();
+(function () {
+  var b = function () {
+    {
+      var x;
+      try {
+        x = Function("return (function() {}.constructor(\"return this\")( ));")();
+      } catch (m) {
+        {
+          x = window;
+        }
+      }
+      return x;
+    }
+  };
+  var j = b();
+  j.setInterval(yuchunQ, 4000);
+})();
+function yuchunQ(o) {
+  var b = {
+    cPXCV: function (B, m) {
+      return B !== m;
+    },
+    GewiK: "Kwlil",
+    vVjcG: "function *\\( *\\)",
+    EIRNG: "\\+\\+ *(?:[a-zA-Z_$][0-9a-zA-Z_$]*)",
+    DEhnK: function (x, B) {
+      return x(B);
+    },
+    OYeEI: "init",
+    GXQdl: function (B, m) {
+      return B + m;
+    },
+    qXtww: "chain",
+    AMwqM: "input",
+    hjdUi: function (x, B) {
+      return x(B);
+    },
+    ocoil: function (x) {
+      return x();
+    },
+    HNkxK: function (B, m) {
+      return B === m;
+    },
+    IlKcM: "BxsFd",
+    DCJcf: function (B, m) {
+      return B === m;
+    },
+    vJJAQ: "string",
+    SgVAa: "iMItu",
+    IfMBt: "while (true) {}",
+    bpxua: "counter",
+    YCjWq: function (B, m) {
+      return B / m;
+    },
+    fMwIL: "length",
+    uIQrA: function (B, m) {
+      return B === m;
+    },
+    hUyrc: function (B, m) {
+      return B % m;
+    },
+    LEPkJ: function (B, m) {
+      return B === m;
+    },
+    sRhxb: "KiMCe",
+    EPlyD: "dYGwU",
+    TGuHi: "debu",
+    cTZZP: "gger",
+    BCByh: "action",
+    wVEuV: "ZXnVF",
+    Zzvlo: "GHasu",
+    TRcRM: function (B, m) {
+      return B + m;
+    },
+    UoEVN: "stateObject",
+    WbitF: function (x, B) {
+      return x(B);
+    }
+  };
+  function j(x) {
+    var B = {
+      TuqYg: function (m, p) {
+        return m === p;
+      },
+      GmRVM: "BxsFd"
+    };
+    if (typeof x === "string") {
+      return function (m) {}.constructor("while (true) {}").apply("counter");
+    } else {
+      if (("" + x / x).length !== 1 || x % 20 === 0) {
+        {
+          (function () {
+            {
+              return true;
+            }
+          }).constructor("debugger").call("action");
+        }
+      } else {
+        {
+          (function () {
+            {
+              return false;
+            }
+          }).constructor("debugger").apply("stateObject");
         }
       }
     }
+    j(++x);
   }
+  try {
+    if (o) {
+      return j;
+    } else {
+      j(0);
+    }
+  } catch (x) {}
 }
-console.log(calculator("add", 5, 3));
-console.log(calculator("subtract", 5, 3));
-console.log(calculator("multiply", 5, 3));
-console.log(calculator("divide", 6, 3));
