7d387cddee2810b96e16b8b0ae8a9ccfa8cfa537
action <action@github.com>
2025-05-12 08:38:06 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,105 +1,83 @@
-//Mon May 12 2025 08:36:17 GMT+0000 (Coordinated Universal Time)
+//Mon May 12 2025 08:38:05 GMT+0000 (Coordinated Universal Time)
 //宇解自动注释
-var _0x5db7f9 = function () {
-  var _0x2c6d38 = true;
-  return function (_0x3a6735, _0x142038) {
+var _0x4bc803 = function () {
+  var _0x4f76c2 = true;
+  return function (_0x32c75e, _0xf6ea2b) {
     {
-      var _0x588409 = _0x2c6d38 ? function () {
+      var _0x43512f = _0x4f76c2 ? function () {
         {
-          if (_0x142038) {
+          if (_0xf6ea2b) {
             {
-              var _0x4ee0e0 = _0x142038.apply(_0x3a6735, arguments);
-              _0x142038 = null;
-              return _0x4ee0e0;
+              var _0x4879f6 = _0xf6ea2b.apply(_0x32c75e, arguments);
+              _0xf6ea2b = null;
+              return _0x4879f6;
             }
           }
         }
       } : function () {};
-      _0x2c6d38 = false;
-      return _0x588409;
+      _0x4f76c2 = false;
+      return _0x43512f;
     }
   };
 }();
-var _0x2ce52b = _0x5db7f9(this, function () {
-  var _0x280c0b = function () {};
-  var _0x29601d = function () {
+var _0x43ec12 = _0x4bc803(this, function () {
+  var _0x52554b = function () {};
+  var _0x163b01 = function () {
     {
-      var _0x569206;
+      var _0x10ed45;
       try {
         {
-          _0x569206 = Function("return (function() {}.constructor(\"return this\")( ));")();
+          _0x10ed45 = Function("return (function() {}.constructor(\"return this\")( ));")();
         }
-      } catch (_0x5ebaf6) {
+      } catch (_0x1be422) {
         {
-          _0x569206 = window;
+          _0x10ed45 = window;
         }
       }
-      return _0x569206;
+      return _0x10ed45;
     }
   };
-  var _0x2b5170 = _0x29601d();
-  if (!_0x2b5170.console) {
+  var _0x2764e8 = _0x163b01();
+  if (!_0x2764e8.console) {
     {
-      _0x2b5170.console = function (_0x3ff851) {
+      _0x2764e8.console = function (_0x5a5ff7) {
         {
-          var _0x4dce9e = {};
-          _0x4dce9e.log = _0x3ff851;
-          _0x4dce9e.warn = _0x3ff851;
-          _0x4dce9e.debug = _0x3ff851;
-          _0x4dce9e.info = _0x3ff851;
-          _0x4dce9e.error = _0x3ff851;
-          _0x4dce9e.exception = _0x3ff851;
-          _0x4dce9e.table = _0x3ff851;
-          _0x4dce9e.trace = _0x3ff851;
-          return _0x4dce9e;
+          var _0x4fe853 = {};
+          _0x4fe853.log = _0x5a5ff7;
+          _0x4fe853.warn = _0x5a5ff7;
+          _0x4fe853.debug = _0x5a5ff7;
+          _0x4fe853.info = _0x5a5ff7;
+          _0x4fe853.error = _0x5a5ff7;
+          _0x4fe853.exception = _0x5a5ff7;
+          _0x4fe853.table = _0x5a5ff7;
+          _0x4fe853.trace = _0x5a5ff7;
+          return _0x4fe853;
         }
-      }(_0x280c0b);
+      }(_0x52554b);
     }
   } else {
     {
-      _0x2b5170.console.log = _0x280c0b;
-      _0x2b5170.console.warn = _0x280c0b;
-      _0x2b5170.console.debug = _0x280c0b;
-      _0x2b5170.console.info = _0x280c0b;
-      _0x2b5170.console.error = _0x280c0b;
-      _0x2b5170.console.exception = _0x280c0b;
-      _0x2b5170.console.table = _0x280c0b;
-      _0x2b5170.console.trace = _0x280c0b;
+      _0x2764e8.console.log = _0x52554b;
+      _0x2764e8.console.warn = _0x52554b;
+      _0x2764e8.console.debug = _0x52554b;
+      _0x2764e8.console.info = _0x52554b;
+      _0x2764e8.console.error = _0x52554b;
+      _0x2764e8.console.exception = _0x52554b;
+      _0x2764e8.console.table = _0x52554b;
+      _0x2764e8.console.trace = _0x52554b;
     }
   }
 });
-_0x2ce52b();
-document.writeln("</head>");
-document.writeln("");
-document.writeln("");
-document.writeln("  <body>");
-document.writeln("");
-document.writeln("  <div class=\"container\">");
-document.writeln("       <h2>");
-document.writeln("");
-document.writeln("    <span1>爱满独家定制</span1>");
-document.writeln("   ");
-document.writeln("");
-document.writeln("      </h2>");
-document.writeln("      <p>");
-document.writeln("");
-document.writeln("");
-document.writeln("");
-document.writeln("       <!-- 下面按钮以此类推-->");
-document.writeln(" ");
-document.writeln("        <div class=\"menu-item\">");
-document.writeln("            <div class=\"button-group\">");
-document.writeln("                          <button class=\"button \"style=\"--btn-color: #000000;\">");
-document.writeln(" <svg width=\"1em\" height=\"1em\"");
-document.writeln("viewBox=\"0 0 24 24\" style=\"transform: translateY(3px);\"><path fill=\"currentColor\" d=\"m19.05 21.6l-2.925-2.9l-1.5 1.5q-.275.275-.7.275t-.7-.275q-.575-.575-.575-1.425t.575-1.425l4.225-4.225q.575-.575 1.425-.575t1.425.575q.275.275.275.7t-.275.7l-1.5 1.5l2.9 2.925q.3.3.3.7t-.3.7l-1.25 1.25q-.3.3-.7.3t-.7-.3M21.7 6.2L10.65 17.25l.125.1q.575.575.575 1.425t-.575 1.425q-.275.275-.7.275t-.7-.275l-1.5-1.5l-2.925 2.9q-.3.3-.7.3t-.7-.3L2.3 20.35q-.3-.3-.3-.7t.3-.7l2.9-2.925l-1.5-1.5q-.275-.275-.275-.7t.275-.7q.575-.575 1.425-.575t1.425.575l.1.125L17.425 2.475q.275-.275.638-.425t.762-.15H21q.425 0 .713.288T22 2.9v2.575q0 .2-.075.388T21.7 6.2M6.225 10.125l-3.65-3.65Q2.3 6.2 2.15 5.838T2 5.075V2.9q0-.425.288-.712T3 1.9h2.175q.4 0 .763.15t.637.425l3.65 3.65q.3.3.3.713t-.3.712L7.65 10.125q-.3.3-.712.3t-.713-.3\" /></svg>         ");
-document.writeln("   自定义吐球");
-document.writeln(" ");
-document.writeln("");
-document.writeln("");
-document.writeln("                    <label class=\"switch\">");
-document.writeln("                        <input type=\"checkbox\" onclick=\"tuqiu(this)\">");
-document.writeln("                        <span class=\"slider\"></span>");
-document.writeln("");
-document.writeln("                    </label>");
-document.writeln("                </button>");
-document.writeln("    <audio id=\"audio\" src=\"https://downsc.chinaz.net/Files/DownLoad/sound1/202005/12862.wav\"></audio>");
+_0x43ec12();
+function playSound() {
+  var _0x51841b = document.getElementById("audio");
+  if (_0x51841b.paused) {
+    {
+      _0x51841b.load;
+      _0x51841b.play();
+    }
+  }
+}
+window.onclick = function () {
+  playSound();
+};
