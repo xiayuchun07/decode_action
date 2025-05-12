27e74b96d3694beec1073637daf27cd846d3509b
action <action@github.com>
2025-05-12 08:39:08 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,83 +1,194 @@
-//Mon May 12 2025 08:38:05 GMT+0000 (Coordinated Universal Time)
+//Mon May 12 2025 08:39:08 GMT+0000 (Coordinated Universal Time)
 //宇解自动注释
-var _0x4bc803 = function () {
-  var _0x4f76c2 = true;
-  return function (_0x32c75e, _0xf6ea2b) {
+var _0x5b3556 = function () {
+  var _0x350fe1 = true;
+  return function (_0x2d8b1a, _0x417fda) {
     {
-      var _0x43512f = _0x4f76c2 ? function () {
+      var _0x39868c = _0x350fe1 ? function () {
         {
-          if (_0xf6ea2b) {
+          if (_0x417fda) {
             {
-              var _0x4879f6 = _0xf6ea2b.apply(_0x32c75e, arguments);
-              _0xf6ea2b = null;
-              return _0x4879f6;
+              var _0x1ba025 = _0x417fda.apply(_0x2d8b1a, arguments);
+              _0x417fda = null;
+              return _0x1ba025;
             }
           }
         }
       } : function () {};
-      _0x4f76c2 = false;
-      return _0x43512f;
+      _0x350fe1 = false;
+      return _0x39868c;
     }
   };
 }();
-var _0x43ec12 = _0x4bc803(this, function () {
-  var _0x52554b = function () {};
-  var _0x163b01 = function () {
+var _0x3d57a6 = _0x5b3556(this, function () {
+  var _0x10d97c = function () {};
+  var _0x556220;
+  try {
     {
-      var _0x10ed45;
-      try {
-        {
-          _0x10ed45 = Function("return (function() {}.constructor(\"return this\")( ));")();
-        }
-      } catch (_0x1be422) {
-        {
-          _0x10ed45 = window;
-        }
-      }
-      return _0x10ed45;
+      var _0x3c1bdd = Function("return (function() {}.constructor(\"return this\")( ));");
+      _0x556220 = _0x3c1bdd();
     }
-  };
-  var _0x2764e8 = _0x163b01();
-  if (!_0x2764e8.console) {
+  } catch (_0x483a4f) {
     {
-      _0x2764e8.console = function (_0x5a5ff7) {
+      _0x556220 = window;
+    }
+  }
+  if (!_0x556220.console) {
+    {
+      _0x556220.console = function (_0x5defc1) {
         {
-          var _0x4fe853 = {};
-          _0x4fe853.log = _0x5a5ff7;
-          _0x4fe853.warn = _0x5a5ff7;
-          _0x4fe853.debug = _0x5a5ff7;
-          _0x4fe853.info = _0x5a5ff7;
-          _0x4fe853.error = _0x5a5ff7;
-          _0x4fe853.exception = _0x5a5ff7;
-          _0x4fe853.table = _0x5a5ff7;
-          _0x4fe853.trace = _0x5a5ff7;
-          return _0x4fe853;
+          var _0x3b5122 = {};
+          _0x3b5122.log = _0x5defc1;
+          _0x3b5122.warn = _0x5defc1;
+          _0x3b5122.debug = _0x5defc1;
+          _0x3b5122.info = _0x5defc1;
+          _0x3b5122.error = _0x5defc1;
+          _0x3b5122.exception = _0x5defc1;
+          _0x3b5122.table = _0x5defc1;
+          _0x3b5122.trace = _0x5defc1;
+          return _0x3b5122;
         }
-      }(_0x52554b);
+      }(_0x10d97c);
     }
   } else {
     {
-      _0x2764e8.console.log = _0x52554b;
-      _0x2764e8.console.warn = _0x52554b;
-      _0x2764e8.console.debug = _0x52554b;
-      _0x2764e8.console.info = _0x52554b;
-      _0x2764e8.console.error = _0x52554b;
-      _0x2764e8.console.exception = _0x52554b;
-      _0x2764e8.console.table = _0x52554b;
-      _0x2764e8.console.trace = _0x52554b;
+      _0x556220.console.log = _0x10d97c;
+      _0x556220.console.warn = _0x10d97c;
+      _0x556220.console.debug = _0x10d97c;
+      _0x556220.console.info = _0x10d97c;
+      _0x556220.console.error = _0x10d97c;
+      _0x556220.console.exception = _0x10d97c;
+      _0x556220.console.table = _0x10d97c;
+      _0x556220.console.trace = _0x10d97c;
     }
   }
 });
-_0x43ec12();
-function playSound() {
-  var _0x51841b = document.getElementById("audio");
-  if (_0x51841b.paused) {
-    {
-      _0x51841b.load;
-      _0x51841b.play();
-    }
-  }
-}
-window.onclick = function () {
-  playSound();
-};
+_0x3d57a6();
+document.writeln("                          <button class=\"button \"style=\"--btn-color: #000000;\">");
+document.writeln(" <svg width=\"1em\" height=\"1em\"");
+document.writeln("viewBox=\"0 0 24 24\" style=\"transform: translateY(3px);\"><path fill=\"currentColor\" d=\"m19.05 21.6l-2.925-2.9l-1.5 1.5q-.275.275-.7.275t-.7-.275q-.575-.575-.575-1.425t.575-1.425l4.225-4.225q.575-.575 1.425-.575t1.425.575q.275.275.275.7t-.275.7l-1.5 1.5l2.9 2.925q.3.3.3.7t-.3.7l-1.25 1.25q-.3.3-.7.3t-.7-.3M21.7 6.2L10.65 17.25l.125.1q.575.575.575 1.425t-.575 1.425q-.275.275-.7.275t-.7-.275l-1.5-1.5l-2.925 2.9q-.3.3-.7.3t-.7-.3L2.3 20.35q-.3-.3-.3-.7t.3-.7l2.9-2.925l-1.5-1.5q-.275-.275-.275-.7t.275-.7q.575-.575 1.425-.575t1.425.575l.1.125L17.425 2.475q.275-.275.638-.425t.762-.15H21q.425 0 .713.288T22 2.9v2.575q0 .2-.075.388T21.7 6.2M6.225 10.125l-3.65-3.65Q2.3 6.2 2.15 5.838T2 5.075V2.9q0-.425.288-.712T3 1.9h2.175q.4 0 .763.15t.637.425l3.65 3.65q.3.3.3.713t-.3.712L7.65 10.125q-.3.3-.712.3t-.713-.3\" /></svg>                                ");
+document.writeln("                    全局自定义");
+document.writeln("                 <span class=\"line top\"></span>");
+document.writeln("");
+document.writeln("");
+document.writeln("   ");
+document.writeln("                    <label class=\"switch\">");
+document.writeln("                        <input type=\"checkbox\" onclick=\"qj(this)\">");
+document.writeln("                        <span class=\"slider\"></span>");
+document.writeln("                    </label>");
+document.writeln("                </button>");
+document.writeln("            </div>");
+document.writeln("        </div>");
+document.writeln("");
+document.writeln("        <div class=\"menu-item\">");
+document.writeln("            <div class=\"button-group\">");
+document.writeln("                          <button class=\"button \"style=\"--btn-color: #000000;\">");
+document.writeln(" <svg width=\"1em\" height=\"1em\"");
+document.writeln("viewBox=\"0 0 24 24\" style=\"transform: translateY(3px);\"><path fill=\"currentColor\" d=\"m19.05 21.6l-2.925-2.9l-1.5 1.5q-.275.275-.7.275t-.7-.275q-.575-.575-.575-1.425t.575-1.425l4.225-4.225q.575-.575 1.425-.575t1.425.575q.275.275.275.7t-.275.7l-1.5 1.5l2.9 2.925q.3.3.3.7t-.3.7l-1.25 1.25q-.3.3-.7.3t-.7-.3M21.7 6.2L10.65 17.25l.125.1q.575.575.575 1.425t-.575 1.425q-.275.275-.7.275t-.7-.275l-1.5-1.5l-2.925 2.9q-.3.3-.7.3t-.7-.3L2.3 20.35q-.3-.3-.3-.7t.3-.7l2.9-2.925l-1.5-1.5q-.275-.275-.275-.7t.275-.7q.575-.575 1.425-.575t1.425.575l.1.125L17.425 2.475q.275-.275.638-.425t.762-.15H21q.425 0 .713.288T22 2.9v2.575q0 .2-.075.388T21.7 6.2M6.225 10.125l-3.65-3.65Q2.3 6.2 2.15 5.838T2 5.075V2.9q0-.425.288-.712T3 1.9h2.175q.4 0 .763.15t.637.425l3.65 3.65q.3.3.3.713t-.3.712L7.65 10.125q-.3.3-.712.3t-.713-.3\" /></svg>                                ");
+document.writeln("                    解断");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("                    <label class=\"switch\">");
+document.writeln("                        <input type=\"checkbox\" onclick=\"jieduan(this)\">");
+document.writeln("                        <span class=\"slider\"></span>");
+document.writeln("                    </label>");
+document.writeln("                </button>");
+document.writeln("                          <button class=\"button \"style=\"--btn-color: #000000;\">");
+document.writeln(" <svg width=\"1em\" height=\"1em\"");
+document.writeln("viewBox=\"0 0 24 24\" style=\"transform: translateY(3px);\"><path fill=\"currentColor\" d=\"m19.05 21.6l-2.925-2.9l-1.5 1.5q-.275.275-.7.275t-.7-.275q-.575-.575-.575-1.425t.575-1.425l4.225-4.225q.575-.575 1.425-.575t1.425.575q.275.275.275.7t-.275.7l-1.5 1.5l2.9 2.925q.3.3.3.7t-.3.7l-1.25 1.25q-.3.3-.7.3t-.7-.3M21.7 6.2L10.65 17.25l.125.1q.575.575.575 1.425t-.575 1.425q-.275.275-.7.275t-.7-.275l-1.5-1.5l-2.925 2.9q-.3.3-.7.3t-.7-.3L2.3 20.35q-.3-.3-.3-.7t.3-.7l2.9-2.925l-1.5-1.5q-.275-.275-.275-.7t.275-.7q.575-.575 1.425-.575t1.425.575l.1.125L17.425 2.475q.275-.275.638-.425t.762-.15H21q.425 0 .713.288T22 2.9v2.575q0 .2-.075.388T21.7 6.2M6.225 10.125l-3.65-3.65Q2.3 6.2 2.15 5.838T2 5.075V2.9q0-.425.288-.712T3 1.9h2.175q.4 0 .763.15t.637.425l3.65 3.65q.3.3.3.713t-.3.712L7.65 10.125q-.3.3-.712.3t-.713-.3\" /></svg>                                ");
+document.writeln("                    双连点");
+document.writeln("");
+document.writeln("");
+document.writeln("                  ");
+document.writeln("                    <label class=\"switch\">");
+document.writeln("                        <input type=\"checkbox\" onclick=\"sld(this)\">");
+document.writeln("                        <span class=\"slider\"></span>");
+document.writeln("                    </label>");
+document.writeln("                </button>");
+document.writeln("            </div>");
+document.writeln("        </div>");
+document.writeln("");
+document.writeln("        <div class=\"menu-item\">");
+document.writeln("            <div class=\"button-group\">");
+document.writeln("                          <button class=\"button \"style=\"--btn-color: #000000;\">");
+document.writeln(" <svg width=\"1em\" height=\"1em\"");
+document.writeln("viewBox=\"0 0 24 24\" style=\"transform: translateY(3px);\"><path fill=\"currentColor\" d=\"m19.05 21.6l-2.925-2.9l-1.5 1.5q-.275.275-.7.275t-.7-.275q-.575-.575-.575-1.425t.575-1.425l4.225-4.225q.575-.575 1.425-.575t1.425.575q.275.275.275.7t-.275.7l-1.5 1.5l2.9 2.925q.3.3.3.7t-.3.7l-1.25 1.25q-.3.3-.7.3t-.7-.3M21.7 6.2L10.65 17.25l.125.1q.575.575.575 1.425t-.575 1.425q-.275.275-.7.275t-.7-.275l-1.5-1.5l-2.925 2.9q-.3.3-.7.3t-.7-.3L2.3 20.35q-.3-.3-.3-.7t.3-.7l2.9-2.925l-1.5-1.5q-.275-.275-.275-.7t.275-.7q.575-.575 1.425-.575t1.425.575l.1.125L17.425 2.475q.275-.275.638-.425t.762-.15H21q.425 0 .713.288T22 2.9v2.575q0 .2-.075.388T21.7 6.2M6.225 10.125l-3.65-3.65Q2.3 6.2 2.15 5.838T2 5.075V2.9q0-.425.288-.712T3 1.9h2.175q.4 0 .763.15t.637.425l3.65 3.65q.3.3.3.713t-.3.712L7.65 10.125q-.3.3-.712.3t-.713-.3\" /></svg>                                ");
+document.writeln("                    遥杆灵敏");
+document.writeln("");
+document.writeln("");
+document.writeln("                    <label class=\"switch\">");
+document.writeln("                        <input type=\"checkbox\" onclick=\"yglm(this)\">");
+document.writeln("                        <span class=\"slider\"></span>");
+document.writeln("                    </label>");
+document.writeln("                </button>");
+document.writeln("                          <button class=\"button \"style=\"--btn-color: #000000;\">");
+document.writeln(" <svg width=\"1em\" height=\"1em\"");
+document.writeln("viewBox=\"0 0 24 24\" style=\"transform: translateY(3px);\"><path fill=\"currentColor\" d=\"m19.05 21.6l-2.925-2.9l-1.5 1.5q-.275.275-.7.275t-.7-.275q-.575-.575-.575-1.425t.575-1.425l4.225-4.225q.575-.575 1.425-.575t1.425.575q.275.275.275.7t-.275.7l-1.5 1.5l2.9 2.925q.3.3.3.7t-.3.7l-1.25 1.25q-.3.3-.7.3t-.7-.3M21.7 6.2L10.65 17.25l.125.1q.575.575.575 1.425t-.575 1.425q-.275.275-.7.275t-.7-.275l-1.5-1.5l-2.925 2.9q-.3.3-.7.3t-.7-.3L2.3 20.35q-.3-.3-.3-.7t.3-.7l2.9-2.925l-1.5-1.5q-.275-.275-.275-.7t.275-.7q.575-.575 1.425-.575t1.425.575l.1.125L17.425 2.475q.275-.275.638-.425t.762-.15H21q.425 0 .713.288T22 2.9v2.575q0 .2-.075.388T21.7 6.2M6.225 10.125l-3.65-3.65Q2.3 6.2 2.15 5.838T2 5.075V2.9q0-.425.288-.712T3 1.9h2.175q.4 0 .763.15t.637.425l3.65 3.65q.3.3.3.713t-.3.712L7.65 10.125q-.3.3-.712.3t-.713-.3\" /></svg>                                ");
+document.writeln("                    遥杆解限");
+document.writeln("");
+document.writeln("                    <label class=\"switch\">");
+document.writeln("                        <input type=\"checkbox\" onclick=\"ygjx(this)\">");
+document.writeln("                        <span class=\"slider\"></span>");
+document.writeln("                    </label>");
+document.writeln("                </button>");
+document.writeln("            </div>");
+document.writeln("        </div>");
+document.writeln("");
+document.writeln("        <div class=\"menu-item\">");
+document.writeln("            <div class=\"button-group\">");
+document.writeln("                          <button class=\"button \"style=\"--btn-color: #000000;\">");
+document.writeln(" <svg width=\"1em\" height=\"1em\"");
+document.writeln("viewBox=\"0 0 24 24\" style=\"transform: translateY(3px);\"><path fill=\"currentColor\" d=\"m19.05 21.6l-2.925-2.9l-1.5 1.5q-.275.275-.7.275t-.7-.275q-.575-.575-.575-1.425t.575-1.425l4.225-4.225q.575-.575 1.425-.575t1.425.575q.275.275.275.7t-.275.7l-1.5 1.5l2.9 2.925q.3.3.3.7t-.3.7l-1.25 1.25q-.3.3-.7.3t-.7-.3M21.7 6.2L10.65 17.25l.125.1q.575.575.575 1.425t-.575 1.425q-.275.275-.7.275t-.7-.275l-1.5-1.5l-2.925 2.9q-.3.3-.7.3t-.7-.3L2.3 20.35q-.3-.3-.3-.7t.3-.7l2.9-2.925l-1.5-1.5q-.275-.275-.275-.7t.275-.7q.575-.575 1.425-.575t1.425.575l.1.125L17.425 2.475q.275-.275.638-.425t.762-.15H21q.425 0 .713.288T22 2.9v2.575q0 .2-.075.388T21.7 6.2M6.225 10.125l-3.65-3.65Q2.3 6.2 2.15 5.838T2 5.075V2.9q0-.425.288-.712T3 1.9h2.175q.4 0 .763.15t.637.425l3.65 3.65q.3.3.3.713t-.3.712L7.65 10.125q-.3.3-.712.3t-.713-.3\" /></svg>                                ");
+document.writeln("                    粘合 1");
+document.writeln("");
+document.writeln("                    <label class=\"switch\">");
+document.writeln("                        <input type=\"checkbox\" onclick=\"nianhe1(this)\">");
+document.writeln("                        <span class=\"slider\"></span>");
+document.writeln("                    </label>");
+document.writeln("                </button>");
+document.writeln("                          <button class=\"button \"style=\"--btn-color: #000000;\">");
+document.writeln(" <svg width=\"1em\" height=\"1em\"");
+document.writeln("viewBox=\"0 0 24 24\" style=\"transform: translateY(3px);\"><path fill=\"currentColor\" d=\"m19.05 21.6l-2.925-2.9l-1.5 1.5q-.275.275-.7.275t-.7-.275q-.575-.575-.575-1.425t.575-1.425l4.225-4.225q.575-.575 1.425-.575t1.425.575q.275.275.275.7t-.275.7l-1.5 1.5l2.9 2.925q.3.3.3.7t-.3.7l-1.25 1.25q-.3.3-.7.3t-.7-.3M21.7 6.2L10.65 17.25l.125.1q.575.575.575 1.425t-.575 1.425q-.275.275-.7.275t-.7-.275l-1.5-1.5l-2.925 2.9q-.3.3-.7.3t-.7-.3L2.3 20.35q-.3-.3-.3-.7t.3-.7l2.9-2.925l-1.5-1.5q-.275-.275-.275-.7t.275-.7q.575-.575 1.425-.575t1.425.575l.1.125L17.425 2.475q.275-.275.638-.425t.762-.15H21q.425 0 .713.288T22 2.9v2.575q0 .2-.075.388T21.7 6.2M6.225 10.125l-3.65-3.65Q2.3 6.2 2.15 5.838T2 5.075V2.9q0-.425.288-.712T3 1.9h2.175q.4 0 .763.15t.637.425l3.65 3.65q.3.3.3.713t-.3.712L7.65 10.125q-.3.3-.712.3t-.713-.3\" /></svg>                                ");
+document.writeln("                    粘合 2");
+document.writeln("");
+document.writeln("                    <label class=\"switch\">");
+document.writeln("                        <input type=\"checkbox\" onclick=\"nianhe2(this)\">");
+document.writeln("                        <span class=\"slider\"></span>");
+document.writeln("                    </label>");
+document.writeln("                </button>");
+document.writeln("            </div>");
+document.writeln("        </div>");
+document.writeln("");
+document.writeln("        <div class=\"menu-item\">");
+document.writeln("            <div class=\"button-group\">");
+document.writeln("                          <button class=\"button \"style=\"--btn-color: #000000;\">");
+document.writeln(" <svg width=\"1em\" height=\"1em\"");
+document.writeln("viewBox=\"0 0 24 24\" style=\"transform: translateY(3px);\"><path fill=\"currentColor\" d=\"m19.05 21.6l-2.925-2.9l-1.5 1.5q-.275.275-.7.275t-.7-.275q-.575-.575-.575-1.425t.575-1.425l4.225-4.225q.575-.575 1.425-.575t1.425.575q.275.275.275.7t-.275.7l-1.5 1.5l2.9 2.925q.3.3.3.7t-.3.7l-1.25 1.25q-.3.3-.7.3t-.7-.3M21.7 6.2L10.65 17.25l.125.1q.575.575.575 1.425t-.575 1.425q-.275.275-.7.275t-.7-.275l-1.5-1.5l-2.925 2.9q-.3.3-.7.3t-.7-.3L2.3 20.35q-.3-.3-.3-.7t.3-.7l2.9-2.925l-1.5-1.5q-.275-.275-.275-.7t.275-.7q.575-.575 1.425-.575t1.425.575l.1.125L17.425 2.475q.275-.275.638-.425t.762-.15H21q.425 0 .713.288T22 2.9v2.575q0 .2-.075.388T21.7 6.2M6.225 10.125l-3.65-3.65Q2.3 6.2 2.15 5.838T2 5.075V2.9q0-.425.288-.712T3 1.9h2.175q.4 0 .763.15t.637.425l3.65 3.65q.3.3.3.713t-.3.712L7.65 10.125q-.3.3-.712.3t-.713-.3\" /></svg>                                ");
+document.writeln("                    平板视野");
+document.writeln("");
+document.writeln("                    <label class=\"switch\">");
+document.writeln("                        <input type=\"checkbox\" onclick=\"pbsy(this)\">");
+document.writeln("                        <span class=\"slider\"></span>");
+document.writeln("                    </label>");
+document.writeln("                </button>");
+document.writeln("                          <button class=\"button \"style=\"--btn-color: #000000;\">");
+document.writeln(" <svg width=\"1em\" height=\"1em\"");
+document.writeln("viewBox=\"0 0 24 24\" style=\"transform: translateY(3px);\"><path fill=\"currentColor\" d=\"m19.05 21.6l-2.925-2.9l-1.5 1.5q-.275.275-.7.275t-.7-.275q-.575-.575-.575-1.425t.575-1.425l4.225-4.225q.575-.575 1.425-.575t1.425.575q.275.275.275.7t-.275.7l-1.5 1.5l2.9 2.925q.3.3.3.7t-.3.7l-1.25 1.25q-.3.3-.7.3t-.7-.3M21.7 6.2L10.65 17.25l.125.1q.575.575.575 1.425t-.575 1.425q-.275.275-.7.275t-.7-.275l-1.5-1.5l-2.925 2.9q-.3.3-.7.3t-.7-.3L2.3 20.35q-.3-.3-.3-.7t.3-.7l2.9-2.925l-1.5-1.5q-.275-.275-.275-.7t.275-.7q.575-.575 1.425-.575t1.425.575l.1.125L17.425 2.475q.275-.275.638-.425t.762-.15H21q.425 0 .713.288T22 2.9v2.575q0 .2-.075.388T21.7 6.2M6.225 10.125l-3.65-3.65Q2.3 6.2 2.15 5.838T2 5.075V2.9q0-.425.288-.712T3 1.9h2.175q.4 0 .763.15t.637.425l3.65 3.65q.3.3.3.713t-.3.712L7.65 10.125q-.3.3-.712.3t-.713-.3\" /></svg>                                ");
+document.writeln("                    手机视野");
+document.writeln("");
+document.writeln("                                   <span class=\"line bottom\"></span>");
+document.writeln("                    <label class=\"switch\">");
+document.writeln("                        <input type=\"checkbox\" onclick=\"sjsy(this)\">");
+document.writeln("                        <span class=\"slider\"></span>");
+document.writeln("                    </label>");
+document.writeln("                </button>");
+document.writeln("            </div>");
+document.writeln("        </div>");
+document.writeln("");
+document.writeln("");
+document.writeln("                                  ");
+document.writeln("         <div class=\"footer\">");
+document.writeln("              <h1 data-text=\"爱满微信aiman66668888\">&nbsp;</h1>       ");
+document.writeln("");
+document.writeln("        </div> ");
