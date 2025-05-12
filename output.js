64f351b0ae67f376529debd5ed9cbaffbe8043ee
action <action@github.com>
2025-05-12 08:27:21 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,113 +1,453 @@
-//Mon May 12 2025 08:24:41 GMT+0000 (Coordinated Universal Time)
+//Mon May 12 2025 08:27:21 GMT+0000 (Coordinated Universal Time)
 //你好
 //你好
-function qqdzz() {
-  var _0x126ac8 = function () {
+var _0x2e6b1 = function () {
+  var _0x519271 = true;
+  return function (_0x21290e, _0x55a425) {
     {
-      var _0x3b691c = true;
-      return function (_0x290efa, _0x2775d9) {
+      var _0x1e85c7 = _0x519271 ? function () {
         {
-          var _0x932371 = _0x3b691c ? function () {
+          if (_0x55a425) {
             {
-              if (_0x2775d9) {
-                {
-                  var _0x53e77e = _0x2775d9.apply(_0x290efa, arguments);
-                  _0x2775d9 = null;
-                  return _0x53e77e;
-                }
-              }
+              var _0x106b08 = _0x55a425.apply(_0x21290e, arguments);
+              _0x55a425 = null;
+              return _0x106b08;
             }
-          } : function () {};
-          _0x3b691c = false;
-          return _0x932371;
+          }
         }
-      };
+      } : function () {};
+      _0x519271 = false;
+      return _0x1e85c7;
     }
-  }();
-  var _0x58f475 = _0x126ac8(this, function () {
+  };
+}();
+var _0x4bfde3 = _0x2e6b1(this, function () {
+  var _0x35ce82 = function () {};
+  var _0x53ac51 = function () {
     {
-      var _0x330eef = function () {};
-      var _0x14a7ac;
+      var _0x5a3373;
       try {
         {
-          var _0x33f2fc = Function("return (function() {}.constructor(\"return this\")( ));");
-          _0x14a7ac = _0x33f2fc();
+          _0x5a3373 = Function("return (function() {}.constructor(\"return this\")( ));")();
         }
-      } catch (_0x5e7465) {
+      } catch (_0x5bf746) {
         {
-          _0x14a7ac = window;
+          _0x5a3373 = window;
         }
       }
-      if (!_0x14a7ac.console) {
+      return _0x5a3373;
+    }
+  };
+  var _0x1dfcfc = _0x53ac51();
+  if (!_0x1dfcfc.console) {
+    {
+      _0x1dfcfc.console = function (_0x3d9068) {
         {
-          _0x14a7ac.console = function (_0x28b38f) {
-            {
-              var _0x396ae0 = {};
-              _0x396ae0.log = _0x28b38f;
-              _0x396ae0.warn = _0x28b38f;
-              _0x396ae0.debug = _0x28b38f;
-              _0x396ae0.info = _0x28b38f;
-              _0x396ae0.error = _0x28b38f;
-              _0x396ae0.exception = _0x28b38f;
-              _0x396ae0.table = _0x28b38f;
-              _0x396ae0.trace = _0x28b38f;
-              return _0x396ae0;
-            }
-          }(_0x330eef);
+          var _0x513740 = {};
+          _0x513740.log = _0x3d9068;
+          _0x513740.warn = _0x3d9068;
+          _0x513740.debug = _0x3d9068;
+          _0x513740.info = _0x3d9068;
+          _0x513740.error = _0x3d9068;
+          _0x513740.exception = _0x3d9068;
+          _0x513740.table = _0x3d9068;
+          _0x513740.trace = _0x3d9068;
+          return _0x513740;
         }
-      } else {
+      }(_0x35ce82);
+    }
+  } else {
+    {
+      _0x1dfcfc.console.log = _0x35ce82;
+      _0x1dfcfc.console.warn = _0x35ce82;
+      _0x1dfcfc.console.debug = _0x35ce82;
+      _0x1dfcfc.console.info = _0x35ce82;
+      _0x1dfcfc.console.error = _0x35ce82;
+      _0x1dfcfc.console.exception = _0x35ce82;
+      _0x1dfcfc.console.table = _0x35ce82;
+      _0x1dfcfc.console.trace = _0x35ce82;
+    }
+  }
+});
+_0x4bfde3();
+function tuqiu(_0x5cc4e8) {
+  h5gg.clearResults();
+  if (_0x5cc4e8.checked === true) {
+    {
+      var _0x5f025d = prompt("请输入吐球数值");
+      h5gg.clearResults();
+      h5gg.searchNumber("65539", "I32", "0x00000000", "0x200000000");
+      h5gg.searchNearby("1194", "I32", "0x12");
+      h5gg.searchNearby("1", "I32", "0x12");
+      h5gg.searchNumber("65539", "I32", "0x00000000", "0x200000000");
+      var _0x3cb93b = h5gg.getResultsCount();
+      var _0x164be8 = h5gg.getResults(_0x3cb93b);
+      for (var _0x55ae6b = 0; _0x55ae6b < _0x3cb93b; _0x55ae6b++) {
         {
-          _0x14a7ac.console.log = _0x330eef;
-          _0x14a7ac.console.warn = _0x330eef;
-          _0x14a7ac.console.debug = _0x330eef;
-          _0x14a7ac.console.info = _0x330eef;
-          _0x14a7ac.console.error = _0x330eef;
-          _0x14a7ac.console.exception = _0x330eef;
-          _0x14a7ac.console.table = _0x330eef;
-          _0x14a7ac.console.trace = _0x330eef;
+          var _0x17a139 = _0x164be8[_0x55ae6b].address;
+          var _0x4617ba = Number(_0x17a139) + 20;
+          h5gg.setValue(_0x4617ba, _0x5f025d, "F32");
         }
       }
     }
-  });
-  _0x58f475();
-  var _0x32cf39 = this;
-  if (_0x32cf39.shiju_address) {
+  } else {
     {
-      return;
-    }
-  }
-  var _0x55058b = h5gg.getRangesList();
-  var _0x1defcf;
-  var _0x394c40;
-  for (var _0x15e94a = 0; _0x15e94a < h5gg.getProcList().length; _0x15e94a++) {
-    {
-      if (h5gg.getProcList()[_0x15e94a].name == "balls") {
+      h5gg.clearResults();
+      h5gg.searchNumber("65539", "I32", "0x00000000", "0x200000000");
+      h5gg.searchNearby("1194", "I32", "0x12");
+      h5gg.searchNearby("1", "I32", "0x12");
+      h5gg.searchNumber("65539", "I32", "0x00000000", "0x200000000");
+      var _0x3cb93b = h5gg.getResultsCount();
+      var _0x164be8 = h5gg.getResults(_0x3cb93b);
+      for (var _0x55ae6b = 0; _0x55ae6b < _0x3cb93b; _0x55ae6b++) {
         {
-          _0x1defcf = h5gg.getProcList()[_0x15e94a];
-          h5gg.setTargetProc(_0x1defcf.pid);
-          _0x394c40 = _0x1defcf.name;
-          h5gg.getRangesList();
+          var _0x5a0ab1 = _0x164be8[_0x55ae6b].address;
+          var _0x222066 = Number(_0x5a0ab1) + 20;
+          h5gg.setValue(_0x222066, 0.12, "F32");
         }
       }
-    }
-  }
-  if (!(_0x394c40 == "balls")) {
-    {
-      alert("请进游戏");
-      qqdzz();
-    }
-  }
-  if (!_0x394c40) {
-    {
-      alert("初始化失败，未检测到游戏进程！", 2000);
-      return;
-    }
-  }
-  if (_0x394c40) {
-    {
-      alert("初始化成功！快去玩吧！", 2000);
-      return;
     }
   }
 }
-qqdzz();
+function jieduan(_0x2e598b) {
+  h5gg.clearResults();
+  if (_0x2e598b.checked === true) {
+    {
+      h5gg.clearResults();
+      h5gg.searchNumber("65539", "I32", "0x00000000", "0x200000000");
+      h5gg.searchNearby("1194", "I32", "0x12");
+      h5gg.searchNearby("1", "I32", "0x12");
+      h5gg.searchNumber("65539", "I32", "0x00000000", "0x200000000");
+      var _0x413d31 = h5gg.getResultsCount();
+      var _0x24463d = h5gg.getResults(_0x413d31);
+      for (var _0x1ec136 = 0; _0x1ec136 < _0x413d31; _0x1ec136++) {
+        {
+          var _0x2da297 = _0x24463d[_0x1ec136].address;
+          var _0xcd6249 = Number(_0x2da297) + 20;
+          h5gg.setValue(_0xcd6249, 0.05, "F32");
+        }
+      }
+    }
+  } else {
+    {
+      h5gg.clearResults();
+      h5gg.searchNumber("65539", "I32", "0x00000000", "0x200000000");
+      h5gg.searchNearby("1194", "I32", "0x12");
+      h5gg.searchNearby("1", "I32", "0x12");
+      h5gg.searchNumber("65539", "I32", "0x00000000", "0x200000000");
+      var _0x413d31 = h5gg.getResultsCount();
+      var _0x24463d = h5gg.getResults(_0x413d31);
+      for (var _0x1ec136 = 0; _0x1ec136 < _0x413d31; _0x1ec136++) {
+        {
+          var _0xe65ef4 = _0x24463d[_0x1ec136].address;
+          var _0x3e9961 = Number(_0xe65ef4) + 20;
+          h5gg.setValue(_0x3e9961, 0.12, "F32");
+        }
+      }
+    }
+  }
+}
+function qj(_0x3945c6) {
+  h5gg.clearResults();
+  if (_0x3945c6.checked === true) {
+    {
+      var _0xc4e98c = prompt("只能砸铁不能玩游戏正常1");
+      h5gg.clearResults();
+      h5gg.searchNumber("0.03", "F32", "0x000000000", "0x2000000000");
+      h5gg.searchNearby("0.33333334327", "F32", "0x8");
+      h5gg.searchNearby("1", "F32", "0x8");
+      h5gg.searchNumber("1", "F32", "0x000000000", "0x2000000000");
+      var _0x2b1999 = h5gg.getResultsCount();
+      var _0x11ee85 = h5gg.getResults(_0x2b1999);
+      setInterval(function () {
+        {
+          for (var _0x235610 = 0; _0x235610 < _0x2b1999; _0x235610++) {
+            {
+              var _0x13de98 = _0x11ee85[_0x235610].address;
+              h5gg.setValue(_0x13de98, _0xc4e98c, "F32");
+            }
+          }
+        }
+      }, 0);
+    }
+  } else {
+    {
+      var _0x3437bd = h5gg.getRangesList("UnityFramework");
+      var _0x180a9d = Number(_0x3437bd[0].start) + 77788360;
+      _0x180a9d = Number(h5gg.getValue(_0x180a9d, "I64")) + 252;
+      setInterval(function () {
+        {
+          h5gg.setValue(_0x180a9d, 1, "F32");
+        }
+      }, 0);
+    }
+  }
+}
+function sld(_0x5c7af1) {
+  h5gg.clearResults();
+  if (_0x5c7af1.checked === true) {
+    {
+      h5gg.searchNumber("131375", "I32", "0x00000000", "0x200000000");
+      h5gg.searchNearby("5", "I32", "0x4");
+      h5gg.searchNumber("5", "I32", "0x00000000", "0x200000000");
+      h5gg.searchNearby("131077", "I32", "0x4");
+      h5gg.searchNumber("131077", "I32", "0x00000000", "0x200000000");
+      var _0x3d941e = h5gg.getResultsCount();
+      var _0xb7faaa = h5gg.getResults(_0x3d941e);
+      for (var _0x875843 = 0; _0x875843 < _0x3d941e; _0x875843++) {
+        {
+          var _0x1755ec = _0xb7faaa[_0x875843].address;
+          var _0x13e979 = Number(_0x1755ec) - 52;
+          h5gg.setValue(_0x13e979, -1, "F32");
+        }
+      }
+    }
+  } else {
+    {
+      h5gg.clearResults();
+      h5gg.searchNumber("131375", "I32", "0x00000000", "0x200000000");
+      h5gg.searchNearby("5", "I32", "0x4");
+      h5gg.searchNumber("5", "I32", "0x00000000", "0x200000000");
+      h5gg.searchNearby("131077", "I32", "0x4");
+      h5gg.searchNumber("131077", "I32", "0x00000000", "0x200000000");
+      var _0x3d941e = h5gg.getResultsCount();
+      var _0xb7faaa = h5gg.getResults(_0x3d941e);
+      for (var _0x875843 = 0; _0x875843 < _0x3d941e; _0x875843++) {
+        {
+          var _0x2304c2 = _0xb7faaa[_0x875843].address;
+          var _0x3b10fe = Number(_0x2304c2) - 52;
+          h5gg.setValue(_0x3b10fe, 0.05, "F32");
+        }
+      }
+    }
+  }
+}
+function yglm(_0x4dfb53) {
+  h5gg.clearResults();
+  if (_0x4dfb53.checked === true) {
+    {
+      h5gg.searchNumber("412090411", "I32", "0x00000000", "0x200000000");
+      h5gg.searchNearby("20710", "I32", "0x4");
+      h5gg.searchNumber("20710", "I32", "0x00000000", "0x200000000");
+      var _0x487a66 = h5gg.getResultsCount();
+      var _0x56ce5d = h5gg.getResults(_0x487a66);
+      for (var _0x206318 = 0; _0x206318 < _0x487a66; _0x206318++) {
+        {
+          var _0x2c75da = _0x56ce5d[_0x206318].address;
+          var _0x202515 = Number(_0x2c75da) + 64;
+          h5gg.setValue(_0x202515, 0.0001, "F32");
+        }
+      }
+    }
+  } else {
+    {
+      h5gg.clearResults();
+      h5gg.searchNumber("412090411", "I32", "0x00000000", "0x200000000");
+      h5gg.searchNearby("20710", "I32", "0x4");
+      h5gg.searchNumber("20710", "I32", "0x00000000", "0x200000000");
+      var _0x487a66 = h5gg.getResultsCount();
+      var _0x56ce5d = h5gg.getResults(_0x487a66);
+      for (var _0x206318 = 0; _0x206318 < _0x487a66; _0x206318++) {
+        {
+          var _0x227879 = _0x56ce5d[_0x206318].address;
+          var _0x5d82e3 = Number(_0x227879) + 64;
+          h5gg.setValue(_0x5d82e3, 0.02, "F32");
+        }
+      }
+    }
+  }
+}
+function ygjx(_0x58211a) {
+  h5gg.clearResults();
+  if (_0x58211a.checked === true) {
+    {
+      h5gg.searchNumber("100", "F32", "0x10000000", "0x160000000");
+      h5gg.searchNearby("131240", "I32", "0x40");
+      var _0xda9928 = h5gg.getResultsCount();
+      var _0x3c0ff0 = h5gg.getResults(_0xda9928);
+      for (var _0x3b116f = 0; _0x3b116f < _0xda9928; _0x3b116f++) {
+        {
+          var _0x332657 = _0x3c0ff0[_0x3b116f].address;
+          var _0x347507 = /C$/;
+          var _0x36c818 = _0x347507.test(_0x332657);
+          if (_0x36c818) {
+            {
+              h5gg.setValue(_0x332657, 9999999999, "F32");
+            }
+          }
+        }
+      }
+    }
+  } else {
+    {
+      h5gg.clearResults();
+      h5gg.searchNumber("10000000000", "F32", "0x10000000", "0x160000000");
+      h5gg.searchNearby("131240", "I32", "0x40");
+      var _0xda9928 = h5gg.getResultsCount();
+      var _0x3c0ff0 = h5gg.getResults(_0xda9928);
+      for (var _0x3b116f = 0; _0x3b116f < _0xda9928; _0x3b116f++) {
+        {
+          var _0x27430c = _0x3c0ff0[_0x3b116f].address;
+          var _0x347507 = /C$/;
+          var _0x36c818 = _0x347507.test(_0x27430c);
+          if (_0x36c818) {
+            {
+              h5gg.setValue(_0x27430c, 100, "F32");
+            }
+          }
+        }
+      }
+    }
+  }
+}
+function nianhe1(_0x4d123f) {
+  h5gg.clearResults();
+  if (_0x4d123f.checked === true) {
+    {
+      var _0x43866f = prompt("默认粘合速度1.7建议0.1～1.7之间");
+      h5gg.searchNumber("18", "F32", "0x00000000", "0x200000000");
+      h5gg.searchNearby("5", "F32", "0x4");
+      h5gg.searchNearby("2.8026e-45", "F32", "0x4");
+      h5gg.searchNumber("5", "F32", "0x00000000", "0x200000000");
+      var _0x5f59ba = h5gg.getResultsCount();
+      var _0x37d49e = h5gg.getResults(_0x5f59ba);
+      for (var _0x369dd3 = 0; _0x369dd3 < _0x5f59ba; _0x369dd3++) {
+        {
+          var _0x5b1332 = _0x37d49e[_0x369dd3].address;
+          var _0x69e34c = Number(_0x5b1332) + 376;
+          h5gg.setValue(_0x69e34c, _0x43866f, "F32");
+        }
+      }
+    }
+  } else {
+    {
+      h5gg.clearResults();
+      h5gg.searchNumber("18", "F32", "0x00000000", "0x200000000");
+      h5gg.searchNearby("5", "F32", "0x4");
+      h5gg.searchNearby("2.8026e-45", "F32", "0x4");
+      h5gg.searchNumber("5", "F32", "0x00000000", "0x200000000");
+      var _0x5f59ba = h5gg.getResultsCount();
+      var _0x37d49e = h5gg.getResults(_0x5f59ba);
+      for (var _0x369dd3 = 0; _0x369dd3 < _0x5f59ba; _0x369dd3++) {
+        {
+          var _0x26cd36 = _0x37d49e[_0x369dd3].address;
+          var _0x244990 = Number(_0x26cd36) + 376;
+          h5gg.setValue(_0x244990, 1.7, "F32");
+        }
+      }
+    }
+  }
+}
+function nianhe2(_0x57c30a) {
+  h5gg.clearResults();
+  if (_0x57c30a.checked === true) {
+    {
+      var _0x70d239 = prompt("正常开0~0.5\n反向粘合0.6以上");
+      h5gg.searchNumber("112", "I32", "0x00000000", "0x200000000");
+      h5gg.searchNearby("287", "I32", "0x4");
+      h5gg.searchNearby("65536", "I32", "0x4");
+      h5gg.searchNumber("65536", "I32", "0x00000000", "0x200000000");
+      var _0x4abfdc = h5gg.getResultsCount();
+      var _0x15395e = h5gg.getResults(_0x4abfdc);
+      for (var _0x25a03c = 0; _0x25a03c < _0x4abfdc; _0x25a03c++) {
+        {
+          var _0xc1cab6 = _0x15395e[_0x25a03c].address;
+          var _0x412875 = Number(_0xc1cab6) - 24;
+          h5gg.setValue(_0x412875, _0x70d239, "F32");
+        }
+      }
+    }
+  } else {
+    {
+      h5gg.clearResults();
+      h5gg.searchNumber("112", "I32", "0x00000000", "0x200000000");
+      h5gg.searchNearby("287", "I32", "0x4");
+      h5gg.searchNearby("65536", "I32", "0x4");
+      h5gg.searchNumber("65536", "I32", "0x00000000", "0x200000000");
+      var _0x4abfdc = h5gg.getResultsCount();
+      var _0x15395e = h5gg.getResults(_0x4abfdc);
+      for (var _0x25a03c = 0; _0x25a03c < _0x4abfdc; _0x25a03c++) {
+        {
+          var _0x411f0c = _0x15395e[_0x25a03c].address;
+          var _0x49e4cf = Number(_0x411f0c) - 24;
+          h5gg.setValue(_0x49e4cf, 0.588, "F32");
+        }
+      }
+    }
+  }
+}
+function pbsy(_0x118647) {
+  h5gg.clearResults();
+  if (_0x118647.checked === true) {
+    {
+      var _0x22b88a = prompt("正常视野1.5请输入视野数值");
+      h5gg.clearResults();
+      h5gg.searchNumber("800", "F32", "0x00000000", "0x200000000");
+      h5gg.searchNearby("-750", "F32", "0x24");
+      h5gg.searchNearby("8", "F32", "0x24");
+      h5gg.searchNumber("-750", "F32", "0x00000000", "0x200000000");
+      var _0x6b936c = h5gg.getResultsCount();
+      var _0xc147d2 = h5gg.getResults(_0x6b936c);
+      for (var _0x4b26d9 = 0; _0x4b26d9 < _0x6b936c; _0x4b26d9++) {
+        {
+          var _0x32919d = _0xc147d2[_0x4b26d9].address;
+          var _0x60fd45 = Number(_0x32919d) + 20;
+          h5gg.setValue(_0x60fd45, _0x22b88a, "F32");
+        }
+      }
+    }
+  } else {
+    {
+      h5gg.clearResults();
+      h5gg.searchNumber("800", "F32", "0x00000000", "0x200000000");
+      h5gg.searchNearby("-750", "F32", "0x24");
+      h5gg.searchNearby("8", "F32", "0x24");
+      h5gg.searchNumber("-750", "F32", "0x00000000", "0x200000000");
+      var _0x6b936c = h5gg.getResultsCount();
+      var _0xc147d2 = h5gg.getResults(_0x6b936c);
+      for (var _0x4b26d9 = 0; _0x4b26d9 < _0x6b936c; _0x4b26d9++) {
+        {
+          var _0x59733d = _0xc147d2[_0x4b26d9].address;
+          var _0x29fe1a = Number(_0x59733d) + 20;
+          h5gg.setValue(_0x29fe1a, 1.5, "F32");
+        }
+      }
+    }
+  }
+}
+function sjsy(_0x2dbed9) {
+  h5gg.clearResults();
+  if (_0x2dbed9.checked === true) {
+    {
+      var _0x1fe25e = prompt("手机视野倍数");
+      h5gg.searchNumber("-3278106", "I32", "0x00000000", "0x200000000");
+      h5gg.searchNearby("-8880791", "I32", "0x4");
+      h5gg.searchNumber("-8880791", "I32", "0x00000000", "0x200000000");
+      var _0x52b35b = h5gg.getResultsCount();
+      var _0x3cd523 = h5gg.getResults(_0x52b35b);
+      for (var _0x997369 = 0; _0x997369 < _0x52b35b; _0x997369++) {
+        {
+          var _0x371e8d = _0x3cd523[_0x997369].address;
+          var _0x2a15ae = Number(_0x371e8d) + 96;
+          h5gg.setValue(_0x2a15ae, _0x1fe25e, "F32");
+        }
+      }
+    }
+  } else {
+    {
+      h5gg.clearResults();
+      h5gg.searchNumber("-3278106", "I32", "0x00000000", "0x200000000");
+      h5gg.searchNearby("-8880791", "I32", "0x4");
+      h5gg.searchNumber("-8880791", "I32", "0x00000000", "0x200000000");
+      var _0x52b35b = h5gg.getResultsCount();
+      var _0x3cd523 = h5gg.getResults(_0x52b35b);
+      for (var _0x997369 = 0; _0x997369 < _0x52b35b; _0x997369++) {
+        {
+          var _0x8df527 = _0x3cd523[_0x997369].address;
+          var _0x3ab07d = Number(_0x8df527) + 96;
+          h5gg.setValue(_0x3ab07d, 0.461823, "F32");
+        }
+      }
+    }
+  }
+}
