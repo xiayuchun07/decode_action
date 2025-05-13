2f5656ea5fb9f8968d2708cdb13ac29dcc3c1e28
action <action@github.com>
2025-05-13 18:39:37 +0000

    Add decoded output file

---
+++ output.js
@@ -0,0 +1,18 @@
+
+var _0x5b1aca = {};
+_0x5b1aca.JSTool = function () {};
+_0x5b1aca.JSTool.prototype = {
+  "_name": "JS在线工具",
+  "_information": {
+    "上线时间": ["2022-11-18", "JS在线工具"],
+    "功能": ["单文件加密", "批量加密(zip上传模式)", "混合加密", "压缩", "美化等功能"],
+    "联系我们/问题反馈": ["QQ", "2365508731", "请填写验证方式"]
+  },
+  "getName": function () {
+    return this._name;
+  },
+  "getInformation": function () {
+    return this._information;
+  }
+};
+var _0x2976e7 = new _0x5b1aca.JSTool();
