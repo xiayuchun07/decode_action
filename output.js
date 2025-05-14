550133012a7a016dc957b5aa6aa13975ae004032
action <action@github.com>
2025-05-14 10:41:28 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,1 +1,24 @@
 
+var encode_version = "jsjiami.com.v5";
+var a = {};
+var b = {};
+(function (_0x4a0e6e, _0x30ebff) {
+  _0x4a0e6e.info = "这是一个一系列js操作。";
+  _0x30ebff.warning = "如果您的JS里嵌套了PHP，JSP标签，等等其他非JavaScript的代码，请提取出来再加密。这个工具不能加密php、jsp等模版内容";
+  _0x30ebff.intro = "本工具由 www.jsjiami.com 提供接口。";
+})(a, b);
+(function (_0x19db3b, _0x28a420, _0x2b9ce1) {
+  _0x2b9ce1 = "al";
+  try {
+    _0x2b9ce1 += "ert";
+    _0x28a420 = encode_version;
+    if (!(typeof _0x28a420 !== "undefined" && _0x28a420 === "jsjiami.com.v5")) {
+      {
+        _0x19db3b[_0x2b9ce1]("删除版本号，js会定期弹窗，还请支持我们的工作");
+      }
+    }
+  } catch (_0x1d1a3e) {
+    _0x19db3b[_0x2b9ce1]("删除版本号，js会定期弹窗");
+  }
+})(window);
+encode_version = "jsjiami.com.v5";
