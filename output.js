5697464e8ac8924e18e3c634a79fefa80e57b1c3
action <action@github.com>
2025-06-01 20:32:25 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,57 +1,49 @@
 
-function a0_0x1e14(_0x28ac18, _0x3b5577) {
-  const _0x5aa4f7 = a0_0x3469();
-  a0_0x1e14 = function (_0x11224c, _0x593c40) {
-    _0x11224c = _0x11224c - 266;
-    let _0x851366 = _0x5aa4f7[_0x11224c];
-    return _0x851366;
-  };
-  return a0_0x1e14(_0x28ac18, _0x3b5577);
-}
-const a0_0x45a79b = a0_0x1e14;
-(function (_0x50174e, _0x435e88) {
-  const _0x436273 = a0_0x1e14;
-  const _0x2037a8 = _0x50174e();
+const a0_0x10de2b = a0_0x5d2b;
+(function (_0x31a828, _0x43f1cf) {
+  const _0x210bd5 = a0_0x5d2b;
+  const _0x4de9f7 = _0x31a828();
   while (true) {
     try {
-      const _0x496513 = parseInt(_0x436273(270)) / 1 + -parseInt(_0x436273(268)) / 2 + -parseInt(_0x436273(271)) / 3 + parseInt(_0x436273(280)) / 4 * (-parseInt(_0x436273(272)) / 5) + parseInt(_0x436273(281)) / 6 * (parseInt(_0x436273(277)) / 7) + -parseInt(_0x436273(266)) / 8 * (-parseInt(_0x436273(283)) / 9) + parseInt(_0x436273(282)) / 10 * (parseInt(_0x436273(275)) / 11);
-      if (_0x496513 === _0x435e88) {
+      const _0x2b42da = -parseInt(_0x210bd5(197)) / 1 + -parseInt(_0x210bd5(199)) / 2 + -parseInt(_0x210bd5(205)) / 3 * (-parseInt(_0x210bd5(204)) / 4) + parseInt(_0x210bd5(206)) / 5 + parseInt(_0x210bd5(200)) / 6 * (-parseInt(_0x210bd5(209)) / 7) + parseInt(_0x210bd5(198)) / 8 + parseInt(_0x210bd5(202)) / 9;
+      if (_0x2b42da === _0x43f1cf) {
         break;
       } else {
-        _0x2037a8.push(_0x2037a8.shift());
+        _0x4de9f7.push(_0x4de9f7.shift());
       }
-    } catch (_0x24a311) {
-      _0x2037a8.push(_0x2037a8.shift());
+    } catch (_0x5b6022) {
+      _0x4de9f7.push(_0x4de9f7.shift());
     }
   }
-})(a0_0x3469, 240397);
-let logger = require(a0_0x45a79b(285))[a0_0x45a79b(276)]("pomelo");
-let lifecycleDomain = require(a0_0x45a79b(279));
-function a0_0x3469() {
-  const _0x11a317 = ["1974863ioNtHi", "getLogger", "50771oCIgeo", "beforeShutdown", "./domain/lifecycleDomain", "7212riOPkz", "126pnfusG", "30Otdxpp", "1268919uLiVaX", "exports", "pomelo-logger", "afterStartAll", "info", "8lAsKLy", "curServer", "291766OFWxLS", "load config finished", "376613SLkgko", "1098231EPvBtH", "1265vJRRip", "beforeStartup", "afterStartup"];
-  a0_0x3469 = function () {
-    return _0x11a317;
+})(a0_0xe929, 462226);
+let domain = require("../domain/lifecycleDomain");
+module[a0_0x10de2b(208)] = function (_0x3fba06) {
+  return new remote(_0x3fba06);
+};
+let remote = function (_0xff2af4) {
+  const _0x4fa922 = a0_0x10de2b;
+  this[_0x4fa922(210)] = _0xff2af4;
+};
+let pro = remote[a0_0x10de2b(207)];
+pro.reloadParameterNotify = function (_0x215345) {
+  const _0x196cb1 = a0_0x10de2b;
+  domain[_0x196cb1(201)]().then(() => {
+    _0x215345();
+  })[_0x196cb1(203)](() => {});
+};
+function a0_0x5d2b(_0x51699f, _0x43f1e9) {
+  const _0x47f73e = a0_0xe929();
+  a0_0x5d2b = function (_0x39e337, _0x173cb7) {
+    _0x39e337 = _0x39e337 - 197;
+    let _0x52465e = _0x47f73e[_0x39e337];
+    return _0x52465e;
   };
-  return a0_0x3469();
+  return a0_0x5d2b(_0x51699f, _0x43f1e9);
 }
-module.exports[a0_0x45a79b(273)] = function (_0x203223, _0x3cde9a) {
-  const _0x141e03 = a0_0x45a79b;
-  logger[_0x141e03(287)](_0x203223[_0x141e03(267)].id, _0x141e03(273));
-  _0x3cde9a();
-};
-module[a0_0x45a79b(284)][a0_0x45a79b(274)] = function (_0x1e0f76, _0x229a19) {
-  const _0x2cfbb5 = a0_0x45a79b;
-  logger[_0x2cfbb5(287)](_0x1e0f76[_0x2cfbb5(267)].id, _0x2cfbb5(274));
-  _0x229a19();
-};
-module[a0_0x45a79b(284)][a0_0x45a79b(278)] = function (_0x39708d, _0x384444) {
-  const _0x290525 = a0_0x45a79b;
-  logger[_0x290525(287)](_0x39708d.curServer.id, _0x290525(278));
-  _0x384444();
-};
-module[a0_0x45a79b(284)][a0_0x45a79b(286)] = async function (_0x2b1863) {
-  const _0x23ceab = a0_0x45a79b;
-  logger[_0x23ceab(287)](_0x2b1863[_0x23ceab(267)].id, _0x23ceab(286));
-  await lifecycleDomain[_0x23ceab(286)]();
-  logger[_0x23ceab(287)](_0x2b1863[_0x23ceab(267)].id, _0x23ceab(269));
-};
+function a0_0xe929() {
+  const _0x3783b1 = ["prototype", "exports", "1045933ebGSNh", "app", "824643mVyowa", "755832vVEGJX", "1490172uwIkae", "36ByTEPe", "loadParameter", "21755583oCyjCP", "catch", "6836PZOYuH", "69gUbsQk", "1886980VFIdgV"];
+  a0_0xe929 = function () {
+    return _0x3783b1;
+  };
+  return a0_0xe929();
+}
