f29fdc691263f936af7d01fd84a69c9e0287ac46
action <action@github.com>
2025-06-01 21:29:48 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,52 +1,48 @@
 
-function a0_0x5734(_0xbf6f78, _0x4303a2) {
-  const _0x3978c2 = a0_0x4f16();
-  a0_0x5734 = function (_0x492d46, _0xc63287) {
-    _0x492d46 = _0x492d46 - 233;
-    let _0x4d90f4 = _0x3978c2[_0x492d46];
-    return _0x4d90f4;
+function a0_0x539e() {
+  const _0x241643 = ["loadParameter", "reloadParameterNotify", "2653776NrpjJI", "895784heJgIf", "1ufGuMk", "then", "1120356MVwCHW", "catch", "8144505JvZODm", "967766ZSUYZC", "7AdKTuj", "exports", "2476368EZZecE", "37129320XibLiD"];
+  a0_0x539e = function () {
+    return _0x241643;
   };
-  return a0_0x5734(_0xbf6f78, _0x4303a2);
+  return a0_0x539e();
 }
-const a0_0x449455 = a0_0x5734;
-function a0_0x4f16() {
-  const _0xe69aa3 = ["getInstance", "prototype", "13057sEalhb", "3137766xzWjaf", "16600030tEHFgz", "getSpreaderUser", "getLowerUser", "7374135RCtqyg", "2AmKtYG", "1635724qAPOmE", "21MWsWUH", "797246OPtirH", "64776kwKwhE", "2985800tbydTo", "9JDyQCy", "45eDhXFC", "exports"];
-  a0_0x4f16 = function () {
-    return _0xe69aa3;
-  };
-  return a0_0x4f16();
-}
-(function (_0x101f1e, _0x1239b3) {
-  const _0x486019 = a0_0x5734;
-  const _0x5888e4 = _0x101f1e();
+const a0_0x3ba1a0 = a0_0x503d;
+(function (_0x25c43c, _0x282b83) {
+  const _0x5f1820 = a0_0x503d;
+  const _0x5a075d = _0x25c43c();
   while (true) {
     try {
-      const _0x569375 = -parseInt(_0x486019(245)) / 1 * (parseInt(_0x486019(242)) / 2) + -parseInt(_0x486019(248)) / 3 * (parseInt(_0x486019(243)) / 4) + -parseInt(_0x486019(241)) / 5 + parseInt(_0x486019(237)) / 6 * (parseInt(_0x486019(244)) / 7) + parseInt(_0x486019(247)) / 8 * (-parseInt(_0x486019(249)) / 9) + -parseInt(_0x486019(238)) / 10 + parseInt(_0x486019(236)) / 11 * (parseInt(_0x486019(246)) / 12);
-      if (_0x569375 === _0x1239b3) {
+      const _0x1f3002 = -parseInt(_0x5f1820(221)) / 1 * (parseInt(_0x5f1820(212)) / 2) + -parseInt(_0x5f1820(215)) / 3 + parseInt(_0x5f1820(220)) / 4 + -parseInt(_0x5f1820(211)) / 5 + parseInt(_0x5f1820(209)) / 6 * (-parseInt(_0x5f1820(213)) / 7) + -parseInt(_0x5f1820(219)) / 8 + parseInt(_0x5f1820(216)) / 9;
+      if (_0x1f3002 === _0x282b83) {
         break;
       } else {
-        _0x5888e4.push(_0x5888e4.shift());
+        _0x5a075d.push(_0x5a075d.shift());
       }
-    } catch (_0x45810d) {
-      _0x5888e4.push(_0x5888e4.shift());
+    } catch (_0xe57e5b) {
+      _0x5a075d.push(_0x5a075d.shift());
     }
   }
-})(a0_0x4f16, 951315);
-let unionUserService = require("../../../services/unionUserServices")[a0_0x449455(234)]();
-module[a0_0x449455(233)] = function (_0x5d147c) {
-  return new remote(_0x5d147c);
+})(a0_0x539e, 892738);
+let domain = require("../domain/lifecycleDomain");
+module[a0_0x3ba1a0(214)] = function (_0x4a8c64) {
+  return new remote(_0x4a8c64);
 };
-let remote = function (_0x122697) {
-  this.app = _0x122697;
+function a0_0x503d(_0x1d377d, _0x5553ee) {
+  const _0x2978d5 = a0_0x539e();
+  a0_0x503d = function (_0x10a9ff, _0x5d275b) {
+    _0x10a9ff = _0x10a9ff - 208;
+    let _0xda6972 = _0x2978d5[_0x10a9ff];
+    return _0xda6972;
+  };
+  return a0_0x503d(_0x1d377d, _0x5553ee);
+}
+let remote = function (_0x4c81b7) {
+  this.app = _0x4c81b7;
 };
-let pro = remote[a0_0x449455(235)];
-pro[a0_0x449455(239)] = async (_0x352664, _0x2ae14d, _0x1709ad) => {
-  const _0xa30fa5 = a0_0x449455;
-  let _0x5a4417 = await unionUserService[_0xa30fa5(239)](_0x352664, _0x2ae14d);
-  _0x1709ad(null, _0x5a4417);
-};
-pro[a0_0x449455(240)] = async (_0x5c14b7, _0x16e74c, _0x172ca5) => {
-  const _0x4e52e7 = a0_0x449455;
-  let _0x3a9a43 = await unionUserService[_0x4e52e7(240)](_0x5c14b7, _0x16e74c);
-  _0x172ca5(null, _0x3a9a43);
+let pro = remote.prototype;
+pro[a0_0x3ba1a0(218)] = function (_0x1c58fc) {
+  const _0x2c00bb = a0_0x3ba1a0;
+  domain[_0x2c00bb(217)]()[_0x2c00bb(208)](() => {
+    _0x1c58fc();
+  })[_0x2c00bb(210)](() => {});
 };
