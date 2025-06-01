b8337a3594dd5b61fc4588ffe7fc8fa41c0673fd
action <action@github.com>
2025-06-01 20:51:07 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,50 +1,64 @@
 
-function a0_0x1dc7() {
-  const _0x3e9b0d = ["pomelo-logger", "invokeCallback", "2718633IZkpdS", "9lbpmbb", "416350rrWcMT", "loadParameter", "9335672DsZttQ", "12bNHADL", "21OmeqmR", "exports", "debug", "2vxKvNz", "pomelo", "15601168OePUSc", "1204955SUvgEW", "972560MryTzi", "1160106aCzvKA", "getLogger", "hall load parameter finished", "loadConfig"];
-  a0_0x1dc7 = function () {
-    return _0x3e9b0d;
+const a0_0x40d207 = a0_0x578b;
+function a0_0x578b(_0x494810, _0x59d5c8) {
+  const _0x439b4b = a0_0x3967();
+  a0_0x578b = function (_0x2da819, _0x95a481) {
+    _0x2da819 = _0x2da819 - 198;
+    let _0x3c0694 = _0x439b4b[_0x2da819];
+    return _0x3c0694;
   };
-  return a0_0x1dc7();
+  return a0_0x578b(_0x494810, _0x59d5c8);
 }
-const a0_0x184352 = a0_0x10bf;
-(function (_0x352239, _0x25e5b7) {
-  const _0x540949 = a0_0x10bf;
-  const _0xef63fd = _0x352239();
+(function (_0x59ad26, _0x5b392e) {
+  const _0x53c700 = a0_0x578b;
+  const _0x116171 = _0x59ad26();
   while (true) {
     try {
-      const _0x29e2f9 = parseInt(_0x540949(364)) / 1 + -parseInt(_0x540949(360)) / 2 * (-parseInt(_0x540949(371)) / 3) + -parseInt(_0x540949(376)) / 4 * (-parseInt(_0x540949(363)) / 5) + parseInt(_0x540949(365)) / 6 * (parseInt(_0x540949(377)) / 7) + -parseInt(_0x540949(375)) / 8 + parseInt(_0x540949(372)) / 9 * (parseInt(_0x540949(373)) / 10) + -parseInt(_0x540949(362)) / 11;
-      if (_0x29e2f9 === _0x25e5b7) {
+      const _0x574a35 = -parseInt(_0x53c700(221)) / 1 * (-parseInt(_0x53c700(199)) / 2) + parseInt(_0x53c700(210)) / 3 + -parseInt(_0x53c700(198)) / 4 + -parseInt(_0x53c700(216)) / 5 + parseInt(_0x53c700(203)) / 6 + -parseInt(_0x53c700(212)) / 7 * (-parseInt(_0x53c700(220)) / 8) + -parseInt(_0x53c700(201)) / 9 * (parseInt(_0x53c700(214)) / 10);
+      if (_0x574a35 === _0x5b392e) {
         break;
       } else {
-        _0xef63fd.push(_0xef63fd.shift());
+        _0x116171.push(_0x116171.shift());
       }
-    } catch (_0x5a8806) {
-      _0xef63fd.push(_0xef63fd.shift());
+    } catch (_0x3a9ff0) {
+      _0x116171.push(_0x116171.shift());
     }
   }
-})(a0_0x1dc7, 638185);
-let domain = module[a0_0x184352(378)];
-let pomelo = require(a0_0x184352(361));
-let logger = require(a0_0x184352(369))[a0_0x184352(366)]("pomelo");
-let utils = require("../../../util/utils");
-let parameterServices = require("../../../services/configServices");
-let async = require("async");
-function a0_0x10bf(_0x4959e3, _0x36efab) {
-  const _0x57b2e5 = a0_0x1dc7();
-  a0_0x10bf = function (_0x511047, _0x11b372) {
-    _0x511047 = _0x511047 - 360;
-    let _0x238660 = _0x57b2e5[_0x511047];
-    return _0x238660;
+})(a0_0x3967, 469777);
+let async = require(a0_0x40d207(202));
+let lifecycleDomain = require(a0_0x40d207(213));
+function a0_0x3967() {
+  const _0x5145e8 = ["afterStartAll", "exports", "parallel", "8kKcAnt", "1NYQvov", "3101920GOxZtI", "296836ctQVpw", "afterStartup", "9gMSdEB", "async", "3443280YnqWEn", "log", "timeEnd", "beforeStartup", "unionManager", "curServer", "ShutDown", "2056200oEnVpD", "time", "2330069bvLeam", "./domain/lifecycleDomain", "2582760xleADb", "beforeShutdown", "1185160EQBUPX"];
+  a0_0x3967 = function () {
+    return _0x5145e8;
   };
-  return a0_0x10bf(_0x4959e3, _0x36efab);
+  return a0_0x3967();
 }
-domain.afterStartAll = async function (_0x316a1c) {
-  const _0x4126cd = a0_0x184352;
-  await domain[_0x4126cd(374)]();
-  utils[_0x4126cd(370)](_0x316a1c);
+module.exports[a0_0x40d207(206)] = function (_0x6aad85, _0x407ee4) {
+  const _0x1d4c5e = a0_0x40d207;
+  console[_0x1d4c5e(204)](_0x6aad85[_0x1d4c5e(208)].id, _0x1d4c5e(206));
+  _0x407ee4();
 };
-domain[a0_0x184352(374)] = async function () {
-  const _0x5d5774 = a0_0x184352;
-  await parameterServices[_0x5d5774(368)]();
-  logger[_0x5d5774(379)](_0x5d5774(367));
+module.exports[a0_0x40d207(200)] = function (_0x3a1fc4, _0x16a9fb) {
+  console.log(_0x3a1fc4.curServer.id, "afterStartup");
+  _0x16a9fb();
+};
+module.exports[a0_0x40d207(215)] = function (_0x18872d, _0x2959ab) {
+  const _0x5854fe = a0_0x40d207;
+  console.log(_0x18872d.curServer.id, _0x5854fe(215));
+  console[_0x5854fe(211)](_0x5854fe(209) + _0x18872d[_0x5854fe(208)].id);
+  async[_0x5854fe(219)]([function (_0x1e74e7) {
+    const _0x367415 = _0x5854fe;
+    _0x18872d[_0x367415(207)][_0x367415(215)](_0x1e74e7);
+  }], function (_0x4b257d, _0x45c1ad) {
+    const _0x5e1dc2 = _0x5854fe;
+    console.log("callback in lifecycle and I think this will not hit in pomelo-sync@0.0.3", _0x4b257d, _0x45c1ad);
+    console[_0x5e1dc2(205)](_0x5e1dc2(209) + _0x18872d[_0x5e1dc2(208)].id);
+    _0x2959ab();
+  });
+};
+module[a0_0x40d207(218)].afterStartAll = function (_0x764877) {
+  const _0x355ed0 = a0_0x40d207;
+  console[_0x355ed0(204)](_0x764877[_0x355ed0(208)].id, "afterStartAll");
+  lifecycleDomain[_0x355ed0(217)]();
 };
