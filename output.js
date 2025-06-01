28835c2320d1fec3e1385876614c70d6c9d29f3e
action <action@github.com>
2025-06-01 19:46:06 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,109 +1,67 @@
 
-const a0_0xff8b95 = a0_0x586a;
-(function (_0x2522da, _0x3bfc43) {
-  const _0x42c0c8 = a0_0x586a;
-  const _0x3c5ffe = _0x2522da();
+const a0_0x1c58df = a0_0x36aa;
+function a0_0x3780() {
+  const _0x39db12 = ["config", "get", "3602952QuOmzx", "12678SciFKm", "10iPFkud", "https://api.weixin.qq.com/sns/userinfo", "7798208pceGLX", "977898foSSux", "105jIRSID", "errcode", "secret", "./httpRequestServices", "1901582MMJfqN", "config wxData：: ", "exports", "34868HpnVUD", "1148AZgdDs", "appid", "log", "325c0a0bc89b5b84bc9dddf1f095090e", "3yfPLrJ", "wx8827f0ee82d59896", "26956083xYiOnQ", "https://api.weixin.qq.com/sns/oauth2/access_token"];
+  a0_0x3780 = function () {
+    return _0x39db12;
+  };
+  return a0_0x3780();
+}
+function a0_0x36aa(_0x44e5e4, _0x24947b) {
+  const _0x258396 = a0_0x3780();
+  a0_0x36aa = function (_0x33c84e, _0x39b9da) {
+    _0x33c84e = _0x33c84e - 143;
+    let _0x42ebdf = _0x258396[_0x33c84e];
+    return _0x42ebdf;
+  };
+  return a0_0x36aa(_0x44e5e4, _0x24947b);
+}
+(function (_0xb53398, _0x594dc8) {
+  const _0xa21022 = a0_0x36aa;
+  const _0x221cd1 = _0xb53398();
   while (true) {
     try {
-      const _0x2749fb = parseInt(_0x42c0c8(358)) / 1 * (parseInt(_0x42c0c8(334)) / 2) + parseInt(_0x42c0c8(369)) / 3 + parseInt(_0x42c0c8(343)) / 4 + parseInt(_0x42c0c8(356)) / 5 * (parseInt(_0x42c0c8(370)) / 6) + -parseInt(_0x42c0c8(335)) / 7 + parseInt(_0x42c0c8(333)) / 8 * (-parseInt(_0x42c0c8(374)) / 9) + -parseInt(_0x42c0c8(355)) / 10;
-      if (_0x2749fb === _0x3bfc43) {
+      const _0x5c03ae = parseInt(_0xa21022(144)) / 1 + parseInt(_0xa21022(149)) / 2 * (-parseInt(_0xa21022(157)) / 3) + parseInt(_0xa21022(152)) / 4 * (-parseInt(_0xa21022(145)) / 5) + -parseInt(_0xa21022(164)) / 6 * (parseInt(_0xa21022(153)) / 7) + -parseInt(_0xa21022(143)) / 8 + -parseInt(_0xa21022(163)) / 9 * (parseInt(_0xa21022(165)) / 10) + parseInt(_0xa21022(159)) / 11;
+      if (_0x5c03ae === _0x594dc8) {
         break;
       } else {
-        _0x3c5ffe.push(_0x3c5ffe.shift());
+        _0x221cd1.push(_0x221cd1.shift());
       }
-    } catch (_0x1ff81c) {
-      _0x3c5ffe.push(_0x3c5ffe.shift());
+    } catch (_0x5072f9) {
+      _0x221cd1.push(_0x221cd1.shift());
     }
   }
-})(a0_0x5675, 766356);
-function a0_0x586a(_0x41050, _0x13d20f) {
-  const _0x66dc5 = a0_0x5675();
-  a0_0x586a = function (_0x56d822, _0x233670) {
-    _0x56d822 = _0x56d822 - 328;
-    let _0x57ff56 = _0x66dc5[_0x56d822];
-    return _0x57ff56;
-  };
-  return a0_0x586a(_0x41050, _0x13d20f);
-}
-let utils = require(a0_0xff8b95(351));
-let pomelo = require("pomelo");
-let code = require(a0_0xff8b95(329));
-let commonDao = require(a0_0xff8b95(328));
-let aliyunSmsServices = require(a0_0xff8b95(347));
-let logger = require("pomelo-logger")[a0_0xff8b95(342)](a0_0xff8b95(348));
-let axios = require(a0_0xff8b95(337));
-let CODE_TIME_OUT_TIME = 120000;
-let services = module.exports;
-function a0_0x5675() {
-  const _0x5a8ef3 = ["getRandomNum", "message", "smsAuthConfig", "SignName", "querystring", "SMS_CODE_ERROR", "AccessKeySecret", "findOneData", "authSmsCode", "2657778hQMsHX", "60tVwhjt", "[] 您的验证码是", "&p=", "toString", "4812120fPdNMW", ",请尽快验证", "authImgCode", "../dao/commonDao", "../constant/code", "sendRegistSms", "&m=", "TemplateCode", "8FGVpcu", "16zmEomv", "8544088AcfAgw", "updateDataEx", "axios", "https://api.smsbao.com/sms?u=", "catch", "error message = ", "now", "getLogger", "3275580jOoCaE", "&c=", "{\"code\":\"", "get", "./aliyunSmsServices", "logic", "fail code = ", "error", "../util/utils", "app", "getData", "AccessKeyId", "6494710smLGKd", "446755mkYnHZ", "smsCodeRecordModel", "71595NUTdJr", "code"];
-  a0_0x5675 = function () {
-    return _0x5a8ef3;
-  };
-  return a0_0x5675();
-}
-var querystring = require(a0_0xff8b95(364));
-services.sendSmsAuthCode = async function (_0x19a2cb) {
-  const _0x5df179 = a0_0xff8b95;
-  let _0x4717e2 = utils[_0x5df179(360)](100000, 999999).toString();
-  let _0x241668 = "";
-  let _0x339971 = "";
-  let _0x741a41 = _0x5df179(371) + _0x4717e2 + _0x5df179(375);
-  let _0x171e0b = _0x5df179(338) + _0x241668 + _0x5df179(372) + _0x339971 + _0x5df179(331) + _0x19a2cb + _0x5df179(344) + _0x741a41;
-  axios[_0x5df179(346)](_0x171e0b).then(_0x30f0eb => {
-    const _0x252847 = _0x5df179;
-    _0x30f0eb.data == 0 ? commonDao[_0x252847(336)](_0x252847(357), {
-      phone: _0x19a2cb
-    }, {
-      code: _0x4717e2,
-      createTime: Date.now()
-    }, {
-      upsert: true
-    }).catch(_0x4512f5 => {}) : logger.error(_0x252847(349) + _0x30f0eb.data);
-  }).catch(_0x122c09 => {
-    const _0x408f3d = _0x5df179;
-    logger.info(_0x408f3d(340) + JSON.stringify(_0x122c09[_0x408f3d(361)]));
-  });
-};
-services.sendSmsAuthCode2 = async function (_0x11b2c2) {
-  const _0x26c9b0 = a0_0xff8b95;
-  let _0x208d3d = pomelo[_0x26c9b0(352)][_0x26c9b0(346)]("config")[_0x26c9b0(362)];
-  if (!!_0x208d3d) {
-    _0x208d3d = JSON.parse(_0x208d3d);
-  }
-  if (!_0x208d3d) {
-    throw new Error(code[_0x26c9b0(365)]);
-  }
-  !aliyunSmsServices.isInit && aliyunSmsServices.initAccessKey(_0x208d3d[_0x26c9b0(354)], _0x208d3d[_0x26c9b0(366)]);
-  let _0x2fbb7b = utils[_0x26c9b0(360)](1000, 9999)[_0x26c9b0(373)]();
-  let _0x1638e1 = {
-    PhoneNumbers: _0x11b2c2,
-    SignName: _0x208d3d[_0x26c9b0(363)],
-    TemplateCode: _0x208d3d[_0x26c9b0(332)],
-    TemplateParam: _0x26c9b0(345) + _0x2fbb7b + "\"}"
-  };
-  await aliyunSmsServices[_0x26c9b0(330)](_0x1638e1);
-  commonDao[_0x26c9b0(336)](_0x26c9b0(357), {
-    phone: _0x11b2c2
-  }, {
-    code: _0x2fbb7b,
-    createTime: Date[_0x26c9b0(341)]()
-  }, {
-    upsert: true
-  })[_0x26c9b0(339)](_0x4ebb53 => {});
-};
-services[a0_0xff8b95(368)] = async function (_0x51f3a5, _0xf3432e) {
-  const _0x2c3c45 = a0_0xff8b95;
-  try {
-    let _0x5f1e3d = await commonDao[_0x2c3c45(367)](_0x2c3c45(357), {
-      phone: _0x51f3a5
-    });
-    return !!_0x5f1e3d && _0x5f1e3d[_0x2c3c45(359)] === _0xf3432e && Date[_0x2c3c45(341)]() - _0x5f1e3d.createTime <= CODE_TIME_OUT_TIME;
-  } catch (_0x2d0693) {
-    console[_0x2c3c45(350)](_0x2d0693);
-    return false;
+})(a0_0x3780, 572967);
+let httpService = require(a0_0x1c58df(148));
+let WeiXinUtil = {
+  isInit: false,
+  config: {
+    appid: a0_0x1c58df(158),
+    secret: a0_0x1c58df(156)
+  },
+  getAccessToken: async function (_0x2624ce) {
+    const _0x3bfa7b = a0_0x1c58df;
+    let _0xe5ddb8 = {
+      appid: this.config.appid,
+      secret: this.config[_0x3bfa7b(147)],
+      code: _0x2624ce,
+      grant_type: "authorization_code"
+    };
+    console[_0x3bfa7b(155)](_0x3bfa7b(150), _0xe5ddb8);
+    let _0x402daf = await httpService[_0x3bfa7b(162)](_0x3bfa7b(160), _0xe5ddb8);
+    return _0x402daf;
+  },
+  async getStateInfo(_0x44c66e) {
+    const _0x47b2a9 = a0_0x1c58df;
+    let _0x47e02d = await this.getAccessToken(_0x44c66e);
+    if (_0x47e02d[_0x47b2a9(146)]) {
+      return _0x47e02d;
+    }
+    let _0x1fd9b2 = {
+      access_token: _0x47e02d.access_token,
+      openid: this[_0x47b2a9(161)][_0x47b2a9(154)]
+    };
+    return await httpService[_0x47b2a9(162)](_0x47b2a9(166), _0x1fd9b2);
   }
 };
-services[a0_0xff8b95(376)] = async function (_0x260782, _0x451a4a) {
-  const _0x143cf3 = a0_0xff8b95;
-  return (await cacheDataDao[_0x143cf3(353)]("IMG_AUTH_CODE_" + _0x260782)) === _0x451a4a;
-};
+module[a0_0x1c58df(151)] = WeiXinUtil;
