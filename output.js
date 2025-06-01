d3cdc3542e050b7f3dfe67e8d4e6396d0ae084d2
action <action@github.com>
2025-06-01 21:33:07 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,51 +1,88 @@
 
-function a0_0x1b70() {
-  const _0x136c0e = ["18347DvQvxl", "getTodayWinGoldCountRankRequest", "2412642RpcDtk", "278060yaFjfs", "prototype", "uid", "693746zYhSTG", "3239290inrFBm", "INVALID_UERS", "../../../constant/code", "437198JYmjah", "2622627jdmzAa", "296KaSjgN", "centerManager", "app", "5uDKsBU", "getRankListData", "3DwzFCZ", "exports"];
-  a0_0x1b70 = function () {
-    return _0x136c0e;
+function a0_0x1253() {
+  const _0xec4380 = ["http load parameter finished", "466744vuoRfU", "65WUrNGN", "afterStartAll", "7EgAPuF", "format", "findOneAndUpdateEx", "996186EGmikf", "afterStartup", "loadParameter", "findOneData", "75412wuCejP", "teamDataModel", "loadConfig", "debug", "6933420WYbfGk", "uniqueIDModel", "pomelo-logger", "length", "moment", "../../../services/configServices", "30601930MxUuFq", "unionModel", "2312583lBvkUM", "217688ObZIGd", "updateDataEx", "unionID", "exports", "28uAjwXn"];
+  a0_0x1253 = function () {
+    return _0xec4380;
   };
-  return a0_0x1b70();
+  return a0_0x1253();
 }
-const a0_0x5d15e9 = a0_0x4474;
-(function (_0x5aed6c, _0x39f65d) {
-  const _0xabf5e6 = a0_0x4474;
-  const _0x3df669 = _0x5aed6c();
+const a0_0x228ef4 = a0_0x13b5;
+function a0_0x13b5(_0xef0e21, _0x2a2e23) {
+  const _0x1ddf39 = a0_0x1253();
+  a0_0x13b5 = function (_0x5a12f1, _0x35db5e) {
+    _0x5a12f1 = _0x5a12f1 - 165;
+    let _0x19445b = _0x1ddf39[_0x5a12f1];
+    return _0x19445b;
+  };
+  return a0_0x13b5(_0xef0e21, _0x2a2e23);
+}
+(function (_0x47dfbe, _0x46c872) {
+  const _0x58f480 = a0_0x13b5;
+  const _0x376fa5 = _0x47dfbe();
   while (true) {
     try {
-      const _0x5f16fc = parseInt(_0xabf5e6(315)) / 1 + -parseInt(_0xabf5e6(319)) / 2 * (-parseInt(_0xabf5e6(307)) / 3) + -parseInt(_0xabf5e6(312)) / 4 + parseInt(_0xabf5e6(305)) / 5 * (-parseInt(_0xabf5e6(311)) / 6) + -parseInt(_0xabf5e6(309)) / 7 * (parseInt(_0xabf5e6(321)) / 8) + -parseInt(_0xabf5e6(320)) / 9 + parseInt(_0xabf5e6(316)) / 10;
-      if (_0x5f16fc === _0x39f65d) {
+      const _0x4b8315 = parseInt(_0x58f480(188)) / 1 * (parseInt(_0x58f480(176)) / 2) + -parseInt(_0x58f480(171)) / 3 + -parseInt(_0x58f480(178)) / 4 * (parseInt(_0x58f480(179)) / 5) + parseInt(_0x58f480(184)) / 6 * (-parseInt(_0x58f480(181)) / 7) + -parseInt(_0x58f480(172)) / 8 + -parseInt(_0x58f480(192)) / 9 + parseInt(_0x58f480(169)) / 10;
+      if (_0x4b8315 === _0x46c872) {
         break;
       } else {
-        _0x3df669.push(_0x3df669.shift());
+        _0x376fa5.push(_0x376fa5.shift());
       }
-    } catch (_0x6c4381) {
-      _0x3df669.push(_0x3df669.shift());
+    } catch (_0xbc6f18) {
+      _0x376fa5.push(_0x376fa5.shift());
     }
   }
-})(a0_0x1b70, 376272);
-let code = require(a0_0x5d15e9(318));
-function a0_0x4474(_0x4a7c14, _0x3b0c59) {
-  const _0x2f813e = a0_0x1b70();
-  a0_0x4474 = function (_0x28f7e6, _0xf5405b) {
-    _0x28f7e6 = _0x28f7e6 - 304;
-    let _0x1e56c4 = _0x2f813e[_0x28f7e6];
-    return _0x1e56c4;
-  };
-  return a0_0x4474(_0x4a7c14, _0x3b0c59);
-}
-module[a0_0x5d15e9(308)] = function (_0x1cbf8d) {
-  return new Handler(_0x1cbf8d);
+})(a0_0x1253, 864560);
+let domain = module[a0_0x228ef4(175)];
+let logger = require(a0_0x228ef4(165)).getLogger("pomelo");
+let parameterServices = require(a0_0x228ef4(168));
+let commonDao = require("../../../dao/commonDao");
+let moment = require(a0_0x228ef4(167));
+domain[a0_0x228ef4(185)] = async function (_0x29d2b0) {
+  const _0x1f378b = a0_0x228ef4;
+  await domain[_0x1f378b(186)]();
+  _0x29d2b0();
 };
-let Handler = function (_0x59a2e8) {
-  const _0x32b711 = a0_0x5d15e9;
-  this[_0x32b711(304)] = _0x59a2e8;
-};
-Handler[a0_0x5d15e9(313)][a0_0x5d15e9(310)] = function (_0x28704a, _0x17b4db, _0x1bae48) {
-  const _0x51eed3 = a0_0x5d15e9;
-  !_0x17b4db[_0x51eed3(314)] ? _0x1bae48(null, {
-    code: code[_0x51eed3(317)]
-  }) : _0x1bae48(null, {
-    code: code.OK,
-    msg: this[_0x51eed3(304)][_0x51eed3(322)][_0x51eed3(306)](_0x28704a.startIndex, _0x28704a.count, _0x17b4db[_0x51eed3(314)])
+domain[a0_0x228ef4(180)] = async function () {
+  const _0xa62963 = a0_0x228ef4;
+  let _0xa8489 = await commonDao.findData(_0xa62963(170), {
+    isDel: false
   });
+  for (let _0x4543ae = 0; _0x4543ae < _0xa8489[_0xa62963(166)]; _0x4543ae++) {
+    let _0x47c832 = moment()[_0xa62963(182)]("YYYY-MM-DD");
+    let _0x214b01 = moment()[_0xa62963(182)]("x");
+    let _0x4913bb = _0xa8489[_0x4543ae][_0xa62963(174)];
+    let _0x4bcd8f = await commonDao[_0xa62963(187)](_0xa62963(189), {
+      unionID: _0x4913bb,
+      dateTime: _0x47c832
+    });
+    !_0x4bcd8f && (await commonDao[_0xa62963(183)](_0xa62963(189), {
+      unionID: _0x4913bb,
+      dateTime: _0x47c832
+    }, {
+      unionID: _0x4913bb,
+      dateTime: _0x47c832,
+      createTime: _0x214b01,
+      userCount: 0,
+      drawCount: 0,
+      drawTotalCount: 0,
+      diamondCount: 0
+    }, {
+      new: true,
+      upsert: true
+    }));
+  }
+  await commonDao[_0xa62963(173)](_0xa62963(193), {
+    key: 1
+  }, {
+    $setOnInsert: {
+      unionInviteID: 21130144
+    }
+  }, {
+    upsert: true
+  });
+};
+domain[a0_0x228ef4(186)] = async function () {
+  const _0x978adc = a0_0x228ef4;
+  await parameterServices[_0x978adc(190)]();
+  logger[_0x978adc(191)](_0x978adc(177));
 };
