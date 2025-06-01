720c4ea0d5bd03eb33a9a8b5a4f029c7700d10c9
action <action@github.com>
2025-06-01 15:37:17 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,146 +1,441 @@
 
-const a0_0x4ce072 = a0_0x5dd1;
-function a0_0x5dd1(_0x5a37b9, _0x460a2e) {
-  const _0x18f481 = a0_0x4316();
-  a0_0x5dd1 = function (_0x5c5612, _0x58bdab) {
-    _0x5c5612 = _0x5c5612 - 154;
-    let _0x2fe2ca = _0x18f481[_0x5c5612];
-    return _0x2fe2ca;
+function a0_0x1d02(_0x599001, _0x4ab2f1) {
+  const _0x66bc5 = a0_0x1475();
+  a0_0x1d02 = function (_0x29e877, _0x3bb9ab) {
+    _0x29e877 = _0x29e877 - 485;
+    let _0x108676 = _0x66bc5[_0x29e877];
+    return _0x108676;
   };
-  return a0_0x5dd1(_0x5a37b9, _0x460a2e);
+  return a0_0x1d02(_0x599001, _0x4ab2f1);
 }
-(function (_0x3f669b, _0x2028c9) {
-  const _0x915916 = a0_0x5dd1;
-  const _0x46cc1f = _0x3f669b();
+const a0_0x1ff764 = a0_0x1d02;
+function a0_0x1475() {
+  const _0x450e85 = ["userReadyNotify", "12EXwTId", "GET_ROOM_ONLINE_USER_INFO_PUSH", "userInfoChangePush", "ROOM_DRAW_FINISHED_PUSH", "8238503yVfMSp", "GAME_WIN_RATE_NOTIFY", "746493IHbggx", "NOANSWER_WAIT_SECOND", "getAskDismissStatusPushData", "12RMSgEk", "GET_ROOM_SHOW_USER_INFO_NOTIFY", "GAME_INVITATION_NOTIFY", "getGameWinRatePushData", "getAskForDismissNotifyData", "roomDismissPush", "getUserChangeSeatPush", "selfEntryRoomPush", "ASK_FOR_DISMISS_STATUS_NOTIFY", "ROOM_DISMISS_PUSH", "1509035HBdGPq", "9OlehBn", "ROOM_DISMISS_RESULT_PUSH", "USER_CHAT_NOTIFY", "getRoomSceneInfoPush", "roomDrawFinished", "USER_RECONNECT_NOTIFY", "getGameRule", "60nOSxxP", "getUserReconnectNotifyData", "ANSWER_EXIT_SECOND", "GET_ROOM_ONLINE_USER_INFO_NOTIFY", "USER_LEAVE_ROOM_NOTIFY", "USER_WATCH_PUSH", "USER_CHANGE_SEAT_NOTIFY", "GAME_WIN_RATE_PUSH", "4DWmpaA", "getGameEndPushData", "USER_RECONNECT_PUSH", "userReadyPush", "GET_ROOM_SHOW_USER_INFO_PUSH", "22jMObZk", "OTHER_USER_ENTRY_ROOM_PUSH", "USER_CHANGE_SEAT_PUSH", "userLeaveRoomResponse", "getUserChangeSeatNotify", "ROOM_USER_WIN_TOTAL_PUSH", "SORRY_I_WILL_WIN_NOTIFY", "ASK_FOR_DISMISS_STATUS_PUSH", "USER_LEAVE_ROOM_RESPONSE", "2083942pTUmNT", "GET_ROOM_SCENE_INFO_NOTIFY", "getUserReconnectPushData", "userOnlineEnum", "ASK_FOR_DISMISS_NOTIFY", "BE_SHORT_OF_GOLD_COINS_PUSH", "ROOM_USER_INFO_CHANGE_PUSH", "roomDismissResultPush", "getGameWinRateNotifyData", "GAME_INVITATION_RESUIL_PUSH", "userLeaveRoomPush", "roomMessagePush", "userChatNotify", "userPlayEnum", "USER_CHAT_PUSH", "userWatchPush", "ASK_FOR_DISMISS_PUSH", "2311896stElrP", "getRoomOnlineUserInfoPush", "gameInvitationResult", "USER_LEAVE_ROOM_PUSH", "EXIT_WAIT_SECOND", "userChatPush", "511743IHfAZI", "ROOM_NOTICE_PUSH", "getRoomSceneInfoNotify", "USER_READY_PUSH", "USER_OFF_LINE_PUSH", "GAME_END_PUSH", "getRoomOnlineUserInfoNotify", "USER_GIFT_SEND_PUSH", "userLeaveRoomNotify", "2250840zNYrbq"];
+  a0_0x1475 = function () {
+    return _0x450e85;
+  };
+  return a0_0x1475();
+}
+(function (_0x31aa39, _0x106e39) {
+  const _0xef43be = a0_0x1d02;
+  const _0x2edb5f = _0x31aa39();
   while (true) {
     try {
-      const _0x7c4cd6 = parseInt(_0x915916(180)) / 1 * (parseInt(_0x915916(188)) / 2) + parseInt(_0x915916(168)) / 3 * (-parseInt(_0x915916(171)) / 4) + parseInt(_0x915916(189)) / 5 * (-parseInt(_0x915916(173)) / 6) + parseInt(_0x915916(178)) / 7 + -parseInt(_0x915916(183)) / 8 + parseInt(_0x915916(181)) / 9 * (parseInt(_0x915916(170)) / 10) + parseInt(_0x915916(155)) / 11;
-      if (_0x7c4cd6 === _0x2028c9) {
+      const _0x4190c3 = -parseInt(_0xef43be(528)) / 1 + parseInt(_0xef43be(557)) / 2 * (parseInt(_0xef43be(511)) / 3) + parseInt(_0xef43be(531)) / 4 * (-parseInt(_0xef43be(541)) / 5) + parseInt(_0xef43be(522)) / 6 * (-parseInt(_0xef43be(488)) / 7) + parseInt(_0xef43be(505)) / 8 * (-parseInt(_0xef43be(542)) / 9) + -parseInt(_0xef43be(520)) / 10 * (parseInt(_0xef43be(562)) / 11) + -parseInt(_0xef43be(549)) / 12 * (-parseInt(_0xef43be(526)) / 13);
+      if (_0x4190c3 === _0x106e39) {
         break;
       } else {
-        _0x46cc1f.push(_0x46cc1f.shift());
+        _0x2edb5f.push(_0x2edb5f.shift());
       }
-    } catch (_0x2ea23d) {
-      _0x46cc1f.push(_0x46cc1f.shift());
+    } catch (_0x3eb4f4) {
+      _0x2edb5f.push(_0x2edb5f.shift());
     }
   }
-})(a0_0x4316, 411348);
-function a0_0x4316() {
-  const _0x543582 = ["61590IzhEKR", "93356UzGqSZ", "roomMessagePush", "1698oTYvtj", "KickUserPush", "JoinGameAgainPush", "ServerMessagePush", "stack", "3594227znQEOo", "../util/token", "990FOQlSw", "63QTNCse", "SelfEntryRoomPush", "3664472mpdxfG", "popDialogContentPush", "pomelo-logger", "pushMessageByUids", "error", "534qhnYHR", "1935nFtfqw", "get", "connector", "2508451achQlZ", "pushRouter", "updateUserInfoPush", "broadcastPush", "channelService", "PopDialogContentPush", "app", "selfEntryRoomPush", "getLogger", "exports", "gameInvitation", "createMsg", "pomelo", "9EJSyin", "broadcast"];
-  a0_0x4316 = function () {
-    return _0x543582;
+})(a0_0x1475, 523336);
+let proto = module.exports;
+proto.USER_READY_NOTIFY = 301;
+proto[a0_0x1ff764(514)] = 401;
+proto[a0_0x1ff764(563)] = 402;
+proto[a0_0x1ff764(553)] = 303;
+proto[a0_0x1ff764(487)] = 403;
+proto[a0_0x1ff764(508)] = 404;
+proto.ROOM_DISMISS_PUSH = 405;
+proto[a0_0x1ff764(494)] = 406;
+proto[a0_0x1ff764(544)] = 307;
+proto[a0_0x1ff764(502)] = 407;
+proto[a0_0x1ff764(515)] = 408;
+proto[a0_0x1ff764(525)] = 409;
+proto[a0_0x1ff764(512)] = 410;
+proto[a0_0x1ff764(527)] = 311;
+proto[a0_0x1ff764(556)] = 411;
+proto[a0_0x1ff764(547)] = 312;
+proto[a0_0x1ff764(559)] = 412;
+proto[a0_0x1ff764(492)] = 313;
+proto[a0_0x1ff764(504)] = 413;
+proto[a0_0x1ff764(516)] = 414;
+proto[a0_0x1ff764(485)] = 415;
+proto.ASK_FOR_DISMISS_STATUS_NOTIFY = 316;
+proto.ASK_FOR_DISMISS_STATUS_PUSH = 416;
+proto[a0_0x1ff764(532)] = 317;
+proto.GET_ROOM_SHOW_USER_INFO_PUSH = 417;
+proto.GET_ROOM_SCENE_INFO_NOTIFY = 318;
+proto.GET_ROOM_SCENE_INFO_PUSH = 418;
+proto[a0_0x1ff764(552)] = 319;
+proto[a0_0x1ff764(523)] = 419;
+proto.USER_CHANGE_SEAT_NOTIFY = 320;
+proto.USER_CHANGE_SEAT_PUSH = 420;
+proto[a0_0x1ff764(493)] = 421;
+proto[a0_0x1ff764(509)] = 30;
+proto[a0_0x1ff764(529)] = 120;
+proto[a0_0x1ff764(551)] = 10;
+proto.USER_GIFT_SEND_NOTIFY = 330;
+proto[a0_0x1ff764(518)] = 430;
+proto[a0_0x1ff764(554)] = 431;
+proto[a0_0x1ff764(543)] = 432;
+proto[a0_0x1ff764(567)] = 433;
+proto.roomUserWinTotalPush = function (_0x964ca2) {
+  return {
+    type: proto.ROOM_USER_WIN_TOTAL_PUSH,
+    data: {
+      userWinTotalScore: _0x964ca2
+    }
   };
-  return a0_0x4316();
-}
-let api = module[a0_0x4ce072(164)];
-let pomelo = require("pomelo");
-let logger = require(a0_0x4ce072(185))[a0_0x4ce072(163)](a0_0x4ce072(167));
-let token = require(a0_0x4ce072(179));
-api[a0_0x4ce072(172)] = function (_0x2750e8, _0x1633f3) {
-  return new Promise((_0x5168dd, _0x361f2a) => {
-    const _0x160f5b = a0_0x5dd1;
-    let _0x524fc3 = pomelo[_0x160f5b(161)].get(_0x160f5b(159));
-    _0x2750e8.pushRouter = "RoomMessagePush";
-    let _0x442dde = token.createMsg(_0x2750e8);
-    _0x524fc3[_0x160f5b(186)](_0x160f5b(176), _0x442dde, _0x1633f3, {}, function (_0x343e13) {
-      const _0x2312e1 = _0x160f5b;
-      !!_0x343e13 ? (logger[_0x2312e1(187)](_0x343e13[_0x2312e1(177)]), _0x361f2a(_0x343e13)) : _0x5168dd();
-    });
-  });
 };
-api.gameMessagePush = function (_0x5c2994, _0xbe0d7d) {
-  return new Promise((_0x4076e0, _0x4150ed) => {
-    const _0x3ed108 = a0_0x5dd1;
-    let _0x59647d = pomelo[_0x3ed108(161)].get(_0x3ed108(159));
-    _0x5c2994[_0x3ed108(156)] = "GameMessagePush";
-    let _0x1bfb6c = token.createMsg(_0x5c2994);
-    _0x59647d[_0x3ed108(186)](_0x3ed108(176), _0x1bfb6c, _0xbe0d7d, {}, function (_0x409d31) {
-      const _0x6dd78f = _0x3ed108;
-      !!_0x409d31 ? (logger[_0x6dd78f(187)](_0x409d31.stack), _0x4150ed(_0x409d31)) : _0x4076e0();
-    });
-  });
+proto.userStatusEnum = {
+  NONE: 0,
+  READY: 1,
+  PLAYING: 2,
+  OFFLINE: 4,
+  WATCH: 5,
+  DISMISS: 8
 };
-api[a0_0x4ce072(162)] = function (_0x21fd39, _0x57cb60) {
-  return new Promise((_0x529672, _0x310bd5) => {
-    const _0xe071d = a0_0x5dd1;
-    let _0x20a3ab = pomelo[_0xe071d(161)].get("channelService");
-    _0x21fd39.pushRouter = _0xe071d(182);
-    let _0x558ec1 = token[_0xe071d(166)](_0x21fd39);
-    _0x20a3ab.pushMessageByUids(_0xe071d(176), _0x558ec1, _0x57cb60, {}, function (_0x37f5c2) {
-      const _0x48ba5f = _0xe071d;
-      !!_0x37f5c2 ? (logger.error(_0x37f5c2[_0x48ba5f(177)]), _0x310bd5(_0x37f5c2)) : _0x529672();
-    });
-  });
+proto[a0_0x1ff764(491)] = {
+  OFFLINE: 0,
+  ONLINE: 1
 };
-api[a0_0x4ce072(157)] = function (_0x4c00f4, _0x3f57a4) {
-  return new Promise((_0x4ba9b5, _0x86092f) => {
-    const _0x325979 = a0_0x5dd1;
-    let _0x147c3d = pomelo[_0x325979(161)][_0x325979(190)](_0x325979(159));
-    _0x4c00f4[_0x325979(156)] = "UpdateUserInfoPush";
-    let _0x92123d = token[_0x325979(166)](_0x4c00f4);
-    _0x147c3d[_0x325979(186)](_0x325979(176), _0x92123d, _0x3f57a4, {}, function (_0x54b1ae) {
-      const _0x2adb89 = _0x325979;
-      !!_0x54b1ae ? (logger[_0x2adb89(187)](_0x54b1ae[_0x2adb89(177)]), _0x86092f(_0x54b1ae)) : _0x4ba9b5(true);
-    });
-  });
+proto[a0_0x1ff764(501)] = {
+  NONE: 0,
+  READY: 1,
+  PLAYING: 2,
+  WATCH: 3
 };
-api.kickUserPush = function (_0x48555c, _0x1c0ee2) {
-  return new Promise((_0x2a3048, _0x5361d2) => {
-    const _0x4f9269 = a0_0x5dd1;
-    let _0x38e561 = pomelo[_0x4f9269(161)].get(_0x4f9269(159));
-    _0x48555c[_0x4f9269(156)] = _0x4f9269(174);
-    let _0x3c2e66 = token[_0x4f9269(166)](_0x48555c);
-    _0x38e561[_0x4f9269(186)](_0x4f9269(176), _0x3c2e66, _0x1c0ee2, {}, function (_0x24b25f) {
-      const _0x3c3ee6 = _0x4f9269;
-      !!_0x24b25f ? (logger[_0x3c3ee6(187)](_0x24b25f.stack), _0x5361d2(_0x24b25f)) : _0x2a3048(true);
-    });
-  });
+proto[a0_0x1ff764(533)] = 380;
+proto.GAME_INVITATION_RESUIL_PUSH = 480;
+proto[a0_0x1ff764(507)] = function (_0x4bef0e) {
+  const _0x5042ef = a0_0x1ff764;
+  return {
+    type: proto[_0x5042ef(497)],
+    data: {
+      msg: _0x4bef0e
+    }
+  };
 };
-api[a0_0x4ce072(165)] = function (_0x5ed26a, _0x4eb409) {
-  return new Promise((_0x59e87c, _0x348a32) => {
-    const _0x15ade3 = a0_0x5dd1;
-    let _0x584d1d = pomelo[_0x15ade3(161)][_0x15ade3(190)](_0x15ade3(159));
-    _0x5ed26a[_0x15ade3(156)] = "gameInvitation";
-    let _0x573dbf = token[_0x15ade3(166)](_0x5ed26a);
-    _0x584d1d[_0x15ade3(186)]("ServerMessagePush", _0x573dbf, _0x4eb409, {}, function (_0x2fefbf) {
-      const _0x519d27 = _0x15ade3;
-      !!_0x2fefbf ? (logger[_0x519d27(187)](_0x2fefbf[_0x519d27(177)]), _0x348a32(_0x2fefbf)) : _0x59e87c(true);
-    });
-  });
+proto[a0_0x1ff764(538)] = function (_0x3e298b, _0x42500e, _0x3205aa, _0x263e0f, _0x1c4186, _0x9fe30a) {
+  return {
+    gameType: _0x3e298b,
+    gameRule: _0x42500e,
+    roomID: _0x3205aa,
+    gameData: _0x263e0f,
+    userInfoArr: _0x1c4186,
+    roomCreatorInfo: _0x9fe30a
+  };
 };
-api.joinGameAgainPush = function (_0x3eb6c0, _0x7c896a) {
-  return new Promise((_0x2bd76d, _0x491c18) => {
-    const _0x253952 = a0_0x5dd1;
-    let _0x28f844 = pomelo.app[_0x253952(190)]("channelService");
-    _0x3eb6c0.pushRouter = _0x253952(175);
-    let _0x1c088f = token[_0x253952(166)](_0x3eb6c0);
-    _0x28f844.pushMessageByUids(_0x253952(176), _0x1c088f, _0x7c896a, {}, function (_0x3da540) {
-      const _0x3b4274 = _0x253952;
-      !!_0x3da540 ? (logger.error(_0x3da540[_0x3b4274(177)]), _0x491c18(_0x3da540)) : _0x2bd76d(true);
-    });
-  });
+proto[a0_0x1ff764(499)] = function (_0x356d01) {
+  return _0x356d01;
 };
-api[a0_0x4ce072(158)] = function (_0x429a68) {
-  return new Promise((_0x578cfd, _0x34999a) => {
-    const _0x396610 = a0_0x5dd1;
-    let _0x12add7 = pomelo[_0x396610(161)].get(_0x396610(159));
-    _0x429a68[_0x396610(156)] = "BroadcastPush";
-    let _0x1b0951 = token.createMsg(_0x429a68);
-    _0x12add7[_0x396610(169)](_0x396610(154), _0x396610(176), _0x1b0951, null, function (_0x3b47d1) {
-      const _0x4c4078 = _0x396610;
-      !!_0x3b47d1 ? (logger[_0x4c4078(187)](_0x3b47d1[_0x4c4078(177)]), _0x34999a(_0x3b47d1)) : _0x578cfd();
-    });
-  });
+proto[a0_0x1ff764(500)] = function (_0x47b8c9, _0x20c438) {
+  const _0x12e8df = a0_0x1ff764;
+  return {
+    type: proto[_0x12e8df(544)],
+    data: {
+      toChairID: _0x47b8c9,
+      msg: _0x20c438
+    }
+  };
 };
-api[a0_0x4ce072(184)] = function (_0x19734b, _0x18b828) {
-  return new Promise((_0x512cde, _0x93292a) => {
-    const _0x1de73c = a0_0x5dd1;
-    let _0x484d00 = pomelo[_0x1de73c(161)][_0x1de73c(190)](_0x1de73c(159));
-    _0x19734b[_0x1de73c(156)] = _0x1de73c(160);
-    let _0x5674c2 = token[_0x1de73c(166)](_0x19734b);
-    _0x484d00[_0x1de73c(186)]("ServerMessagePush", _0x5674c2, _0x18b828, {}, function (_0x1c2f31) {
-      const _0x59063 = _0x1de73c;
-      !!_0x1c2f31 ? (logger[_0x59063(187)](_0x1c2f31[_0x59063(177)]), _0x93292a(_0x1c2f31)) : _0x512cde();
-    });
-  });
+proto[a0_0x1ff764(510)] = function (_0x1bdbf0, _0x4bff93, _0x168f6b, _0x2c9c94, _0x36ca18) {
+  return {
+    type: proto.USER_CHAT_PUSH,
+    data: {
+      chairID: _0x1bdbf0,
+      uid: _0x4bff93,
+      type: _0x168f6b,
+      msg: _0x2c9c94,
+      recipientID: _0x36ca18
+    }
+  };
+};
+proto[a0_0x1ff764(521)] = function (_0x16ddbe) {
+  return {
+    type: proto.USER_READY_NOTIFY,
+    data: {
+      isReady: _0x16ddbe
+    }
+  };
+};
+proto[a0_0x1ff764(560)] = function (_0x1bbdc9) {
+  const _0x3ca937 = a0_0x1ff764;
+  return {
+    type: proto[_0x3ca937(514)],
+    data: {
+      chairID: _0x1bbdc9
+    }
+  };
+};
+proto[a0_0x1ff764(503)] = function (_0x11a625, _0x48002d) {
+  const _0x121768 = a0_0x1ff764;
+  return {
+    type: proto[_0x121768(554)],
+    data: {
+      chairID: _0x11a625,
+      userInfo: _0x48002d
+    }
+  };
+};
+proto.otherUserEntryRoomPush = function (_0x3052ae, _0x210f52) {
+  const _0x3ac235 = a0_0x1ff764;
+  return {
+    type: proto[_0x3ac235(563)],
+    data: {
+      roomUserInfo: _0x3052ae,
+      gameType: _0x210f52
+    }
+  };
+};
+proto[a0_0x1ff764(519)] = function () {
+  return {
+    type: proto.USER_LEAVE_ROOM_NOTIFY,
+    data: {}
+  };
+};
+proto[a0_0x1ff764(565)] = function (_0x1205b2, _0x2042b3) {
+  return {
+    type: proto.USER_LEAVE_ROOM_RESPONSE,
+    data: {
+      chairID: _0x1205b2,
+      uid: _0x2042b3
+    }
+  };
+};
+proto[a0_0x1ff764(498)] = function (_0x5d388d) {
+  return {
+    type: proto.USER_LEAVE_ROOM_PUSH,
+    data: {
+      roomUserInfo: _0x5d388d
+    }
+  };
+};
+proto.roomDismissReason = {
+  RDR_NONE: 0,
+  RDR_OWENER_ASK: 1,
+  RDR_USER_ASK: 2,
+  RDR_TIME_OUT: 4,
+  RDR_NOT_ENOUGH_GOLD: 8
+};
+proto[a0_0x1ff764(536)] = function (_0x19584b) {
+  const _0x2af11a = a0_0x1ff764;
+  return {
+    type: proto[_0x2af11a(540)],
+    data: {
+      reason: _0x19584b
+    }
+  };
+};
+proto[a0_0x1ff764(495)] = function () {
+  const _0x318f05 = a0_0x1ff764;
+  return {
+    type: proto[_0x318f05(543)],
+    data: {}
+  };
+};
+proto[a0_0x1ff764(524)] = function (_0x4e5a53) {
+  const _0x529de3 = a0_0x1ff764;
+  return {
+    type: proto[_0x529de3(494)],
+    data: {
+      changeInfo: _0x4e5a53
+    }
+  };
+};
+proto.userOffLinePush = function (_0x2e0758) {
+  const _0x1f86e4 = a0_0x1ff764;
+  return {
+    type: proto[_0x1f86e4(515)],
+    data: {
+      chairID: _0x2e0758
+    }
+  };
+};
+proto[a0_0x1ff764(546)] = function (_0x13e860) {
+  const _0x469f24 = a0_0x1ff764;
+  return {
+    type: proto[_0x469f24(525)],
+    data: {
+      allDrawScoreRecord: _0x13e860
+    }
+  };
+};
+proto[a0_0x1ff764(496)] = function (_0x135e8f) {
+  const _0x9822c5 = a0_0x1ff764;
+  return {
+    type: this[_0x9822c5(527)],
+    data: {
+      rate: _0x135e8f
+    }
+  };
+};
+proto[a0_0x1ff764(534)] = function () {
+  return {
+    type: this.GAME_WIN_RATE_PUSH,
+    data: {}
+  };
+};
+proto[a0_0x1ff764(548)] = function (_0x452f79, _0x4cd9ae, _0x55bd6b, _0x5b586e, _0x5ec070, _0x3394a1) {
+  return {
+    bureau: _0x452f79,
+    isOwnerPay: _0x4cd9ae,
+    memberCount: _0x55bd6b,
+    diamondCost: _0x5b586e,
+    gameType: _0x5ec070,
+    otherRule: _0x3394a1
+  };
+};
+proto[a0_0x1ff764(550)] = function () {
+  const _0x6c8794 = a0_0x1ff764;
+  return {
+    type: this[_0x6c8794(547)],
+    data: {}
+  };
+};
+proto[a0_0x1ff764(490)] = function (_0x31bc40) {
+  const _0x55fd87 = a0_0x1ff764;
+  return {
+    type: this[_0x55fd87(559)],
+    data: {
+      gameData: _0x31bc40
+    }
+  };
+};
+proto[a0_0x1ff764(535)] = function (_0x5306e1) {
+  const _0x103adf = a0_0x1ff764;
+  return {
+    type: this[_0x103adf(492)],
+    data: {
+      isExit: _0x5306e1
+    }
+  };
+};
+proto.getAskForDismissPushData = function (_0x585bbb, _0x44440d, _0x6de6c3, _0x5adb11, _0x5ead72, _0x4449e5, _0x473cc1) {
+  return {
+    type: this.ASK_FOR_DISMISS_PUSH,
+    data: {
+      nameArr: _0x44440d,
+      scoreArr: _0x6de6c3,
+      chairIDArr: _0x585bbb,
+      tm: _0x5adb11,
+      askChairId: _0x5ead72,
+      onlineArr: _0x4449e5,
+      avatarArr: _0x473cc1
+    }
+  };
+};
+proto[a0_0x1ff764(558)] = function (_0x50a434) {
+  const _0x45dedc = a0_0x1ff764;
+  return {
+    type: this[_0x45dedc(516)],
+    data: {
+      resout: _0x50a434
+    }
+  };
+};
+proto.getAskDismissStatusNotifyData = function () {
+  const _0x5a1762 = a0_0x1ff764;
+  return {
+    type: this[_0x5a1762(539)],
+    data: {}
+  };
+};
+proto[a0_0x1ff764(530)] = function (_0x4d5061) {
+  const _0x4de0ed = a0_0x1ff764;
+  return {
+    type: this[_0x4de0ed(486)],
+    data: {
+      isOnDismiss: _0x4d5061
+    }
+  };
+};
+proto.getRoomShowUserInfoNotify = function () {
+  const _0x10351f = a0_0x1ff764;
+  return {
+    type: proto[_0x10351f(532)],
+    data: {}
+  };
+};
+proto.getRoomShowUserInfoPush = function (_0x139c14, _0x54fa86, _0x5acb0a) {
+  const _0x3baaf5 = a0_0x1ff764;
+  return {
+    type: proto[_0x3baaf5(561)],
+    data: {
+      selfUserInfo: _0x139c14,
+      shenSuanZiInfo: _0x54fa86,
+      fuHaoUserInfoArr: _0x5acb0a
+    }
+  };
+};
+proto[a0_0x1ff764(513)] = function () {
+  const _0x16651d = a0_0x1ff764;
+  return {
+    type: proto[_0x16651d(489)],
+    data: {}
+  };
+};
+proto[a0_0x1ff764(545)] = function (_0x3d45b9, _0xe26478, _0x2f048d, _0x434d2a, _0x2f856c) {
+  return {
+    type: proto.GET_ROOM_SCENE_INFO_PUSH,
+    data: {
+      roomID: _0x3d45b9,
+      roomCreatorInfo: _0xe26478,
+      gameRule: _0x2f048d,
+      roomUserInfoArr: _0x434d2a,
+      gameData: _0x2f856c
+    }
+  };
+};
+proto[a0_0x1ff764(517)] = function () {
+  return {
+    type: proto.GET_ROOM_ONLINE_USER_INFO_NOTIFY,
+    data: {}
+  };
+};
+proto[a0_0x1ff764(506)] = function (_0x3cab9f, _0x1a16d7) {
+  const _0x2e5df3 = a0_0x1ff764;
+  return {
+    type: proto[_0x2e5df3(523)],
+    data: {
+      shenSuanZiInfo: _0x3cab9f,
+      fuHaoUserInfoArr: _0x1a16d7
+    }
+  };
+};
+proto[a0_0x1ff764(566)] = function (_0x5e4540, _0x211763) {
+  const _0x3113d9 = a0_0x1ff764;
+  return {
+    type: proto[_0x3113d9(555)],
+    data: {
+      fromChairID: _0x5e4540,
+      toChairID: _0x211763
+    }
+  };
+};
+proto[a0_0x1ff764(537)] = function (_0x3fd434, _0x4b25cf, _0x11915d, _0x16569f, _0x14c269 = 0) {
+  const _0x21e921 = a0_0x1ff764;
+  return {
+    type: proto[_0x21e921(564)],
+    data: {
+      fromChairID: _0x3fd434,
+      toChairID: _0x4b25cf,
+      uid: _0x11915d,
+      userStatus: _0x16569f,
+      code: _0x14c269
+    }
+  };
+};
+proto.beShortOfGoldCoinsPush = function (_0x55d1ee, _0x507e1f) {
+  return {
+    type: proto.BE_SHORT_OF_GOLD_COINS_PUSH,
+    data: {
+      uid: _0x55d1ee,
+      userStatus: _0x507e1f
+    }
+  };
+};
+proto.getUserGiftSendPush = function (_0x52f4e9, _0x543161, _0x37cbc8) {
+  return {
+    type: proto.USER_GIFT_SEND_PUSH,
+    data: {
+      fromChairID: _0x52f4e9,
+      toChairID: _0x543161,
+      giftID: _0x37cbc8
+    }
+  };
 };
