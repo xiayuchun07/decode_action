eb901f978ac0ec8c6d73178075a19feaf0e8359e
action <action@github.com>
2025-06-01 18:12:47 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,302 +1,55 @@
 
-const a0_0x373f25 = a0_0x4d2e;
-function a0_0x4d2e(_0x5686c9, _0x5ba168) {
-  const _0x355c6d = a0_0x2522();
-  a0_0x4d2e = function (_0x2d2b7f, _0x2a8569) {
-    _0x2d2b7f = _0x2d2b7f - 362;
-    let _0x200804 = _0x355c6d[_0x2d2b7f];
-    return _0x200804;
+function a0_0x59a6() {
+  const _0xec0106 = ["6582675yeKZid", "189LfpGic", "createClient", "8223754SyaGJf", "9vuUTrK", "connect to redis fial", "../../../config/redis.json", "connect", "6fBRaoc", "21714vPKQJu", "error", "auth", "getValue", "4857944UncYYx", "exports", "4242464NVaZuC", "port", "host", "password", "36595060FxRCfR", "825706WRTddS", "redis"];
+  a0_0x59a6 = function () {
+    return _0xec0106;
   };
-  return a0_0x4d2e(_0x5686c9, _0x5ba168);
+  return a0_0x59a6();
 }
-function a0_0x2522() {
-  const _0x102cdc = ["sss", "huilimj", "gyldg", "tdhmj", "1418165QJDfKZ", "lamj", "broadcastType", "mnpdk", "bijiplus", "wjynn", "wakeng", "roomRentPayType", "payType", "jhplus", "wuhmj", "jinhua", "lhd", "tiandakeng", "cnjj", "1543926wgenDt", "userPermissionType", "3760304DIcVNl", "brniuniu", "recordType", "gameRoomStartType", "wolong", "fcmj", "gameLogType", "chadao", "sdb", "roomType", "gyldglf", "ttz", "emailStatus", "lsmj", "zlm", "loginPlatform", "chexuan", "270QnujJN", "gysdg", "ddz", "scoreChangeType", "psz", "fxj", "1838565rqfxWs", "updateDataType", "dezhoupuke", "4604ngZOkU", "shuiyu", "gysdgsf", "pdk", "mmttz", "ptj", "8tykOKq", "xcmj", "qjmj", "exports", "12IOUMyg", "xzmj", "roomCreatorType", "229311PXVtQc", "sgbjh", "gyzj", "7yVcFlt", "wwmj", "gameType", "biji", "gameRoomDismissReason", "pzhmj", "20CMbsBG", "ajmj", "bpmj", "mhxl", "tsmj", "3657434aOAHjC", "rechargePlatform", "dgn"];
-  a0_0x2522 = function () {
-    return _0x102cdc;
-  };
-  return a0_0x2522();
-}
-(function (_0x5a2099, _0x23e82c) {
-  const _0x511782 = a0_0x4d2e;
-  const _0x6e36d3 = _0x5a2099();
+const a0_0x51ee0b = a0_0x183a;
+(function (_0xccd18b, _0x57ef08) {
+  const _0x38f59c = a0_0x183a;
+  const _0x3cd538 = _0xccd18b();
   while (true) {
     try {
-      const _0x22dfed = parseInt(_0x511782(396)) / 1 + parseInt(_0x511782(383)) / 2 * (parseInt(_0x511782(374)) / 3) + parseInt(_0x511782(389)) / 4 * (parseInt(_0x511782(417)) / 5) + -parseInt(_0x511782(432)) / 6 + parseInt(_0x511782(399)) / 7 * (parseInt(_0x511782(434)) / 8) + -parseInt(_0x511782(380)) / 9 * (parseInt(_0x511782(405)) / 10) + parseInt(_0x511782(410)) / 11 * (-parseInt(_0x511782(393)) / 12);
-      if (_0x22dfed === _0x23e82c) {
+      const _0x3ac267 = parseInt(_0x38f59c(270)) / 1 + parseInt(_0x38f59c(259)) / 2 * (parseInt(_0x38f59c(273)) / 3) + parseInt(_0x38f59c(265)) / 4 + parseInt(_0x38f59c(272)) / 5 + parseInt(_0x38f59c(258)) / 6 * (parseInt(_0x38f59c(275)) / 7) + parseInt(_0x38f59c(263)) / 8 * (-parseInt(_0x38f59c(276)) / 9) + -parseInt(_0x38f59c(269)) / 10;
+      if (_0x3ac267 === _0x57ef08) {
         break;
       } else {
-        _0x6e36d3.push(_0x6e36d3.shift());
+        _0x3cd538.push(_0x3cd538.shift());
       }
-    } catch (_0x4b5bb6) {
-      _0x6e36d3.push(_0x6e36d3.shift());
+    } catch (_0x454c2a) {
+      _0x3cd538.push(_0x3cd538.shift());
     }
   }
-})(a0_0x2522, 475410);
-let enumeration = module[a0_0x373f25(392)];
-enumeration[a0_0x373f25(401)] = {
-  SZ: 1,
-  NN: 2,
-  PDK: 3,
-  SG: 4,
-  ZNMJ: 5,
-  SY: 6,
-  DGN: 8,
-  SSS: 9,
-  XZMJ: 10,
-  GCT: 11,
-  LYC: 12,
-  ZXJMJ: 13,
-  SGBJH: 14,
-  SDB: 15,
-  CHEXUAN: 16,
-  DZPK: 17,
-  TDK: 18,
-  WL: 19,
-  YDR: 20,
-  PTJ: 21,
-  MNPDK: 22,
-  MNMJ: 23,
-  DBL: 24,
-  ZLM: 25,
-  WK: 26,
-  DDZ: 27,
-  JGTTZ: 28,
-  MMTTZ: 29,
-  FJSSS: 30,
-  QZMJ: 31,
-  QZSSS: 32,
-  WWMJ: 33,
-  GD: 34,
-  BJ: 35,
-  TSMJ: 36,
-  XCMJ: 37,
-  HZMJ: 38,
-  GYZJ: 39,
-  MHXL: 40,
-  GYLDG: 41,
-  GYSDG: 42,
-  GYLDGLF: 43,
-  GYSDGLF: 44,
-  MJNN: 45,
-  BIJIPLUS: 46,
-  TTZ: 47,
-  CHADAO: 48,
-  BZMJ: 49,
-  TDHMJ: 50,
-  ENSMJ: 51,
-  WUHMJ: 52,
-  PINGSANZHANG: 53,
-  HUILMJ: 54,
-  JGPTJ: 55,
-  JHPLUS: 56,
-  CNJJ: 57,
-  WJYNN: 58,
-  LAMJ: 59,
-  AJMJ: 60,
-  BPMJ: 61,
-  QJMJ: 62,
-  FCMJ: 63,
-  ZGZ: 64,
-  RCSSGMJ: 65,
-  YBMJ: 66,
-  MASMJ: 67,
-  SRPKD: 68,
-  PZHMJ: 69,
-  LSYJMJ: 70,
-  LSESQ: 71,
-  LS12JMJ: 72,
-  LSPKD: 73,
-  XZSRLF: 101,
-  XZLRLF: 102,
-  XZLRYF: 103,
-  PDKERWU: 131,
-  PDKERLIU: 132,
-  PDKSANWU: 133,
-  PDKSANLIU: 134,
-  BRNN: 201,
-  LHD: 202
-};
-enumeration[a0_0x373f25(363)] = {
-  1: a0_0x373f25(428),
-  2: "niuniu",
-  3: a0_0x373f25(386),
-  4: "sangong",
-  5: "znmj",
-  6: a0_0x373f25(384),
-  8: a0_0x373f25(412),
-  9: a0_0x373f25(413),
-  10: a0_0x373f25(394),
-  11: "gct",
-  12: "lyc",
-  13: a0_0x373f25(379),
-  14: a0_0x373f25(397),
-  15: a0_0x373f25(365),
-  16: a0_0x373f25(373),
-  17: a0_0x373f25(382),
-  18: a0_0x373f25(430),
-  19: a0_0x373f25(438),
-  20: "ydr",
-  21: a0_0x373f25(388),
-  22: a0_0x373f25(420),
-  23: "mnmj",
-  24: "daboluo",
-  25: a0_0x373f25(371),
-  26: a0_0x373f25(423),
-  27: a0_0x373f25(376),
-  28: "jgttz",
-  29: a0_0x373f25(387),
-  30: "fjsss",
-  31: "qzmj",
-  32: "qzsss",
-  33: a0_0x373f25(400),
-  34: "gd",
-  35: a0_0x373f25(402),
-  36: a0_0x373f25(409),
-  37: a0_0x373f25(390),
-  38: "hzmj",
-  39: a0_0x373f25(398),
-  40: a0_0x373f25(408),
-  41: a0_0x373f25(415),
-  42: a0_0x373f25(375),
-  43: a0_0x373f25(367),
-  44: a0_0x373f25(385),
-  46: a0_0x373f25(421),
-  47: a0_0x373f25(368),
-  48: a0_0x373f25(364),
-  49: "bzmj",
-  50: a0_0x373f25(416),
-  51: "ensmj",
-  52: a0_0x373f25(427),
-  53: a0_0x373f25(378),
-  54: a0_0x373f25(414),
-  55: "jgptj",
-  56: a0_0x373f25(426),
-  57: a0_0x373f25(431),
-  58: a0_0x373f25(422),
-  59: a0_0x373f25(418),
-  60: a0_0x373f25(406),
-  61: a0_0x373f25(407),
-  62: a0_0x373f25(391),
-  63: a0_0x373f25(362),
-  64: "zgz",
-  69: a0_0x373f25(404),
-  70: a0_0x373f25(370),
-  71: "lsesq",
-  101: a0_0x373f25(394),
-  102: "xzmj",
-  103: a0_0x373f25(394),
-  131: a0_0x373f25(386),
-  132: a0_0x373f25(386),
-  133: a0_0x373f25(386),
-  134: a0_0x373f25(386),
-  201: a0_0x373f25(435),
-  202: a0_0x373f25(429)
-};
-enumeration[a0_0x373f25(372)] = {
-  NONE: 0,
-  ACCOUNT: 1,
-  WEI_XIN: 2,
-  MOBILE_PHONE: 3,
-  APPLE: 4
-};
-enumeration[a0_0x373f25(381)] = {
-  NONE: 0,
-  ADD: 1,
-  REMOVE: 2,
-  UPDATE: 3
-};
-enumeration[a0_0x373f25(369)] = {
-  NONE: 0,
-  NOT_RECEIVE: 1,
-  RECEIVED: 2
-};
-enumeration[a0_0x373f25(425)] = {
-  NONE: 0,
-  ALI_PAY: 1,
-  WE_CHAT: 2,
-  UNION: 3
-};
-enumeration[a0_0x373f25(419)] = {
-  NONE: 0,
-  LOOP: 1,
-  SYSTEM: 2,
-  BIG_WIN: 3
-};
-enumeration[a0_0x373f25(411)] = {
-  NONE: 0
-};
-enumeration[a0_0x373f25(424)] = {
-  BIG_WIN: 1,
-  AA: 2
-};
-enumeration[a0_0x373f25(366)] = {
-  NONE: 0,
-  NORMAL: 1,
-  CONTINUE: 2,
-  HUNDRED: 3
-};
-enumeration[a0_0x373f25(437)] = {
-  NONE: 0,
-  ALL_READY: 1,
-  AUTO_START: 2
-};
-enumeration[a0_0x373f25(403)] = {
-  NONE: 0,
-  BUREAU_FINISHED: 1,
-  USER_DISMISS: 2,
-  UNION_OWNER_DISMISS: 3,
-  LIMIT_SCORE_DISMISS: 4
-};
-enumeration[a0_0x373f25(395)] = {
-  NONE: 0,
-  USER_CREATE: 1,
-  UNION_CREATE: 2
-};
-enumeration.unionType = {
-  BISAI: 0,
-  PUTONG: 1
-};
-enumeration.roomPayType = {
-  AAZHIFU: 1,
-  YINGJIAZHIFU: 2,
-  WOZHIFU: 3
-};
-enumeration[a0_0x373f25(377)] = {
-  NONE: 0,
-  GIVE: 1,
-  MODIFY_LOW: 2,
-  MODIFY_UP: 3,
-  GAME_WIN: 4,
-  GAME_START_UNION_CHOU: 5,
-  GAME_WIN_CHOU: 6,
-  SAFE_BOX: 7,
-  UNION_SERVICE: 8,
-  FIX_SCORE: 9,
-  SAVE_SCORE: 10,
-  DZ_BAO_XIAN: 11
-};
-enumeration[a0_0x373f25(433)] = {
-  NONE: 0,
-  LOGIN_CLIENT: 1,
-  LOGIN_MT: 2,
-  USER_MANAGER: 4,
-  USER_SYSTEM_MANAGER: 8,
-  EXCHANGE_MANAGER: 16,
-  SPREAD_MANAGER: 32,
-  GAME_MANAGER: 64,
-  DATA_MANAGER: 128,
-  GAME_CONTROL: 256,
-  UNION_MANAGER: 512
-};
-enumeration[a0_0x373f25(436)] = {
-  NONE: 0,
-  RECHARGE: 1,
-  WITHDRAWALS: 2,
-  GAME: 3,
-  LOGIN: 4,
-  EXTRACT_COMMISSION: 5,
-  GAME_PROFIT: 6,
-  EXTRACT_INVENTORY: 7,
-  ADMIN_GRANT: 8,
-  SAFE_BOX: 9
-};
+})(a0_0x59a6, 794921);
+function a0_0x183a(_0x4676dc, _0x2e9182) {
+  const _0x29691a = a0_0x59a6();
+  a0_0x183a = function (_0x5f5877, _0x50101b) {
+    _0x5f5877 = _0x5f5877 - 255;
+    let _0x58fd68 = _0x29691a[_0x5f5877];
+    return _0x58fd68;
+  };
+  return a0_0x183a(_0x4676dc, _0x2e9182);
+}
+const redis = require(a0_0x51ee0b(271));
+let redisConfig = require(a0_0x51ee0b(256));
+const client = redis[a0_0x51ee0b(274)](redisConfig.redis[a0_0x51ee0b(266)], redisConfig.redis[a0_0x51ee0b(267)]);
+client[a0_0x51ee0b(261)](redisConfig[a0_0x51ee0b(271)][a0_0x51ee0b(268)]);
+client.on(a0_0x51ee0b(257), _0x4febd9 => {
+  const _0x1d489a = a0_0x51ee0b;
+  !_0x4febd9 ? console.log("connect to redis success") : console[_0x1d489a(260)](_0x1d489a(255));
+});
+class redisClient {
+  static async setValue(_0x2b2f67, _0x146721, _0x4885cc = 60) {
+    return await client.set(_0x2b2f67, _0x146721, "EX", _0x4885cc);
+  }
+  static async [a0_0x51ee0b(262)](_0x568b14) {
+    return new Promise((_0x1e8bad, _0xd63918) => {
+      client.get(_0x568b14, (_0x18eca9, _0x50635e) => {
+        !!_0x18eca9 ? _0xd63918(null) : _0x1e8bad(_0x50635e);
+      });
+    });
+  }
+}
+module[a0_0x51ee0b(264)] = redisClient;
