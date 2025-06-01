71210b2bfc4f7c902b20b76b500307ab847b0c25
action <action@github.com>
2025-06-01 15:25:31 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,2 +1,151 @@
 
-1;
+const a0_0x1977fc = a0_0x3bed;
+(function (_0x29007a, _0x2ae134) {
+  const _0x89000d = a0_0x3bed;
+  const _0x514b34 = _0x29007a();
+  while (true) {
+    try {
+      const _0x3076aa = parseInt(_0x89000d(268)) / 1 * (parseInt(_0x89000d(266)) / 2) + parseInt(_0x89000d(285)) / 3 * (-parseInt(_0x89000d(300)) / 4) + -parseInt(_0x89000d(264)) / 5 * (parseInt(_0x89000d(305)) / 6) + -parseInt(_0x89000d(301)) / 7 + parseInt(_0x89000d(279)) / 8 + parseInt(_0x89000d(269)) / 9 + -parseInt(_0x89000d(297)) / 10 * (-parseInt(_0x89000d(302)) / 11);
+      if (_0x3076aa === _0x2ae134) {
+        break;
+      } else {
+        _0x514b34.push(_0x514b34.shift());
+      }
+    } catch (_0x5e7880) {
+      _0x514b34.push(_0x514b34.shift());
+    }
+  }
+})(a0_0x2c14, 947144);
+module[a0_0x1977fc(290)] = {};
+let exp = module[a0_0x1977fc(290)];
+let pomelo = require(a0_0x1977fc(286));
+exp[a0_0x1977fc(291)] = function () {
+  const _0x5adf52 = a0_0x1977fc;
+  let _0x5d981d = arguments[0].split(".");
+  let _0x1f6f67 = [];
+  for (let _0x15d3ac in arguments) {
+    if (_0x15d3ac === "0") {
+      continue;
+    }
+    arguments[_0x5adf52(267)](_0x15d3ac) && _0x1f6f67[_0x5adf52(296)](arguments[_0x15d3ac]);
+  }
+  pomelo[_0x5adf52(274)][_0x5adf52(291)][_0x5d981d[0]][_0x5d981d[1]][_0x5d981d[2]][_0x5adf52(298)][_0x5adf52(282)](null, _0x1f6f67);
+};
+exp[a0_0x1977fc(294)] = function (_0x4de535, _0x709e3a, _0x31e0ed) {
+  return new Promise((_0x33bb24, _0x5bc5ec) => {
+    const _0x5acc3e = a0_0x3bed;
+    pomelo[_0x5acc3e(274)][_0x5acc3e(291)].game[_0x5acc3e(284)][_0x5acc3e(294)][_0x5acc3e(298)](_0x4de535, _0x709e3a, _0x31e0ed, function (_0x4002b0, _0x58a0e5) {
+      !!_0x4002b0 ? _0x5bc5ec(_0x4002b0) : _0x33bb24(_0x58a0e5);
+    });
+  });
+};
+exp.joinWatch = function (_0x3b3760, _0x5e95b8, _0x12472f) {
+  return new Promise((_0x552fe9, _0x298e98) => {
+    const _0x966a69 = a0_0x3bed;
+    pomelo[_0x966a69(274)].rpc[_0x966a69(281)][_0x966a69(284)].joinWatch.toServer(_0x3b3760, _0x5e95b8, _0x12472f, function (_0x522a6a, _0x21fb1d) {
+      !!_0x522a6a ? _0x298e98(_0x522a6a) : _0x552fe9(_0x21fb1d);
+    });
+  });
+};
+exp.createRoom = function (_0x11d60d, _0x49f4a1, _0x502e43, _0x32270d) {
+  const _0x9648ec = a0_0x1977fc;
+  console.log(_0x9648ec(275), _0x11d60d, _0x49f4a1, _0x502e43, _0x32270d);
+  return new Promise((_0x1c0fb3, _0x57f617) => {
+    const _0x2eb436 = _0x9648ec;
+    pomelo.app.rpc[_0x2eb436(281)].roomRemote[_0x2eb436(278)][_0x2eb436(298)](_0x11d60d, _0x49f4a1, _0x502e43, _0x32270d, function (_0x889535, _0xb3864) {
+      !!_0x889535 ? _0x57f617(_0x889535) : _0x1c0fb3(_0xb3864);
+    });
+  });
+};
+exp[a0_0x1977fc(273)] = function (_0x5cfbf2, _0x44cb81, _0x2460e6) {
+  const _0x5842e4 = a0_0x1977fc;
+  pomelo.app.rpc[_0x5842e4(281)][_0x5842e4(284)].getMatchRoomList[_0x5842e4(298)](_0x5cfbf2, _0x44cb81, _0x2460e6);
+};
+exp[a0_0x1977fc(271)] = function (_0x27d458, _0x24cd2f, _0x22a113, _0x1598d0) {
+  const _0x5acd6e = a0_0x1977fc;
+  pomelo[_0x5acd6e(274)][_0x5acd6e(291)].game[_0x5acd6e(284)][_0x5acd6e(271)][_0x5acd6e(298)](_0x27d458, _0x24cd2f, _0x22a113, _0x1598d0);
+};
+exp[a0_0x1977fc(304)] = function (_0x743508, _0x41acef, _0xe61210, _0x4556a6, _0x850298) {
+  const _0x12b47a = a0_0x1977fc;
+  pomelo[_0x12b47a(274)][_0x12b47a(291)].game[_0x12b47a(284)][_0x12b47a(304)][_0x12b47a(298)](_0x41acef, _0xe61210, _0x4556a6, _0x850298);
+};
+exp[a0_0x1977fc(293)] = function (_0x5c2a5a, _0xa6ce73, _0x206a92) {
+  return new Promise(_0x885308 => {
+    const _0x2ddc15 = a0_0x3bed;
+    pomelo[_0x2ddc15(274)][_0x2ddc15(291)][_0x2ddc15(281)][_0x2ddc15(284)][_0x2ddc15(293)][_0x2ddc15(298)](_0x5c2a5a, _0xa6ce73, _0x206a92, function (_0x9a681b, _0x25c7a5) {
+      _0x885308(!!_0x25c7a5);
+    });
+  });
+};
+exp[a0_0x1977fc(288)] = function (_0x17a977, _0x438dbb) {
+  return new Promise((_0x222b5e, _0x1a012f) => {
+    const _0x11c60a = a0_0x3bed;
+    pomelo.app.rpc[_0x11c60a(281)][_0x11c60a(284)][_0x11c60a(288)][_0x11c60a(298)](_0x17a977, _0x438dbb, function (_0x570ce3, _0x278c9f) {
+      !!_0x570ce3 ? _0x1a012f(_0x570ce3) : _0x222b5e(_0x278c9f);
+    });
+  });
+};
+exp[a0_0x1977fc(280)] = function (_0x2fdfa4, _0x4a063e, _0x51e266) {
+  const _0x25c779 = a0_0x1977fc;
+  pomelo[_0x25c779(274)].rpc.game[_0x25c779(284)].getRoomGameDataByKind[_0x25c779(298)](_0x2fdfa4, _0x4a063e, _0x51e266);
+};
+exp[a0_0x1977fc(265)] = function (_0x17b072, _0x5089f2) {
+  return new Promise((_0x17025e, _0x233828) => {
+    const _0x2d3c77 = a0_0x3bed;
+    pomelo[_0x2d3c77(274)][_0x2d3c77(291)][_0x2d3c77(281)][_0x2d3c77(284)][_0x2d3c77(265)][_0x2d3c77(298)](_0x17b072, _0x5089f2, function (_0x4bb577, _0xafb26c) {
+      !!_0x4bb577 ? _0x233828(_0x4bb577) : _0x17025e(_0xafb26c);
+    });
+  });
+};
+exp[a0_0x1977fc(292)] = function (_0x34edd0, _0x57dc0f, _0xb1b9aa, _0x10c0ce, _0x167f31) {
+  const _0x524f2c = a0_0x1977fc;
+  pomelo[_0x524f2c(274)][_0x524f2c(291)][_0x524f2c(281)][_0x524f2c(284)][_0x524f2c(292)][_0x524f2c(298)](_0x57dc0f, _0xb1b9aa, _0x10c0ce, _0x167f31);
+};
+exp[a0_0x1977fc(287)] = function (_0x4b6e15, _0x17b952, _0x30cb1e, _0x2d07e3) {
+  const _0x3457a1 = a0_0x1977fc;
+  pomelo[_0x3457a1(274)].rpc[_0x3457a1(272)][_0x3457a1(289)][_0x3457a1(287)][_0x3457a1(298)](_0x4b6e15, _0x17b952, _0x30cb1e, _0x2d07e3);
+};
+exp.requestRobotNotify = function (_0x44d34d, _0x2e28e7, _0xb877cf, _0x14882c) {
+  const _0x4af874 = a0_0x1977fc;
+  pomelo.app[_0x4af874(291)][_0x4af874(270)][_0x4af874(299)].requestRobotNotify[_0x4af874(298)](_0x4af874(276), _0x44d34d, _0x2e28e7, _0xb877cf, _0x14882c);
+};
+exp.robotLeaveRoomNotify = function (_0x189b31, _0xa1168f) {
+  return new Promise((_0x29ce39, _0x278d7c) => {
+    const _0x5c2106 = a0_0x3bed;
+    pomelo[_0x5c2106(274)][_0x5c2106(291)][_0x5c2106(270)][_0x5c2106(299)][_0x5c2106(283)][_0x5c2106(298)](_0x5c2106(276), _0x189b31, _0xa1168f, function (_0x5a5395) {
+      !!_0x5a5395 ? _0x278d7c(_0x5a5395) : _0x29ce39();
+    });
+  });
+};
+exp[a0_0x1977fc(303)] = function (_0x2ebb27) {
+  return new Promise(_0x4bda19 => {
+    const _0x21b5e2 = a0_0x3bed;
+    pomelo[_0x21b5e2(274)].rpc.robot[_0x21b5e2(277)][_0x21b5e2(303)][_0x21b5e2(298)](_0x21b5e2(276), _0x2ebb27, function (_0x403c82, _0x50e036) {
+      !!_0x403c82 ? _0x4bda19(0.5) : _0x4bda19(_0x50e036);
+    });
+  });
+};
+exp[a0_0x1977fc(295)] = function (_0x23a036, _0x29fa0a) {
+  return new Promise((_0x1d6c18, _0x44f8af) => {
+    const _0x4f806d = a0_0x3bed;
+    pomelo.app.rpc[_0x4f806d(270)][_0x4f806d(277)][_0x4f806d(295)][_0x4f806d(298)](_0x4f806d(276), _0x23a036, _0x29fa0a, function (_0x14b476) {
+      !!_0x14b476 ? _0x44f8af(_0x14b476) : _0x1d6c18();
+    });
+  });
+};
+function a0_0x3bed(_0x69c11e, _0x236bb7) {
+  const _0x43c9a0 = a0_0x2c14();
+  a0_0x3bed = function (_0x4a1935, _0x365486) {
+    _0x4a1935 = _0x4a1935 - 264;
+    let _0x1f925a = _0x43c9a0[_0x4a1935];
+    return _0x1f925a;
+  };
+  return a0_0x3bed(_0x69c11e, _0x236bb7);
+}
+function a0_0x2c14() {
+  const _0x529c7a = ["5190sCAidc", "toServer", "robotRemote", "1726668HhiZqe", "7849037syQHpV", "7348XIIFFt", "getCurRobotWinRate", "leaveRoom", "853074yHtygh", "20eKGRUY", "getRoomGameDataByRoomID", "6rQQelG", "hasOwnProperty", "392439SHclxg", "3522870FurwcI", "robot", "matchRoom", "hall", "getMatchRoomList", "app", "rpcAPI createRoom", "robot-1", "controllerRemote", "createRoom", "9227032nIMUVu", "getRoomGameDataByKind", "game", "apply", "robotLeaveRoomNotify", "roomRemote", "3ApKJLj", "pomelo", "recharge", "searchRoomByUid", "rechargeRemote", "exports", "rpc", "updateRoomUserInfo", "isUserInRoom", "joinRoom", "robotGoldChanged", "push"];
+  a0_0x2c14 = function () {
+    return _0x529c7a;
+  };
+  return a0_0x2c14();
+}
