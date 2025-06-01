146e501c8f1634f820c5c06195c90bf126e232aa
action <action@github.com>
2025-06-01 17:45:18 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,68 +1,153 @@
 
-const a0_0x16d6f9 = a0_0xc6dd;
-(function (_0x18498a, _0x150c67) {
-  const _0xa75e14 = a0_0xc6dd;
-  const _0x6cfec9 = _0x18498a();
+function a0_0x4920() {
+  const _0x15d5e2 = ["LEAVE_ROOM_GOLD_NOT_ENOUGH_LIMIT", "4765338dJwtHz", "CAN_NOT_CREATE_NEW_HONG_BAO", "BLOCKED_ACCOUNT", "金币超过房间的金币上限", "最后三局 不可加入", "用户已经在联盟中", "NOT_IN_ROOM", "126371CgsbYI", "不在该房间中", "无法重复申请加入", "充值成功", "代理积分不足,无法进入游戏", "无法进入房间，与房间中的其他玩家太近", "玩家正在房间中，无法操作数据", "NOT_FIND_USER", "添加的用户不是你的下级成员", "GET_HALL_SERVERS_FAIL", "充值失败", "NOT_ENOUGH_GOLD", "USER_DATA_LOCKED", "USER_IN_ROOM_DATA_LOCKED", "FAIL", "5913804tNBEob", "钻石不足", "获取大厅服务器失败", "请求超时", "当前联盟禁止玩家或代理邀请新玩家", "进入房间拥挤,请稍后重试", "ROOM_JOIN_FAIL", "当前正在游戏中，无法离开房间", "修改积分权限不足", "ROOM_SEAT_NOT_EMPTY", "ROOM_COUNT_REACH_LIMIT", "30dqCRhR", "权限不足", "ACCOUNT_NOT_EXIST", "已经创建过牌友圈，无法创建多个", "金币低于房间的金币下限", "金币不足，不可加入", "SMS_SEND_FAILED", "ACCOUNT_EXIST", "ROOM_JOIN_BUSY_LIMIT", "用户不在联盟中", "UNION_ADD_SCORE_LIMIT", "用户数据被锁定，请退出房间后重试", "房间玩家已满", "2288470ULikDD", "REQUEST_DATA_ERROR", "ALREADY_CREATED_UNION", "服务器正在维护中，请稍后再试", "NOT_YOUR_MEMBER", "72XOMLLr", "无法进入房间，获取定位信息失败", "RECHARGE_FAIL", "当前位置已有玩家,请重新坐下", "TOKEN_INFO_ERROR", "FORBID_GIVE_SCORE", "用户不存在", "PERMISSION_NOT_ENOUGH", "8753868okUtHx", "INVITE_ID_ERROR", "NOT_ENOUGH_VIP_LEVEL", "ROOM_ALREAY_LIMIT", "无效用户", "UNION_GAME_ENTER_LIMIT_SCORE", "房间数量到达上线", "SMS_CODE_ERROR", "短信发送失败", "TANSFER_LEVEL_CAPTAIN", "exports", "当前队长拥有下级，不能取消", "INVALID_UERS", "当前房间无法进入", "ROOM_NOT_EXIST", "PHONE_ALREADY_BIND", "联盟不存在", "BE_SHORT_OF_GOLD_COINS", "SQL_ERROR", "不能转移给玩家的下级", "726721UajwJZ", "token无效", "NOT_ENOUGH_SCORE", "4NayEBj", "CAN_NOT_ENTER_TOO_NEAR", "USER_BLOCK_LIMITE", "房间不存在", "IMG_CODE_ERROR", "数据库操作错误", "NICKNAME_EXIST", "1334620dKcdZG", "TANSFER_LEVEL_LINIT", "ALREADY_IN_UNION", "FORBID_INVITE_SCORE", "请求数据错误", "ROOM_PLAYER_COUNT_FULL", "CAN_NOT_ENTER_NOT_LOCATION", "SERVER_MAINTENANCE", "NOT_IN_UNION"];
+  a0_0x4920 = function () {
+    return _0x15d5e2;
+  };
+  return a0_0x4920();
+}
+const a0_0x2c7d47 = a0_0x39d9;
+(function (_0x2706a7, _0x2ae29e) {
+  const _0x45c199 = a0_0x39d9;
+  const _0x59f0c1 = _0x2706a7();
   while (true) {
     try {
-      const _0x37bf13 = -parseInt(_0xa75e14(348)) / 1 * (parseInt(_0xa75e14(340)) / 2) + parseInt(_0xa75e14(355)) / 3 * (parseInt(_0xa75e14(323)) / 4) + parseInt(_0xa75e14(351)) / 5 * (-parseInt(_0xa75e14(341)) / 6) + parseInt(_0xa75e14(343)) / 7 + parseInt(_0xa75e14(325)) / 8 * (-parseInt(_0xa75e14(353)) / 9) + parseInt(_0xa75e14(342)) / 10 * (-parseInt(_0xa75e14(354)) / 11) + parseInt(_0xa75e14(334)) / 12;
-      if (_0x37bf13 === _0x150c67) {
+      const _0x1df45c = parseInt(_0x45c199(310)) / 1 * (-parseInt(_0x45c199(313)) / 2) + parseInt(_0x45c199(330)) / 3 + parseInt(_0x45c199(352)) / 4 + -parseInt(_0x45c199(320)) / 5 * (parseInt(_0x45c199(363)) / 6) + parseInt(_0x45c199(337)) / 7 * (-parseInt(_0x45c199(381)) / 8) + parseInt(_0x45c199(290)) / 9 + -parseInt(_0x45c199(376)) / 10;
+      if (_0x1df45c === _0x2ae29e) {
         break;
       } else {
-        _0x6cfec9.push(_0x6cfec9.shift());
+        _0x59f0c1.push(_0x59f0c1.shift());
       }
-    } catch (_0x444d21) {
-      _0x6cfec9.push(_0x6cfec9.shift());
+    } catch (_0x4bdb14) {
+      _0x59f0c1.push(_0x59f0c1.shift());
     }
   }
-})(a0_0x5096, 690222);
-let dispatch = require(a0_0x16d6f9(331));
-let code = require(a0_0x16d6f9(357));
-let exp = module[a0_0x16d6f9(349)];
-let logger = require(a0_0x16d6f9(344))[a0_0x16d6f9(335)](a0_0x16d6f9(333));
-let token = require(a0_0x16d6f9(332));
-function a0_0xc6dd(_0x3666e6, _0x52250e) {
-  const _0x249fa5 = a0_0x5096();
-  a0_0xc6dd = function (_0x5a95e1, _0xed39f6) {
-    _0x5a95e1 = _0x5a95e1 - 323;
-    let _0x57b4de = _0x249fa5[_0x5a95e1];
-    return _0x57b4de;
+})(a0_0x4920, 860163);
+function a0_0x39d9(_0xd7bba0, _0x501930) {
+  const _0x3da8c5 = a0_0x4920();
+  a0_0x39d9 = function (_0x370a89, _0x3a9e78) {
+    _0x370a89 = _0x370a89 - 285;
+    let _0x5b29d1 = _0x3da8c5[_0x370a89];
+    return _0x5b29d1;
   };
-  return a0_0xc6dd(_0x3666e6, _0x52250e);
+  return a0_0x39d9(_0xd7bba0, _0x501930);
 }
-exp.game = function (_0x345313, _0xef5448, _0x655520, _0x531183) {
-  const _0x2912a0 = a0_0x16d6f9;
-  let _0x3da4dd = _0x345313[_0x2912a0(324)](_0x2912a0(329));
-  if (!!_0x3da4dd) {
-    let _0x5e0f67 = dispatch[_0x2912a0(327)](_0x3da4dd, _0x655520[_0x2912a0(347)](_0x2912a0(328)));
-    !_0x5e0f67 ? (logger[_0x2912a0(345)](_0x2912a0(338)), _0x531183(code[_0x2912a0(330)])) : _0x531183(null, _0x5e0f67.id);
-    return;
-  }
-  let _0x4bea38 = JSON[_0x2912a0(339)](token[_0x2912a0(350)](_0xef5448[_0x2912a0(352)][0].body)) || {};
-  if (_0x4bea38.unionID) {
-    let _0x3dcd9f = dispatch[_0x2912a0(327)](_0x4bea38[_0x2912a0(356)], _0x655520[_0x2912a0(347)]("game"));
-    !_0x3dcd9f ? (logger[_0x2912a0(345)](_0x2912a0(338)), _0x531183(code.FAIL)) : _0x531183(null, _0x3dcd9f.id);
-  } else {
-    logger[_0x2912a0(345)](_0x2912a0(337));
-    _0x531183(code[_0x2912a0(330)]);
-  }
+let constant = function () {
+  const _0x2ebb40 = a0_0x39d9;
+  this.OK = 0;
+  this[_0x2ebb40(351)] = 1;
+  this[_0x2ebb40(377)] = 2;
+  this[_0x2ebb40(308)] = 3;
+  this[_0x2ebb40(302)] = 4;
+  this[_0x2ebb40(289)] = 6;
+  this[_0x2ebb40(297)] = 7;
+  this[_0x2ebb40(317)] = 8;
+  this[_0x2ebb40(369)] = 9;
+  this[_0x2ebb40(327)] = 10;
+  this[_0x2ebb40(348)] = 11;
+  this.USER_DATA_LOCKED = 12;
+  this[_0x2ebb40(312)] = 13;
+  this.THE_FINAL_THREE_MATCHES = 14;
+  this[_0x2ebb40(307)] = 15;
+  this.ACCOUNT_OR_PASSWORD_ERROR = 101;
+  this[_0x2ebb40(346)] = 102;
+  this[_0x2ebb40(370)] = 103;
+  this[_0x2ebb40(365)] = 104;
+  this.NOT_FIND_BIND_PHONE = 105;
+  this[_0x2ebb40(305)] = 106;
+  this[_0x2ebb40(344)] = 107;
+  this[_0x2ebb40(319)] = 108;
+  this[_0x2ebb40(286)] = 201;
+  this[_0x2ebb40(292)] = 202;
+  this.BLOCKED_ACCOUNT = 203;
+  this[_0x2ebb40(378)] = 204;
+  this.UNION_NOT_EXIST = 205;
+  this[_0x2ebb40(350)] = 206;
+  this[_0x2ebb40(328)] = 207;
+  this[_0x2ebb40(322)] = 208;
+  this[_0x2ebb40(291)] = 209;
+  this[_0x2ebb40(380)] = 210;
+  this[_0x2ebb40(287)] = 211;
+  this[_0x2ebb40(323)] = 212;
+  this[_0x2ebb40(331)] = 213;
+  this.CAN_NOT_REPEAT_APPLICATION = 214;
+  this.CAN_NOT_JOIN_UNION = 215;
+  this.ROOM_COUNT_REACH_LIMIT = 301;
+  this.LEAVE_ROOM_GOLD_NOT_ENOUGH_LIMIT = 302;
+  this.LEAVE_ROOM_GOLD_EXCEED_LIMIT = 303;
+  this.CAN_NOT_LEAVE_ROOM = 305;
+  this[_0x2ebb40(336)] = 306;
+  this[_0x2ebb40(325)] = 307;
+  this.ROOM_NOT_EXIST = 308;
+  this[_0x2ebb40(326)] = 309;
+  this[_0x2ebb40(314)] = 310;
+  this[_0x2ebb40(371)] = 311;
+  this[_0x2ebb40(358)] = 312;
+  this.ROOM_NOT_WATCH = 313;
+  this[_0x2ebb40(361)] = 314;
+  this[_0x2ebb40(293)] = 315;
+  this[_0x2ebb40(315)] = 316;
+  this[_0x2ebb40(383)] = 401;
+  this.RECHARGE_SUCCESS = 402;
+  this[_0x2ebb40(295)] = 501;
+  this.UNION_ADD_SCORE_LIMIT = 502;
+  this[_0x2ebb40(321)] = 503;
+  this[_0x2ebb40(299)] = 504;
+  this[this.CAN_NOT_REPEAT_APPLICATION] = _0x2ebb40(339);
+  this[this.ROOM_ALREAY_LIMIT] = "已有同类型房间,请返回大厅加入";
+  this[this[_0x2ebb40(358)]] = _0x2ebb40(303);
+  this[this.ROOM_NOT_WATCH] = "当前房间不允许旁观";
+  this[this[_0x2ebb40(361)]] = _0x2ebb40(285);
+  this[this[_0x2ebb40(371)]] = _0x2ebb40(357);
+  this[this[_0x2ebb40(299)]] = _0x2ebb40(301);
+  this[this[_0x2ebb40(321)]] = _0x2ebb40(309);
+  this[this.UNION_GAME_ENTER_LIMIT_SCORE] = _0x2ebb40(341);
+  this[this[_0x2ebb40(373)]] = _0x2ebb40(360);
+  this[this[_0x2ebb40(351)]] = "请求失败";
+  this[this.REQUEST_DATA_ERROR] = _0x2ebb40(324);
+  this[this.SQL_ERROR] = _0x2ebb40(318);
+  this[this[_0x2ebb40(302)]] = _0x2ebb40(294);
+  this[this[_0x2ebb40(289)]] = _0x2ebb40(364);
+  this[this[_0x2ebb40(297)]] = "短信验证码错误";
+  this[this[_0x2ebb40(317)]] = "图形验证码错误";
+  this[this[_0x2ebb40(369)]] = _0x2ebb40(298);
+  this[this[_0x2ebb40(327)]] = _0x2ebb40(379);
+  this[this[_0x2ebb40(348)]] = _0x2ebb40(353);
+  this[this[_0x2ebb40(349)]] = _0x2ebb40(374);
+  this[this.NOT_ENOUGH_SCORE] = "积分不足";
+  this[this.THE_FINAL_THREE_MATCHES] = _0x2ebb40(334);
+  this[this[_0x2ebb40(307)]] = _0x2ebb40(368);
+  this[this.ACCOUNT_OR_PASSWORD_ERROR] = "账号或密码错误";
+  this[this[_0x2ebb40(346)]] = _0x2ebb40(354);
+  this[this.ACCOUNT_EXIST] = "账号已存在";
+  this[this.ACCOUNT_NOT_EXIST] = "帐号不存在";
+  this[this.NOT_FIND_BIND_PHONE] = "该手机号未绑定用户";
+  this[this.PHONE_ALREADY_BIND] = "该手机号已被绑定，无法重复绑定";
+  this[this[_0x2ebb40(344)]] = _0x2ebb40(288);
+  this[this.NOT_IN_ROOM] = _0x2ebb40(338);
+  this[this.CAN_NOT_ENTER_NOT_LOCATION] = _0x2ebb40(382);
+  this[this.CAN_NOT_ENTER_TOO_NEAR] = _0x2ebb40(342);
+  this[this[_0x2ebb40(286)]] = _0x2ebb40(311);
+  this[this[_0x2ebb40(292)]] = "vip等级不足";
+  this[this[_0x2ebb40(332)]] = "帐号被冻结，禁止登录";
+  this[this[_0x2ebb40(378)]] = _0x2ebb40(366);
+  this[this.UNION_NOT_EXIST] = _0x2ebb40(306);
+  this[this[_0x2ebb40(350)]] = _0x2ebb40(343);
+  this[this[_0x2ebb40(328)]] = _0x2ebb40(372);
+  this[this[_0x2ebb40(322)]] = _0x2ebb40(335);
+  this[this[_0x2ebb40(291)]] = "邀请码错误";
+  this[this.NOT_YOUR_MEMBER] = _0x2ebb40(345);
+  this[this[_0x2ebb40(287)]] = "当前联盟禁止赠送积分";
+  this[this[_0x2ebb40(323)]] = _0x2ebb40(356);
+  this[this[_0x2ebb40(331)]] = "暂时无法创建新的红包";
+  this[this[_0x2ebb40(362)]] = _0x2ebb40(296);
+  this[this[_0x2ebb40(329)]] = _0x2ebb40(367);
+  this[this.LEAVE_ROOM_GOLD_EXCEED_LIMIT] = _0x2ebb40(333);
+  this[this.CAN_NOT_LEAVE_ROOM] = _0x2ebb40(359);
+  this[this[_0x2ebb40(325)]] = _0x2ebb40(375);
+  this[this[_0x2ebb40(304)]] = _0x2ebb40(316);
+  this[this[_0x2ebb40(383)]] = _0x2ebb40(347);
+  this[this.RECHARGE_SUCCESS] = _0x2ebb40(340);
+  this[500] = _0x2ebb40(355);
 };
-exp[a0_0x16d6f9(326)] = function (_0x276ca9, _0xfbc8fa, _0x1181a1, _0x385ba7) {
-  const _0x317cb6 = a0_0x16d6f9;
-  if (!_0x276ca9) {
-    _0x385ba7(new Error(_0x317cb6(336)));
-    return;
-  }
-  if (!_0x276ca9[_0x317cb6(346)]) {
-    _0x385ba7(new Error("fail to find frontend id in session"));
-    return;
-  }
-  _0x385ba7(null, _0x276ca9[_0x317cb6(346)]);
-};
-function a0_0x5096() {
-  const _0x1bc6a5 = ["get", "3086040PFHGEU", "connector", "dispatch", "game", "roomID", "FAIL", "./dispatcher", "../util/token", "pomelo", "21580080jWUQIy", "getLogger", "fail to route to connector server for session is empty", "game server msg invaild", "can not dispatcher game server", "parse", "284piMzwe", "376674UEBCtz", "3341230FnOwUQ", "8922907PGjity", "pomelo-logger", "error", "frontendId", "getServersByType", "8079NEPsXZ", "exports", "getMsg", "55rfYcEA", "args", "9dAYKez", "11RyrOUD", "966gzoURb", "unionID", "../constant/code", "2172TuhCge"];
-  a0_0x5096 = function () {
-    return _0x1bc6a5;
-  };
-  return a0_0x5096();
-}
+module[a0_0x2c7d47(300)] = new constant();
