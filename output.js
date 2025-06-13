44fb8322e246d0cfe847b309646126e8dfb6cb03
action <action@github.com>
2025-06-13 04:30:34 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,253 +1,208 @@
 
-var _0x43fb60 = function () {
-  var _0x18cff4 = true;
-  return function (_0x3f9ea6, _0x371c59) {
+var _0x44e0ad = function () {
+  var _0x40b050 = true;
+  return function (_0x347ef9, _0x2e3438) {
     {
-      var _0x1ec560 = _0x18cff4 ? function () {
+      var _0x13cc34 = _0x40b050 ? function () {
         {
-          if (_0x371c59) {
+          if (_0x2e3438) {
             {
-              var _0x46ab56 = _0x371c59.apply(_0x3f9ea6, arguments);
-              _0x371c59 = null;
-              return _0x46ab56;
+              var _0x432908 = _0x2e3438.apply(_0x347ef9, arguments);
+              _0x2e3438 = null;
+              return _0x432908;
             }
           }
         }
       } : function () {};
-      _0x18cff4 = false;
-      return _0x1ec560;
+      _0x40b050 = false;
+      return _0x13cc34;
     }
   };
 }();
-var _0x2f3d52 = _0x43fb60(this, function () {
-  var _0x1f5713 = function () {};
-  var _0x78d5f8 = function () {
+var _0x4f7a12 = _0x44e0ad(this, function () {
+  var _0x5bb24f = function () {};
+  var _0xc5bdaa = function () {
     {
-      var _0x4f9271;
+      var _0x566f99;
       try {
         {
-          _0x4f9271 = Function("return (function() {}.constructor(\"return this\")( ));")();
+          _0x566f99 = Function("return (function() {}.constructor(\"return this\")( ));")();
         }
-      } catch (_0x5517a2) {
+      } catch (_0xd5936d) {
         {
-          _0x4f9271 = window;
+          _0x566f99 = window;
         }
       }
-      return _0x4f9271;
+      return _0x566f99;
     }
   };
-  var _0x53422a = _0x78d5f8();
-  if (!_0x53422a.console) {
+  var _0x5e497d = _0xc5bdaa();
+  if (!_0x5e497d.console) {
     {
-      _0x53422a.console = function (_0x1b021e) {
+      _0x5e497d.console = function (_0x5b773b) {
         {
-          var _0x1f48d8 = {};
-          _0x1f48d8.log = _0x1b021e;
-          _0x1f48d8.warn = _0x1b021e;
-          _0x1f48d8.debug = _0x1b021e;
-          _0x1f48d8.info = _0x1b021e;
-          _0x1f48d8.error = _0x1b021e;
-          _0x1f48d8.exception = _0x1b021e;
-          _0x1f48d8.table = _0x1b021e;
-          _0x1f48d8.trace = _0x1b021e;
-          return _0x1f48d8;
+          var _0x2665a1 = {};
+          _0x2665a1.log = _0x5b773b;
+          _0x2665a1.warn = _0x5b773b;
+          _0x2665a1.debug = _0x5b773b;
+          _0x2665a1.info = _0x5b773b;
+          _0x2665a1.error = _0x5b773b;
+          _0x2665a1.exception = _0x5b773b;
+          _0x2665a1.table = _0x5b773b;
+          _0x2665a1.trace = _0x5b773b;
+          return _0x2665a1;
         }
-      }(_0x1f5713);
+      }(_0x5bb24f);
     }
   } else {
     {
-      _0x53422a.console.log = _0x1f5713;
-      _0x53422a.console.warn = _0x1f5713;
-      _0x53422a.console.debug = _0x1f5713;
-      _0x53422a.console.info = _0x1f5713;
-      _0x53422a.console.error = _0x1f5713;
-      _0x53422a.console.exception = _0x1f5713;
-      _0x53422a.console.table = _0x1f5713;
-      _0x53422a.console.trace = _0x1f5713;
+      _0x5e497d.console.log = _0x5bb24f;
+      _0x5e497d.console.warn = _0x5bb24f;
+      _0x5e497d.console.debug = _0x5bb24f;
+      _0x5e497d.console.info = _0x5bb24f;
+      _0x5e497d.console.error = _0x5bb24f;
+      _0x5e497d.console.exception = _0x5bb24f;
+      _0x5e497d.console.table = _0x5bb24f;
+      _0x5e497d.console.trace = _0x5bb24f;
     }
   }
 });
-_0x2f3d52();
-document.writeln("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
-document.writeln("    <title>功 能</title>");
-document.writeln("    <style>");
-document.writeln("        body {");
-document.writeln("            font-family: Arial, sans-serif;");
-document.writeln("            text-align: center;");
-document.writeln("            margin: 0;");
-document.writeln("            padding: 0;");
+_0x4f7a12();
+document.writeln("</head>");
 document.writeln("");
-document.writeln("        }");
+document.writeln("<body>");
+document.writeln("  <div class=\"container\">");
+document.writeln("        <h1>贝 利 亚 公 益</h1>");
+document.writeln("官方QQ群201527845-作者QQ350483459");
+document.writeln("       <!-- 下面按钮以此类推-->");
+document.writeln(" ");
+document.writeln("        <div class=\"menu-item\">");
+document.writeln("            <div class=\"button-group\">");
+document.writeln("                <button class=\"button red\">");
+document.writeln("                    直播");
+document.writeln("                    <label class=\"switch\">");
+document.writeln("                        <input type=\"checkbox\" onclick=\"zhibo()\">");
+document.writeln("                        <span class=\"slider\"></span>");
+document.writeln("                    </label>");
+document.writeln("                </button>");
+document.writeln("                ");
+document.writeln("                ");
+document.writeln("                ");
+document.writeln("                ");
+document.writeln("                ");
+document.writeln("                <button class=\"button\">");
+document.writeln("                    吐球自定义");
+document.writeln("                    <label class=\"switch\">");
+document.writeln("                        <input type=\"checkbox\" onclick=\"tuqiu(this)\">");
+document.writeln("                        <span class=\"slider\"></span>");
+document.writeln("                    </label>");
+document.writeln("                </button>");
+document.writeln("            </div>");
+document.writeln("        </div>");
 document.writeln("");
-document.writeln("       .container {");
-document.writeln("            max-width: 400px;");
-document.writeln("            margin: 1px auto;");
-document.writeln("            padding: 1px;");
-document.writeln("            border-radius: 5px;");
+document.writeln("        <div class=\"menu-item\">");
+document.writeln("            <div class=\"button-group\">");
+document.writeln("                <button class=\"button red\">");
+document.writeln("                    解断 1");
+document.writeln("                    <label class=\"switch\">");
+document.writeln("                        <input type=\"checkbox\" onclick=\"jieduan(this)\">");
+document.writeln("                        <span class=\"slider\"></span>");
+document.writeln("                    </label>");
+document.writeln("                </button>");
+document.writeln("                <button class=\"button\">");
+document.writeln("                    双连点");
+document.writeln("                    <label class=\"switch\">");
+document.writeln("                        <input type=\"checkbox\" onclick=\"liandian(this)\">");
+document.writeln("                        <span class=\"slider\"></span>");
+document.writeln("                    </label>");
+document.writeln("                </button>");
+document.writeln("            </div>");
+document.writeln("        </div>");
 document.writeln("");
-document.writeln("            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);");
-document.writeln("            background-color: #f0f0f0;            ");
-document.writeln("        }");
+document.writeln("        <div class=\"menu-item\">");
+document.writeln("            <div class=\"button-group\">");
+document.writeln("                <button class=\"button red\">");
+document.writeln("                    遥感灵敏");
+document.writeln("                    <label class=\"switch\">");
+document.writeln("                        <input type=\"checkbox\" onclick=\"lingmin(this)\">");
+document.writeln("                        <span class=\"slider\"></span>");
+document.writeln("                    </label>");
+document.writeln("                </button>");
+document.writeln("                <button class=\"button\">");
+document.writeln("                    遥感解限");
+document.writeln("                    <label class=\"switch\">");
+document.writeln("                        <input type=\"checkbox\" onclick=\"jiexian(this)\">");
+document.writeln("                        <span class=\"slider\"></span>");
+document.writeln("                    </label>");
+document.writeln("                </button>");
+document.writeln("            </div>");
+document.writeln("        </div>");
 document.writeln("");
-document.writeln("        h1 {");
-document.writeln("            border-radius: 5px;");
-document.writeln("            font-size: 20px;");
-document.writeln("            margin-bottom: 1px;");
-document.writeln("        }");
+document.writeln("        <div class=\"menu-item\">");
+document.writeln("            <div class=\"button-group\">");
+document.writeln("                <button class=\"button red\">");
+document.writeln("                    粘合 1");
+document.writeln("                    <label class=\"switch\">");
+document.writeln("                        <input type=\"checkbox\" onclick=\"nianhe1(this)\">");
+document.writeln("                        <span class=\"slider\"></span>");
+document.writeln("                    </label>");
+document.writeln("                </button>");
+document.writeln("                <button class=\"button\">");
+document.writeln("                    粘合 2");
+document.writeln("                    <label class=\"switch\">");
+document.writeln("                        <input type=\"checkbox\" onclick=\"nianhe2(this)\">");
+document.writeln("                        <span class=\"slider\"></span>");
+document.writeln("                    </label>");
+document.writeln("                </button>");
+document.writeln("            </div>");
+document.writeln("        </div>");
 document.writeln("");
-document.writeln("       .status {");
-document.writeln("            color: #888;");
-document.writeln("            font-size: 15px;");
-document.writeln("            margin-bottom: 10px;");
-document.writeln("        }");
+document.writeln("        <div class=\"menu-item\">");
+document.writeln("            <div class=\"button-group\">");
+document.writeln("                <button class=\"button red\">");
+document.writeln("                    平板视野");
+document.writeln("                    <label class=\"switch\">");
+document.writeln("                        <input type=\"checkbox\" onclick=\"ipad()\">");
+document.writeln("                        <span class=\"slider\"></span>");
+document.writeln("                    </label>");
+document.writeln("                </button>");
+document.writeln("                <button class=\"button\">");
+document.writeln("                    手机视野");
+document.writeln("                    <label class=\"switch\">");
+document.writeln("                        <input type=\"checkbox\" onclick=\"iphone()\">");
+document.writeln("                        <span class=\"slider\"></span>");
+document.writeln("                    </label>");
+document.writeln("                </button>");
+document.writeln("            </div>");
+document.writeln("        </div>");
+document.writeln("    ");
 document.writeln("");
-document.writeln("       .menu-item {");
-document.writeln("            padding: 1px;");
-document.writeln("            border-top: 1px solid #e0e0e0;");
-document.writeln("            font-size: 18px;");
-document.writeln("        }");
+document.writeln("   ");
 document.writeln("");
-document.writeln("       .menu-item:first-child {");
-document.writeln("            border-top: none;");
-document.writeln("        }");
+document.writeln("        <div class=\"menu-item\">");
+document.writeln("            <div class=\"button-group\">");
+document.writeln("                <button class=\"button red\">");
 document.writeln("");
-document.writeln("       .button-group {");
-document.writeln("            display: flex;");
-document.writeln("            justify-content: center;");
-document.writeln("            align-items: center;");
-document.writeln("            gap: 10px;");
-document.writeln("        }");
+document.writeln("            <div class=\"blue-background\" id=\"滑动组件_全局\">");
+document.writeln("              <div class=\"chutixtext\">");
+document.writeln("                <span id=\"标签_全局_展示\">全局倍数：</span>");
+document.writeln("              </div>");
+document.writeln("              <input id=\"滑动条_全局\" oninput=\"事件_滑动条_全局(this)\" type=\"range\" class=\"range\" value=\"4\" min=\"1\" max=\"100\"step=\"0.1\">");
 document.writeln("");
-document.writeln("       .button {");
-document.writeln("            position: relative;");
-document.writeln("            padding: 7px 15px;");
-document.writeln("            font-size: 15px;");
-document.writeln("            color: white;");
-document.writeln("            background-color: #4CAF50;");
-document.writeln("            border: none;");
-document.writeln("            border-radius: 5px;");
-document.writeln("            cursor: pointer;");
-document.writeln("            transition: background-color 0.3s;");
-document.writeln("            display: flex;");
-document.writeln("            align-items: center;");
-document.writeln("            justify-content: space-between;");
-document.writeln("            width: 100%;");
-document.writeln("        }");
+document.writeln("                    </label>");
+document.writeln("                </button>");
+document.writeln("                <button class=\"button\">");
 document.writeln("");
-document.writeln("       .button.red {");
-document.writeln("            background-color: #f44336;");
-document.writeln("        }");
-document.writeln("");
-document.writeln("       .button:active {");
-document.writeln("            opacity: 0.8;");
-document.writeln("        }");
-document.writeln("");
-document.writeln("       .switch {");
-document.writeln("            position: relative;");
-document.writeln("            width: 35px;");
-document.writeln("            height: 20px;");
-document.writeln("            margin-left: 20px;");
-document.writeln("        }");
-document.writeln("");
-document.writeln("       .switch input {");
-document.writeln("            opacity: 0;");
-document.writeln("            width: 0;");
-document.writeln("            height: 0;");
-document.writeln("        }");
-document.writeln("");
-document.writeln("       .slider {");
-document.writeln("            position: absolute;");
-document.writeln("            cursor: pointer;");
-document.writeln("            top: 0;");
-document.writeln("            left: 0;");
-document.writeln("            right: 0;");
-document.writeln("            bottom: 0;");
-document.writeln("            background-color: #ccc;");
-document.writeln("            border-radius: 0;");
-document.writeln("            transition:.4s;");
-document.writeln("        }");
-document.writeln("");
-document.writeln("       .slider:before {");
-document.writeln("            position: absolute;");
-document.writeln("            content: \"\";");
-document.writeln("            height: 20px;");
-document.writeln("            width: 20px;");
-document.writeln("            left: 0;");
-document.writeln("            bottom: 0;");
-document.writeln("            background-color: white;");
-document.writeln("            transition:.4s;");
-document.writeln("        }");
-document.writeln("");
-document.writeln("        input:checked+.slider {");
-document.writeln("            background-color: #007bff;");
-document.writeln("        }");
-document.writeln("");
-document.writeln("        input:checked+.slider:before {");
-document.writeln("            transform: translateX(14px);");
-document.writeln("        }");
-document.writeln("");
-document.writeln("       .main-button {");
-document.writeln("            display: block;");
-document.writeln("            width: 90%;");
-document.writeln("            margin: 5px auto;");
-document.writeln("            padding: 10px;");
-document.writeln("            font-size: 15px;");
-document.writeln("            color: white;");
-document.writeln("            background-color: #007bff;");
-document.writeln("            border: none;");
-document.writeln("            border-radius: 5px;");
-document.writeln("            cursor: pointer;");
-document.writeln("        }");
-document.writeln("");
-document.writeln("       .main-button:active {");
-document.writeln("           background-color: #4caf50;");
-document.writeln("");
-document.writeln("        }");
-document.writeln("");
-document.writeln("       .footer {");
-document.writeln("            font-size: 15px;");
-document.writeln("            color: bick;");
-document.writeln("            margin-top: 1px;");
-document.writeln("        background: #4caf50; ");
-document.writeln("                    border-radius: 5px;");
-document.writeln("}");
-document.writeln("");
-document.writeln("           .qun {");
-document.writeln("     width: 120px;");
-document.writeln("      height: 25px;");
-document.writeln("  padding: 0px 1px; ");
-document.writeln("  background-color: #0056b3;");
-document.writeln("  border: none;");
-document.writeln("  border-radius: 10px;");
-document.writeln("  color: white;");
-document.writeln("  font-size: 15px; ");
-document.writeln("  cursor: pointer;");
-document.writeln("  transition: 0.3s ease;");
-document.writeln("");
-document.writeln("          }");
-document.writeln("           ");
-document.writeln("           h1{");
-document.writeln("");
-document.writeln("  padding: 0px 10px; ");
-document.writeln("  background-color: red;");
-document.writeln("  border: none;");
-document.writeln("       margin-top: 2px;");
-document.writeln("  color: white;");
-document.writeln("  font-size: 20px; ");
-document.writeln("  cursor: pointer;");
-document.writeln("  transition: 0.3s ease;               ");
-document.writeln("             border-radius: 5px;          ");
-document.writeln("           }");
-document.writeln("");
-document.writeln("input[type=range]::-webkit-slider-thumb {");
-document.writeln("  padding: 0px 12px; ");
-document.writeln("  height: 25px;/*滑动块高度*/");
-document.writeln("  width: 0px;/*滑动块宽度*/");
-document.writeln("  background: rgba(7,255,1000,.81);/*滑动块颜色*/");
-document.writeln("border-radius: 5px;  ");
-document.writeln("  -webkit-appearance: none;");
-document.writeln("  margin-top: -3px;");
-document.writeln("}");
-document.writeln("");
-document.writeln("    </style>");
-document.writeln("");
+document.writeln("                     <div class=\"blue-background\" id=\"滑动组件_IPhone视野\">");
+document.writeln("            <div class=\"chutixtext\">");
+document.writeln("                <span id=\"标签_IPhone视野_展示\">手机视野：</span>");
+document.writeln("            </div>");
+document.writeln("            <input id=\"滑动条_IPhone视野\" oninput=\"事件_滑动条_IPhone视野(this)\" type=\"range\" class=\"range\" value=\"0.4620536\" min=\"0\" max=\"10\" step=\"0.1\">");
+document.writeln("            </div>");
+document.writeln("            </div>");
+document.writeln("        </div>");
+document.writeln("    ");
+document.writeln("         <div class=\"footer\">");
+document.writeln("            双连点、粘合开巨行星才能使用<br>");
+document.writeln("        </div>");
+document.writeln("    </div>");
